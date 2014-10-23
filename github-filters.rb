784526e5dd7c0a6b6870c1135d7d67050924c528
Xavi Rigau <xrigau@gmail.com>
2014-10-23 15:35:47 +0100

    Going crayzay with class extension

--- github-filters.rb
+++ github-filters.rb
@@ -1,22 +1,36 @@
+module PullsFilters
+
+  def by(user)
+    Pulls.new self.select { |pull| pull['user']['login'] == user }
+  end
+
+end
+
+module ReposFilters
+
+  def with_pulls_by(user)
+    Repos.new self.select { |repo| repo['pulls'].by(user).size > 0 }
+  end
+
+end
+
+class Repos < Array
+  include ReposFilters
+end
+
+class Pulls < Array
+  include PullsFilters
+end
+
 class Filters
 
-  module RepoFilters
-    def pulls_by(user)
-      self.select do |repo|
-        pulls_from_user = repo['pulls'].select do |pull|
-          pull['user']['login'] == user
-        end
-        pulls_from_user.size > 0
-      end
-    end
+  def initialize(api)
+    ghrepos = api.repos ORGANISATION
+    ghrepos.each { |repo| repo['pulls'] = Pulls.new api.pulls(repo) }
+    @repos = Repos.new ghrepos
   end
 
-  def initialize(repos)
-    @repos = repos
-    @repos.extend RepoFilters
-  end
-
-  def only()
+  def repos()
     @repos
   end
 
--- reports.rb
+++ reports.rb
@@ -9,11 +9,8 @@ ACCESS_TOKEN = '748cd4d55bceb80d16d8f722d3012de96cea54e5' # Hal's token
 def filter()
   api = Github.new ACCESS_TOKEN
   api.fetch ORGANISATION
-
-  repos = api.repos ORGANISATION
-  repos.each { |repo| repo['pulls'] = api.pulls(repo) }
-  Filters.new repos
+  Filters.new api
 end
 
-result = filter.only.pulls_by('xrigau')
+result = filter.repos.with_pulls_by('xrigau')
 puts result.size
