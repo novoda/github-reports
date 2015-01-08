0b5a1fe49245b60c4ba869bed4e4e085fa55001f
Xavi Rigau <xrigau@gmail.com>
2015-01-08 11:23:29 +0100

    Fixes formatting to be consistent with the rest

--- reports.rb
+++ reports.rb
@@ -17,17 +17,18 @@ comments = CommentsFilters.new api
 
 organisation = 'novoda'
 users = [
-'malmstein', 
-'shivghadia', 
-'dorvaryn', 
-'orangeman', 
-'devisnik', 
-'grujo', 
-'electryc',
-'stefanhoth',
-'danybony',
-'zegnus',
-'rock3r']
+  'malmstein',
+  'shivghadia',
+  'dorvaryn',
+  'orangeman',
+  'devisnik',
+  'grujo',
+  'electryc',
+  'stefanhoth',
+  'danybony',
+  'zegnus',
+  'rock3r'
+]
 start_date = '2014-08-01'
 end_date = '2014-10-31'
 
@@ -40,26 +41,27 @@ puts "For the organistation #{organisation}"
 puts "between #{start_date} and #{end_date}"
 
 
-users.each { |user|
+users.each do |user|
 
-	prs = pulls.in(organisation).from(start_date).until(end_date)	
-	merged_prs = pulls.full_data_for(prs).merged_by(user)
-	puts "User #{user} merged #{merged_prs.size} PRs"
+  prs = pulls.in(organisation).from(start_date).until(end_date)
+  merged_prs = pulls.full_data_for(prs).merged_by(user)
+  puts "User #{user} merged #{merged_prs.size} PRs"
 
-	prs = pulls.in(organisation).by(user).from(start_date).until(end_date)
-	puts "User #{user} created #{prs.size} PRs"
+  prs = pulls.in(organisation).by(user).from(start_date).until(end_date)
+  puts "User #{user} created #{prs.size} PRs"
 
-	prs = pulls.in(organisation).by(user)
-	prs_comments = comments.in_all(prs).exclude_by(user).from(start_date).until(end_date)
-	puts "People wrote #{prs_comments.size} comments in #{user}'s PRs"
+  prs = pulls.in(organisation).by(user)
+  prs_comments = comments.in_all(prs).exclude_by(user).from(start_date).until(end_date)
+  puts "People wrote #{prs_comments.size} comments in #{user}'s PRs"
 
-	prs = pulls.in(organisation).exclude_by(user)
-	prs_comments = comments.in_all(prs).by(user).from(start_date).until(end_date)
-	puts "User #{user} wrote #{prs_comments.size} comments in other people PRs"
+  prs = pulls.in(organisation).exclude_by(user)
+  prs_comments = comments.in_all(prs).by(user).from(start_date).until(end_date)
+  puts "User #{user} wrote #{prs_comments.size} comments in other people PRs"
 
-	prs = pulls.in(organisation).from(start_date).until(end_date)
-	prs_comments = comments.in_all(prs).by(user).from(start_date).until(end_date)
-	puts "User #{user} commented on all PR's #{prs_comments.size} times"
-	
-	puts "__"
-}
+  prs = pulls.in(organisation).from(start_date).until(end_date)
+  prs_comments = comments.in_all(prs).by(user).from(start_date).until(end_date)
+  puts "User #{user} commented on all PR's #{prs_comments.size} times"
+
+  puts "__"
+
+end
