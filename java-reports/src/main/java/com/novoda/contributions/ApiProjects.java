f5276004310719f56c255a45cde35d95587ab9c3
Blundell <paul@novoda.com>
2016-01-28 17:36:25 +0000

    Start to query for what developers on what project

---
+++ java-reports/src/main/java/com/novoda/contributions/ApiProjects.java
@@ -0,0 +1,43 @@
+package com.novoda.contributions;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ApiProjects {
+
+    @SerializedName("projects")
+    public List<ApiProject> projects;
+
+    public static class ApiProject {
+
+        @SerializedName("project_id")
+        public String projectId;
+
+        @SerializedName("project_name")
+        public String projectName;
+
+        @SerializedName("client_id")
+        public String clientId;
+
+        @SerializedName("client_name")
+        public String clientName;
+
+        @Override
+        public String toString() {
+            return "ApiProject{" +
+                    "projectId='" + projectId + '\'' +
+                    ", projectName='" + projectName + '\'' +
+                    ", clientId='" + clientId + '\'' +
+                    ", clientName='" + clientName + '\'' +
+                    "}\n";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ApiProjects{" +
+                "projects=" + projects +
+                '}';
+    }
+}
