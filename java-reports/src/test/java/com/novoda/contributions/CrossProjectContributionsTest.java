743411371f053ab7e4a4da0d1a56c29eac521cac
Blundell <paul@novoda.com>
2016-01-29 13:15:03 +0000

    Add test framework

---
+++ java-reports/src/test/java/com/novoda/contributions/CrossProjectContributionsTest.java
@@ -0,0 +1,14 @@
+package com.novoda.contributions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class CrossProjectContributionsTest {
+
+
+    @Test
+    public void testTestingWorks() throws Exception {
+        assertFalse(true);
+    }
+}
