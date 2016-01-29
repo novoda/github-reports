951da2b1e1bb3775c91d74d3860822b7a7b3615e
Blundell <paul@novoda.com>
2016-01-29 13:17:20 +0000

    make tests work

--- java-reports/src/test/java/com/novoda/contributions/CrossProjectContributionsTest.java
+++ java-reports/src/test/java/com/novoda/contributions/CrossProjectContributionsTest.java
@@ -3,12 +3,13 @@ package com.novoda.contributions;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CrossProjectContributionsTest {
 
 
     @Test
     public void testTestingWorks() throws Exception {
-        assertFalse(true);
+        assertTrue(true);
     }
 }
