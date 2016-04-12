4af2db60a7f3f3b0354ca11c295efc411634746f
Blundell <paul@novoda.com>
2016-04-12 20:35:30 +0100

    Starts to depend on java module to talk to mysql amazon db
    mysql example that is closer to the real thing

--- hosted-aws-reports/src/main/java/com/novoda/reports/GitHubReportsLambda.java
+++ hosted-aws-reports/src/main/java/com/novoda/reports/GitHubReportsLambda.java
@@ -2,8 +2,7 @@ package com.novoda.reports;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-import java.sql.*;
+import com.novoda.reports.pullrequest.PullRequestDatabase;
 
 public class GitHubReportsLambda implements RequestHandler<GitHubReportsLambda.LambdaRequest, GitHubReportsLambda.LambdaResponse> {
 
@@ -11,56 +10,16 @@ public class GitHubReportsLambda implements RequestHandler<GitHubReportsLambda.L
     public LambdaResponse handleRequest(LambdaRequest input, Context context) {
         NewLineLogger logger = new NewLineLogger(context.getLogger());
         logger.log("start");
+        PullRequestDatabase pullRequestDatabase = new RdsMySqlPullRequestDatabase();
+        logger.log("started");
+        pullRequestDatabase.open();
+        logger.log("opened");
+        pullRequestDatabase.create();
+        logger.log("created");
+        pullRequestDatabase.close();
+        logger.log("closed");
 
-        Connection connection;
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-            String protocol = "jdbc:mysql://";
-            String endpoint = "ire-mysql-github-reports.cmekjjceogeb.eu-west-1.rds.amazonaws.com";
-            String databaseName = "githubreportsdb";
-            int port = 3306;
-            String url = protocol + endpoint + ":" + port + "/" + databaseName;
-            String user = "halreports";
-            String password = "novoda951";
-            connection = DriverManager.getConnection(url, user, password);
-        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            // Reflection
-            throw new IllegalStateException(e);
-        } catch (SQLException e) {
-            // DB
-            e.printStackTrace();
-            throw new IllegalStateException(e);
-        }
-
-        int itemCount = 0;
-        try {
-            Statement statement = connection.createStatement();
-
-            statement.execute("create table IF NOT EXISTS Employee3 ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))");
-            statement.execute("insert into Employee3 (EmpID, Name) values(1, \"Joe\")");
-            statement.execute("insert into Employee3 (EmpID, Name) values(2, \"Bob\")");
-            statement.execute("insert into Employee3 (EmpID, Name) values(3, \"Mary\")");
-
-            ResultSet resultSet = statement.executeQuery("select * from Employee3");
-            while (resultSet.next()) {
-                itemCount++;
-                String id = resultSet.getString("EmpID");
-                String name = resultSet.getString("Name");
-                logger.log(id + " " + name);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            logger.log(e.toString());
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return new LambdaResponse("Added " + itemCount + " items from RDS MySQL table");
+        return new LambdaResponse("Yeah it worked");
     }
 
     public static class LambdaRequest {
