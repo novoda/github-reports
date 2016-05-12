756c006ff09cbc2da8f5f88f815643eb0a6c00b4
takecare <rui@vazteixeira.org>
2016-05-12 14:41:40 +0100

    add fields to repo model

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/repository/Repository.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/repository/Repository.java
@@ -1,7 +1,60 @@
 package com.novoda.github.reports.github.repository;
 
+import com.google.gson.annotations.SerializedName;
+import com.novoda.github.reports.github.User;
+
 public class Repository {
 
-    // TODO model
+    private String id;
 
+    private String name;
+
+    @SerializedName("full_name")
+    private String fullName;
+
+    @SerializedName("private")
+    private boolean privateRepo;
+
+    @SerializedName("forked")
+    private boolean forkedRepo;
+
+    private User owner;
+
+    @SerializedName("open_issues_count")
+    private int openIssues;
+
+    @SerializedName("has_issues")
+    private boolean issuesPresent;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public boolean isPrivateRepo() {
+        return privateRepo;
+    }
+
+    public boolean isForkedRepo() {
+        return forkedRepo;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public int getOpenIssues() {
+        return openIssues;
+    }
+
+    public boolean issuesPresent() {
+        return issuesPresent;
+    }
 }
