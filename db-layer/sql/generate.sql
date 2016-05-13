6a65f498f14324844b68395626000af996009e77
Francesco Pontillo <francescopontillo@gmail.com>
2016-05-13 14:16:33 +0100

    Format SQL generation script

--- db-layer/sql/generate.sql
+++ db-layer/sql/generate.sql
@@ -51,9 +51,9 @@ CREATE TABLE `project` (
 --
 DROP TABLE IF EXISTS `repository`;
 CREATE TABLE `repository` (
-  `_id`  INT(11)      NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  `private` TINYINT(1) NOT NULL,
+  `_id`     INT(11)      NOT NULL,
+  `name`    VARCHAR(255) NOT NULL,
+  `private` TINYINT(1)   NOT NULL,
   PRIMARY KEY (`_id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 );
