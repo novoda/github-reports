0204665fc046a55f6dde8acd9fc722dae0a09501
takecare <rui@vazteixeira.org>
2016-05-16 17:24:58 +0100

    extract client, interceptors and limit count

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/RateLimitCounter.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/RateLimitCounter.java
@@ -1,4 +1,11 @@
 package com.novoda.github.reports.github;
 
 public class RateLimitCounter {
+
+    public static RateLimitCounter newInstance() {
+        return new RateLimitCounter();
+    }
+
+    RateLimitCounter() {
+    }
 }
