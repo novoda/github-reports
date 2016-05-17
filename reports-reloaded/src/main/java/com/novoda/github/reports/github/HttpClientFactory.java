c8646913c3af5648ffa388dc082f4ad1b84097a3
takecare <rui@vazteixeira.org>
2016-05-17 10:56:36 +0100

    add in interfaces

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/HttpClientFactory.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/HttpClientFactory.java
@@ -1,41 +1,9 @@
 package com.novoda.github.reports.github;
 
-import com.novoda.github.reports.properties.CredentialsReader;
-
 import okhttp3.OkHttpClient;
 
-class HttpClientFactory {
+public interface HttpClientFactory {
 
-    private final OkHttpClient.Builder okHttpClientBuilder;
-    private final CacheFactory cacheFactory;
-    private final CredentialsReader credentialsReader;
-    private final RateLimitCounter rateLimitCounter;
-
-    public static HttpClientFactory newInstance() {
-        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
-        CacheFactory cacheFactory = CacheFactory.newInstance();
-        CredentialsReader credentialsReader = CredentialsReader.newInstance();
-        RateLimitCounter rateLimitCounter = RateLimitCounter.newInstance();
-        return new HttpClientFactory(okHttpClientBuilder, cacheFactory, credentialsReader, rateLimitCounter);
-    }
-
-    HttpClientFactory(OkHttpClient.Builder okHttpClientBuilder,
-                      CacheFactory cacheFactory,
-                      CredentialsReader credentialsReader,
-                      RateLimitCounter rateLimitCounter) {
-        this.okHttpClientBuilder = okHttpClientBuilder;
-        this.cacheFactory = cacheFactory;
-        this.credentialsReader = credentialsReader;
-        this.rateLimitCounter = rateLimitCounter;
-    }
-
-    OkHttpClient createClient() {
-        String token = credentialsReader.getAuthToken();
-        return okHttpClientBuilder
-                .cache(cacheFactory.createCache())
-                .addNetworkInterceptor(new OAuthTokenInterceptor(token))
-                .addNetworkInterceptor(new RateLimitCountInterceptor(rateLimitCounter)) // @RUI lambda vs objs (?)
-                .build();
-    }
+    OkHttpClient createClient();
 
 }
