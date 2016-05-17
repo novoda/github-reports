e4f3d48c7a776b6f778c0cac49e6ba0e020c6376
takecare <rui@vazteixeira.org>
2016-05-17 14:51:14 +0100

    add reset intercpetor

---
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/RateLimitResetInterceptor.java
@@ -0,0 +1,26 @@
+package com.novoda.github.reports.github;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RateLimitResetInterceptor implements Interceptor {
+
+    private static final String RATE_LIMIT_RESET_HEADER = "X-RateLimit-Reset";
+
+    RateLimitResetInterceptor() {
+        //
+    }
+
+    @Override
+    public Response intercept(Interceptor.Chain chain) throws IOException {
+        Request request = chain.request();
+        Response response = chain.proceed(request);
+
+        String resetTimestampAsString = response.headers().get(RATE_LIMIT_RESET_HEADER);
+
+        return response;
+    }
+}
