fbc5c6325efb8abb1ef7ce227030ef4c7d27f47f
takecare <rui@vazteixeira.org>
2016-05-17 09:06:35 +0100

    extract cache creation to factory

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/HttpClientFactory.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/HttpClientFactory.java
@@ -7,20 +7,24 @@ import okhttp3.OkHttpClient;
 class HttpClientFactory {
 
     private final OkHttpClient.Builder okHttpClientBuilder;
+    private final CacheFactory cacheFactory;
     private final CredentialsReader credentialsReader;
     private final RateLimitCounter rateLimitCounter;
 
     public static HttpClientFactory newInstance() {
         OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        CacheFactory cacheFactory = CacheFactory.newInstance();
         CredentialsReader credentialsReader = CredentialsReader.newInstance();
         RateLimitCounter rateLimitCounter = RateLimitCounter.newInstance();
-        return new HttpClientFactory(okHttpClientBuilder, credentialsReader, rateLimitCounter);
+        return new HttpClientFactory(okHttpClientBuilder, cacheFactory, credentialsReader, rateLimitCounter);
     }
 
     HttpClientFactory(OkHttpClient.Builder okHttpClientBuilder,
+                      CacheFactory cacheFactory,
                       CredentialsReader credentialsReader,
                       RateLimitCounter rateLimitCounter) {
         this.okHttpClientBuilder = okHttpClientBuilder;
+        this.cacheFactory = cacheFactory;
         this.credentialsReader = credentialsReader;
         this.rateLimitCounter = rateLimitCounter;
     }
@@ -28,7 +32,7 @@ class HttpClientFactory {
     OkHttpClient createClient() {
         String token = credentialsReader.getAuthToken();
         return okHttpClientBuilder
-                .cache(CacheContainer.INSTANCE.cache()) // TODO extract to CacheFactory
+                .cache(cacheFactory.createCache())
                 .addNetworkInterceptor(new OAuthTokenInterceptor(token))
                 .addNetworkInterceptor(new RateLimitCountInterceptor(rateLimitCounter)) // @RUI lambda vs objs (?)
                 .build();
