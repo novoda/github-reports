95b122777cba0a326d4e41d3836e8018e36e1c10
takecare <rui@vazteixeira.org>
2016-05-17 15:09:48 +0100

    pass in interceptors instead of creating them on the fly

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/RateLimitResetInterceptor.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/RateLimitResetInterceptor.java
@@ -6,7 +6,7 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class RateLimitResetInterceptor implements Interceptor {
+class RateLimitResetInterceptor implements Interceptor {
 
     private static final String RATE_LIMIT_RESET_HEADER = "X-RateLimit-Reset";
 
