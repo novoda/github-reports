88da5a633835baa0b44f7b4fa82a6a3ad94381e6
takecare <rui@vazteixeira.org>
2016-05-18 13:51:29 +0100

    add tests around rate limit reset repo

---
+++ reports-reloaded/src/test/java/com/novoda/github/reports/github/network/GithubRateLimitResetRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.novoda.github.reports.github.network;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GithubRateLimitResetRepositoryTest {
+
+    private static final long INITIAL_TIMESTAMP = System.currentTimeMillis();
+
+    private RateLimitResetRepository rateLimitResetRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        rateLimitResetRepository = new GithubRateLimitResetRepository(INITIAL_TIMESTAMP);
+    }
+
+    @Test
+    public void givenAnInitialValue_whenWeGetTheCurrentTimestampWithoutSetting_thenItIsTheInitialTimestamp() {
+
+        long actual = rateLimitResetRepository.get();
+
+        assertEquals(actual, INITIAL_TIMESTAMP);
+    }
+
+    @Test
+    public void whenWeSetATimestamp_thenWeGetThatTimestamp() {
+        long expected = INITIAL_TIMESTAMP + 1974;
+        rateLimitResetRepository.set(expected);
+
+        assertEquals(expected, rateLimitResetRepository.get());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWeSetANegativeTimestamp_thenAnExceptionIsThrown() throws Exception {
+        rateLimitResetRepository.set(-INITIAL_TIMESTAMP);
+    }
+
+}
