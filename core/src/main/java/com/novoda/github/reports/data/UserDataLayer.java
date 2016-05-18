c147ff82de54492e40dfed9338e6e9a14bf0fbac
takecare <rui@vazteixeira.org>
2016-05-18 11:52:23 +0100

    add functional interface annotation

--- core/src/main/java/com/novoda/github/reports/data/UserDataLayer.java
+++ core/src/main/java/com/novoda/github/reports/data/UserDataLayer.java
@@ -4,6 +4,9 @@ import com.novoda.github.reports.data.model.UserStats;
 
 import java.util.Date;
 
+@FunctionalInterface
 public interface UserDataLayer {
+
     UserStats getStats(String user, String repo, Date from, Date to);
+
 }
