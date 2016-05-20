d9d3d001842fbf1ecd57ee4f115dd8ed01939c24
takecare <rui@vazteixeira.org>
2016-05-20 10:06:56 +0100

    remove call-based request

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/network/GithubService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/network/GithubService.java
@@ -4,15 +4,11 @@ import com.novoda.github.reports.github.repository.Repository;
 
 import java.util.List;
 
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import rx.Observable;
 
 public interface GithubService {
-
-    @GET("/orgs/{org}/repos")
-    Call<List<Repository>> getRepositories(@Path("org") String organisation);
 
     @GET("/orgs/{org}/repos")
     Observable<List<Repository>> getRepositoriesFrom(@Path("org") String organisation);
