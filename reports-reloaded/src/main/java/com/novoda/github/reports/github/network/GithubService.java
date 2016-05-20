dc2aa553e7da61608cde8a171b053bd2c311b8f5
takecare <rui@vazteixeira.org>
2016-05-20 15:12:37 +0100

    WIP

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/network/GithubService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/network/GithubService.java
@@ -4,6 +4,7 @@ import com.novoda.github.reports.github.repository.Repository;
 
 import java.util.List;
 
+import okhttp3.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import rx.Observable;
@@ -12,5 +13,8 @@ public interface GithubService {
 
     @GET("/orgs/{org}/repos")
     Observable<List<Repository>> getRepositoriesFrom(@Path("org") String organisation);
+
+    @GET("/orgs/{org}/repos")
+    Observable<Response> getRepositoriesResponsesFrom(@Path("org") String organisation);
 
 }
