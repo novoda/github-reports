afedea92fcfe49fd4172dd57dd8b1af5316b8cf9
takecare <rui@vazteixeira.org>
2016-05-21 16:47:14 +0100

    add method to retrieve data as response

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/repository/GithubRepositoriesService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/repository/GithubRepositoriesService.java
@@ -4,8 +4,13 @@ import com.novoda.github.reports.github.network.GithubApiService;
 import com.novoda.github.reports.github.network.GithubServiceFactory;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import retrofit2.Response;
 import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
 
 class GithubRepositoriesService implements RepositoryService {
 
@@ -23,5 +28,47 @@ class GithubRepositoriesService implements RepositoryService {
     @Override
     public Observable<List<Repository>> getRepositoriesFrom(String organisation) {
         return githubApiService.getRepositoriesFrom(organisation);
+    }
+
+    void getRepositoriesResponsesFrom(String organisation) {
+
+        githubApiService.getRepositoriesResponseFrom(organisation)
+                .map(new Func1<Response, Response>() {
+                    @Override
+                    public Response call(Response response) {
+
+                        // check if there's 'rels'
+                        String linkHeader = response.headers().get("Link");
+                        //String linkHeader = response.header("Link");
+                        if (linkHeader == null) {
+                            return response;
+                        }
+
+                        Pattern pattern = Pattern.compile("\\?page=(\\d)>; rel=\"next\"");
+                        Matcher matcher = pattern.matcher(linkHeader);
+                        while (matcher.find()) {
+                            String group = matcher.group(1);
+                            System.out.println(">>> "+group);
+                        }
+
+                        return response;
+                    }
+                })
+                .toBlocking()
+                .subscribe(new Subscriber<Response>() {
+                    @Override
+                    public void onCompleted() {}
+
+                    @Override
+                    public void onError(Throwable e) {}
+
+                    @Override
+                    public void onNext(Response response) {
+                        List<Repository> repositories = (List<Repository>) response.body();
+                        System.out.println("+++ "+repositories.size());
+                    }
+                });
+
+
     }
 }
