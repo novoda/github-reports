e41ba7c1cfdba509d5a1cd2dde6739fa7d73147a
Francesco Pontillo <francescopontillo@gmail.com>
2016-05-23 10:46:43 +0100

    Move PropertiesReader to core module and use it to get connection params

--- db-layer/src/main/java/com/novoda/github/reports/data/db/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-package com.novoda.github.reports.data.db;
-
-public class BuildConfig {
-    public static final String DATABASE_URL = "***REMOVED***";
-    public static final String DATABASE_SCHEMA = "reports";
-    public static final String DATABASE_USERNAME = "***REMOVED***";
-    public static final String DATABASE_PASSWORD = "***REMOVED***";
-}
