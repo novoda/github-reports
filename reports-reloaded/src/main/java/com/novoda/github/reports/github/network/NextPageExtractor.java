2dbb8946759dba36b6797f6f93980083b28330c9
takecare <rui@vazteixeira.org>
2016-05-24 09:26:05 +0100

    add next page extractor

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/network/NextPageExtractor.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/network/NextPageExtractor.java
@@ -3,31 +3,20 @@ package com.novoda.github.reports.github.network;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import retrofit2.Response;
+public class NextPageExtractor {
 
-class NextPageExtractor {
+    private static final String PAGE_REGEX = "\\?page=(\\d)>; rel=\"next\"";
 
-    public Integer getNext(String ) {
-        return null;
-    }
+    public Integer getNext(String value) {
 
-    private Integer checkForRels(Response response) {
-        // check if there's 'rels'
-        String linkHeader = response.headers().get("Link");
-        //String linkHeader = response.header("Link");
-        if (linkHeader == null) {
-            return null;
-        }
-
-        Pattern pattern = Pattern.compile("\\?page=(\\d)>; rel=\"next\"");
-        Matcher matcher = pattern.matcher(linkHeader);
-        while (matcher.find()) {
+        Pattern pattern = Pattern.compile(PAGE_REGEX);
+        Matcher matcher = pattern.matcher(value);
+        if (matcher.find()) {
             String group = matcher.group(1);
-            System.out.println(">>> " + group);
             return Integer.parseInt(group);
         }
 
-        return null; // FIXME: 23/05/2016 too many return pts
+        return null;
     }
 
 }
