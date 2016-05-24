c4d6174558a2233d85c9cd2134a06e6bc3917d3d
takecare <rui@vazteixeira.org>
2016-05-24 14:54:47 +0100

    add call to filter with since date

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/IssueService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/IssueService.java
@@ -1,9 +1,13 @@
 package com.novoda.github.reports.github.issue;
 
+import java.util.Date;
+
 import rx.Observable;
 
 interface IssueService {
 
     Observable<Issue> getPagedIssuesFor(String organisation, String repository);
+
+    Observable<Issue> getPagedIssuesFor(String organisation, String repository, Date since);
 
 }
