922ef4b97f3fdf8c1e06cbe8a518d1c72f6e805e
takecare <rui@vazteixeira.org>
2016-05-24 14:55:33 +0100

    add support for state and since query params

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/GithubIssueService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/GithubIssueService.java
@@ -4,8 +4,11 @@ import com.novoda.github.reports.github.network.GithubApiService;
 import com.novoda.github.reports.github.network.GithubServiceFactory;
 import com.novoda.github.reports.github.network.NextPageExtractor;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import org.joda.time.DateTime;
 
 import retrofit2.Response;
 import rx.Observable;
@@ -30,20 +33,27 @@ class GithubIssueService implements IssueService {
 
     @Override
     public Observable<Issue> getPagedIssuesFor(String organisation, String repository) {
-        return getPagedIssuesFor(organisation, repository, 1)
+        return getPagedIssuesFor(organisation, repository, null, 1)
                 .flatMapIterable(Response::body);
     }
 
-    private Observable<Response<List<Issue>>> getPagedIssuesFor(String organisation, String repository, Integer page) {
+    private Observable<Response<List<Issue>>> getPagedIssuesFor(String organisation, String repository, String since, Integer page) {
         return githubApiService
-                .getIssuesResponseForPage(organisation, repository, page, DEFAULT_PER_PAGE_COUNT)
+                .getIssuesResponseForPage(organisation, repository, "all", since, page, DEFAULT_PER_PAGE_COUNT)
                 .concatMap(response -> {
                     Optional<Integer> nextPage = nextPageExtractor.getNextPageFrom(response);
                     Observable<Response<List<Issue>>> observable = Observable.just(response);
                     if (nextPage.isPresent()) {
-                        return observable.mergeWith(getPagedIssuesFor(organisation, repository, nextPage.get()));
+                        return observable.mergeWith(getPagedIssuesFor(organisation, repository, since, nextPage.get()));
                     }
                     return observable;
                 });
+    }
+
+    @Override
+    public Observable<Issue> getPagedIssuesFor(String organisation, String repository, Date since) {
+        String date = new DateTime(since.getTime()).toString();
+        return getPagedIssuesFor(organisation, repository, date, 1)
+                .flatMapIterable(Response::body);
     }
 }
