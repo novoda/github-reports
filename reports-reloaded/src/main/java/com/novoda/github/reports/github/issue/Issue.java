09a1e1f2f861b3ea124b00646be491ef0622efa0
takecare <rui@vazteixeira.org>
2016-05-24 14:54:15 +0100

    add more fields to the issue model

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/Issue.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/Issue.java
@@ -1,5 +1,10 @@
 package com.novoda.github.reports.github.issue;
 
+import com.google.gson.annotations.SerializedName;
+import com.novoda.github.reports.github.User;
+
+import java.util.Date;
+
 public class Issue {
 
     private long id;
@@ -7,6 +12,23 @@ public class Issue {
     private int number;
 
     private String title;
+
+    private User user;
+
+    private String state;
+
+    private String url;
+
+    private int comments;
+
+    @SerializedName("created_at")
+    private Date createdAt;
+
+    @SerializedName("updated_at")
+    private Date updatedAt;
+
+    @SerializedName("closed_at")
+    private Date closedAt;
 
     public long getId() {
         return id;
@@ -20,8 +42,36 @@ public class Issue {
         return title;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Date getClosedAt() {
+        return closedAt;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
     @Override
     public String toString() {
-        return String.format("%d{%s}", id, title);
+        return String.format("%s (%d) - %s , %s", title, id, user.getUsername(), updatedAt);
     }
 }
