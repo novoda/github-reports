69ee0ff1572420a28514a869b0a950da0d9b494c
takecare <rui@vazteixeira.org>
2016-05-24 14:56:20 +0100

    add support for since param

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/IssuesServiceClient.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/IssuesServiceClient.java
@@ -2,6 +2,8 @@ package com.novoda.github.reports.github.issue;
 
 import com.novoda.github.reports.github.repository.Repository;
 
+import java.util.Date;
+
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
@@ -26,5 +28,11 @@ public class IssuesServiceClient {
 
     public Observable<Issue> getIssuesFrom(String organisation, Repository repository) {
         return getIssuesFrom(organisation, repository.getName());
+    }
+
+    public Observable<Issue> getIssuesFrom(String organisation, String repository, Date since) {
+        return issueService.getPagedIssuesFor(organisation, repository, since)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.immediate());
     }
 }
