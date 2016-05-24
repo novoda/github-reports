cfbea9f83a209599683d5f925cf886408e622436
Francesco Pontillo <francescopontillo@gmail.com>
2016-05-24 10:20:22 +0100

    Use BigInteger in UserStats, add number of commented issues

--- reports-reloaded/src/main/java/com/novoda/github/reports/data/mock/MockUserDataLayer.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/data/mock/MockUserDataLayer.java
@@ -3,7 +3,7 @@ package com.novoda.github.reports.data.mock;
 import com.novoda.github.reports.data.UserDataLayer;
 import com.novoda.github.reports.data.model.UserStats;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Date;
 
 public class MockUserDataLayer implements UserDataLayer {
@@ -11,12 +11,13 @@ public class MockUserDataLayer implements UserDataLayer {
     public UserStats getStats(String user, String repo, Date from, Date to) {
         return new UserStats(
                 user,
-                BigDecimal.valueOf(25),
-                BigDecimal.valueOf(123),
-                BigDecimal.valueOf(896),
-                BigDecimal.valueOf(65),
-                BigDecimal.valueOf(437),
-                BigDecimal.valueOf(7)
+                BigInteger.valueOf(43),
+                BigInteger.valueOf(7),
+                BigInteger.valueOf(28),
+                BigInteger.valueOf(120),
+                BigInteger.valueOf(12),
+                BigInteger.valueOf(15),
+                BigInteger.valueOf(2)
         );
     }
 }
