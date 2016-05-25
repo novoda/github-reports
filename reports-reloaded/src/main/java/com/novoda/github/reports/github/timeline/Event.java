d2c07d2619ca5b9ab427d03826c8ca877d649d5d
takecare <rui@vazteixeira.org>
2016-05-25 14:31:49 +0100

    add event model

---
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/timeline/Event.java
@@ -0,0 +1,13 @@
+package com.novoda.github.reports.github.timeline;
+
+import com.novoda.github.reports.github.User;
+
+public class Event {
+
+    private long id;
+
+    private User user;
+
+    private String body;
+    
+}
