ca78fe24067420d58b049c5008db0e1c33859b6a
takecare <rui@vazteixeira.org>
2016-05-25 17:35:57 +0100

    support paging in the timeline service

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/timeline/GithubTimelineService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/timeline/GithubTimelineService.java
@@ -1,5 +1,6 @@
 package com.novoda.github.reports.github.timeline;
 
+import com.novoda.github.reports.github.PagedTransformer;
 import com.novoda.github.reports.github.network.GithubApiService;
 import com.novoda.github.reports.github.network.GithubServiceFactory;
 
@@ -7,9 +8,10 @@ import java.util.List;
 
 import retrofit2.Response;
 import rx.Observable;
-import rx.functions.Func1;
 
 class GithubTimelineService implements TimelineService {
+
+    private static final int DEFAULT_PER_PAGE_COUNT = 100;
 
     private final GithubApiService githubApiService;
 
@@ -24,9 +26,18 @@ class GithubTimelineService implements TimelineService {
 
     @Override
     public Observable<Event> getTimelineFor(String organisation, String repository, Integer issueNumber) {
-        return githubApiService.getTimelineFor(organisation, repository, issueNumber, 1, 100)
-                        .map(Response::body)
-                        .flatMapIterable((Func1<List<Event>, Iterable<Event>>) events -> events);
+        return getTimelineFor(organisation, repository, issueNumber, 1, DEFAULT_PER_PAGE_COUNT)
+                .flatMapIterable(Response::body);
+    }
+
+    private Observable<Response<List<Event>>> getTimelineFor(String organisation,
+                                                             String repository,
+                                                             Integer issueNumber,
+                                                             Integer page,
+                                                             Integer pageCount) {
+        
+        return githubApiService.getTimelineFor(organisation, repository, issueNumber, page, pageCount)
+                .compose(PagedTransformer.newInstance(nextPage -> getTimelineFor(organisation, repository, issueNumber, nextPage, pageCount)));
     }
 
 }
