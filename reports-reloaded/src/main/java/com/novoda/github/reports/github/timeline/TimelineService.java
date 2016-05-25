feeac09cf7536a89f49915752536b64afc985c28
takecare <rui@vazteixeira.org>
2016-05-25 16:27:27 +0100

    reduce visibility

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/timeline/TimelineService.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/timeline/TimelineService.java
@@ -2,7 +2,7 @@ package com.novoda.github.reports.github.timeline;
 
 import rx.Observable;
 
-public interface TimelineService {
+interface TimelineService {
 
     Observable<Event> getTimelineFor(String organisation, String repository, Integer issueNumber);
 
