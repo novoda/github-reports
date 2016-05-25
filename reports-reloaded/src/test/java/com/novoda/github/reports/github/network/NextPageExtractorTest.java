ab71b69349d103b3b12bd05aa69bab35f5b65cfb
takecare <rui@vazteixeira.org>
2016-05-25 13:02:12 +0100

    add one more page extractor test

--- reports-reloaded/src/test/java/com/novoda/github/reports/github/network/NextPageExtractorTest.java
+++ reports-reloaded/src/test/java/com/novoda/github/reports/github/network/NextPageExtractorTest.java
@@ -16,6 +16,8 @@ public class NextPageExtractorTest {
     private static final String ANY_LINK_WITH_NEXT_PAGE = "<https://api.github.com/search/code?page=" + ANY_NEXT_PAGE + ">; rel=\"next\"";
     private static final String ANY_LINK_WITH_NEXT_PAGE_AND_PER_PAGE =
             "<https://api.github.com/organizations/74874/repos?page=" + ANY_NEXT_PAGE +"&per_page=100>; rel=\"next\"";
+    private static final String ANY_LINK_WITH_NEXT_PAGE_AND_OTHERS =
+            "<https://api.github.com/organizations/74874/repos?state=all&page=" + ANY_NEXT_PAGE +"&creator=takecare&per_page=100>; rel=\"next\"";
     private static final String ANY_LINK_WITHOUT_NEXT_PAGE = "<https://api.github.com/search/code?page=34>; rel=\"last\"";
 
     private static final String ANY_BODY = "corpo";
@@ -24,6 +26,9 @@ public class NextPageExtractorTest {
     private static final Headers ANY_HEADERS_WITH_NEXT_PAGE = new Headers.Builder().add("Link", ANY_LINK_WITH_NEXT_PAGE).build();
     private static final Headers ANY_HEADERS_WITH_NEXT_PAGE_AND_PER_PAGE = new Headers.Builder()
             .add("Link", ANY_LINK_WITH_NEXT_PAGE_AND_PER_PAGE)
+            .build();
+    private static final Headers ANY_HEADERS_WITH_NEXT_PAGE_AND_OTHERS = new Headers.Builder()
+            .add("Link", ANY_LINK_WITH_NEXT_PAGE_AND_OTHERS)
             .build();
 
     private Response<String> response;
@@ -64,6 +69,15 @@ public class NextPageExtractorTest {
     @Test
     public void givenResponseWithTheNextPageLinkAndPerPageCount_whenGettingTheNextPage_thenReturnsTheNextPage() throws Exception {
         response = Response.success(ANY_BODY, ANY_HEADERS_WITH_NEXT_PAGE_AND_PER_PAGE);
+
+        Optional<Integer> actual = nextPageExtractor.getNextPageFrom(response);
+
+        assertEquals(Optional.of(ANY_NEXT_PAGE), actual);
+    }
+
+    @Test
+    public void givenResponseWithTheNextPageLinkAndOtherQueries_whenGettingTheNextPage_thenReturnsTheNextPage() throws Exception {
+        response = Response.success(ANY_BODY, ANY_HEADERS_WITH_NEXT_PAGE_AND_OTHERS);
 
         Optional<Integer> actual = nextPageExtractor.getNextPageFrom(response);
 
