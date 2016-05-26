e6640ee22c3a93016aecc71b21491544b3e0d165
takecare <rui@vazteixeira.org>
2016-05-26 17:56:48 +0100

    add comment pojo

---
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/issue/Comment.java
@@ -0,0 +1,20 @@
+package com.novoda.github.reports.github.issue;
+
+import com.google.gson.annotations.SerializedName;
+import com.novoda.github.reports.github.User;
+
+import java.util.Date;
+
+public class Comment {
+
+    private long id;
+
+    private User user;
+
+    @SerializedName("created_at")
+    private Date createdAt;
+
+    @SerializedName("updated_at")
+    private Date updatedAt;
+
+}
