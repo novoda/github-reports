5c74b951bfc40c788491c37bdce9fe129fa53c70
takecare <rui@vazteixeira.org>
2016-05-27 15:19:23 +0100

    fix credentials path

--- reports-batch/src/main/java/com/novoda/github/reports/batch/Main.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/Main.java
@@ -1,9 +1,12 @@
 package com.novoda.github.reports.batch;
 
+import com.novoda.github.reports.batch.github.DebugClient;
+
 public class Main {
 
     private void execute(String[] args) {
         System.out.println("This will be reports-batch");
+        DebugClient.getComments();
     }
 
     public static void main(String[] args) {
