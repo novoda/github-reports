c5bbcc223b0877374fa9633880b7f86fc2d9ddfa
takecare <rui@vazteixeira.org>
2016-05-27 11:51:22 +0100

    add comment explaining the annotation

--- reports-reloaded/src/main/java/com/novoda/github/reports/github/User.java
+++ reports-reloaded/src/main/java/com/novoda/github/reports/github/User.java
@@ -6,7 +6,7 @@ public class User {
 
     private int id;
 
-    @SerializedName(value = "login", alternate = {"name"})
+    @SerializedName(value = "login", alternate = {"name"}) // "name" as an alternative due to compatibility issues with the Timeline API
     private String username;
 
     public int getId() {
