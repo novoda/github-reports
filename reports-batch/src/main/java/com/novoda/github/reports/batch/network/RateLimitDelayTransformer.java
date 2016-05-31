a330dbbd22e4f150380e934c57a12d7c04d4b8a2
takecare <rui@vazteixeira.org>
2016-05-31 17:09:28 +0100

    pass in scheduler

--- reports-batch/src/main/java/com/novoda/github/reports/batch/network/RateLimitDelayTransformer.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/network/RateLimitDelayTransformer.java
@@ -7,32 +7,36 @@ import java.util.concurrent.TimeUnit;
 
 import retrofit2.Response;
 import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
 
 public class RateLimitDelayTransformer implements Observable.Transformer<Response<List<Repository>>, Response<List<Repository>>> {
 
     private final RateLimitRemainingCounter rateLimitRemainingCounter;
     private final RateLimitResetRepository rateLimitResetRepository;
     private final SystemClock systemClock;
+    private final Scheduler scheduler;
 
     public static RateLimitDelayTransformer newInstance() {
         RateLimitRemainingCounter rateLimitRemainingCounter = RateLimitRemainingCounterContainer.getInstance();
         RateLimitResetRepository rateLimitResetRepository = RateLimitRemainingResetRepositoryContainer.getInstance();
-        SystemClock systemClock = new SystemClock() {};
-        return new RateLimitDelayTransformer(rateLimitRemainingCounter, rateLimitResetRepository, systemClock);
+        return new RateLimitDelayTransformer(rateLimitRemainingCounter, rateLimitResetRepository, new SystemClock() {}, Schedulers.computation());
     }
 
     RateLimitDelayTransformer(RateLimitRemainingCounter rateLimitRemainingCounter,
                               RateLimitResetRepository rateLimitResetRepository,
-                              SystemClock systemClock) {
+                              SystemClock systemClock,
+                              Scheduler scheduler) {
         this.rateLimitRemainingCounter = rateLimitRemainingCounter;
         this.rateLimitResetRepository = rateLimitResetRepository;
         this.systemClock = systemClock;
+        this.scheduler = scheduler;
     }
 
     @Override
     public Observable<Response<List<Repository>>> call(Observable<Response<List<Repository>>> observable) {
         if (hasExhaustedRateLimit()) {
-            return observable.delaySubscription(getDelayAmount(), TimeUnit.MILLISECONDS);
+            return observable.delaySubscription(getDelayAmount(), TimeUnit.MILLISECONDS, scheduler);
         }
         return observable;
     }
