dc5e5f015ab7606e4c10ee8ccb2bb7b30d408862
takecare <rui@vazteixeira.org>
2016-05-31 11:28:29 +0100

    add delaying subscription for repos

--- reports-batch/src/main/java/com/novoda/github/reports/batch/repository/GithubRepositoriesService.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/repository/GithubRepositoriesService.java
@@ -1,10 +1,14 @@
 package com.novoda.github.reports.batch.repository;
 
-import com.novoda.github.reports.batch.network.PagedTransformer;
 import com.novoda.github.reports.batch.network.GithubApiService;
 import com.novoda.github.reports.batch.network.GithubServiceFactory;
+import com.novoda.github.reports.batch.network.PagedTransformer;
+import com.novoda.github.reports.batch.network.RateLimitRemainingCounterContainer;
+import com.novoda.github.reports.batch.network.RateLimitRemainingResetRepositoryContainer;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import retrofit2.Response;
 import rx.Observable;
@@ -31,9 +35,24 @@ class GithubRepositoriesService implements RepositoryService {
     }
 
     private Observable<Response<List<Repository>>> getPagedRepositoriesFor(String org, Integer page, Integer pageCount) {
-        return githubApiService
-                .getRepositoriesResponseForPage(org, page, pageCount)
+        return //githubApiService.getRepositoriesResponseForPage(org, page, pageCount)
+                delayGetPagedRepositoriesFor(org, page, pageCount)
                 .compose(PagedTransformer.newInstance(nextPage -> getPagedRepositoriesFor(org, nextPage, pageCount)));
+    }
+
+    private Observable<Response<List<Repository>>> delayGetPagedRepositoriesFor(String organisation, Integer page, Integer pageCount) {
+        int numberOfRemainingRequests = RateLimitRemainingCounterContainer.getInstance().get();
+        if (numberOfRemainingRequests == 0) {
+            long now = Instant.now().getEpochSecond();
+            long resetTimestamp = RateLimitRemainingResetRepositoryContainer.getInstance().get();
+            long delay = (resetTimestamp - now) * 1000L;
+            System.out.println("*** Delaying...");
+            return githubApiService
+                    .getRepositoriesResponseForPage(organisation, page, pageCount)
+                    .delaySubscription(delay, TimeUnit.MILLISECONDS);
+        } else {
+            return githubApiService.getRepositoriesResponseForPage(organisation, page, pageCount);
+        }
     }
 
 }
