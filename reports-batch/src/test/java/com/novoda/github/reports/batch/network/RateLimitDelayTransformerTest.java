11e592fdcb183d676c9d72a70d8bfb6a0cf07943
takecare <rui@vazteixeira.org>
2016-05-31 17:49:33 +0100

    fix tests

--- reports-batch/src/test/java/com/novoda/github/reports/batch/network/RateLimitDelayTransformerTest.java
+++ reports-batch/src/test/java/com/novoda/github/reports/batch/network/RateLimitDelayTransformerTest.java
@@ -2,6 +2,7 @@ package com.novoda.github.reports.batch.network;
 
 import com.novoda.github.reports.batch.repository.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -11,6 +12,9 @@ import org.mockito.Mock;
 
 import retrofit2.Response;
 import rx.Observable;
+import rx.observers.TestSubscriber;
+import rx.schedulers.TestScheduler;
+import rx.subjects.PublishSubject;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -28,8 +32,13 @@ public class RateLimitDelayTransformerTest {
     @Mock
     RateLimitResetRepository mockRateLimitResetRepository;
 
-    @Mock
-    Observable<Response<List<Repository>>> mockObservable;
+    private TestSubscriber<Response<List<Repository>>> testSubscriber;
+
+    private TestScheduler testScheduler;
+
+    private PublishSubject<Response<List<Repository>>> publishSubject;
+
+    private Observable<Response<List<Repository>>> observable;
 
     private SystemClock systemClock;
 
@@ -38,28 +47,67 @@ public class RateLimitDelayTransformerTest {
     @Before
     public void setUp() throws Exception {
         initMocks(this);
+
+        testSubscriber = new TestSubscriber<>();
+        testScheduler = new TestScheduler();
+
         systemClock = new TestClock();
-        rateLimitDelayTransformer = new RateLimitDelayTransformer(mockRateLimitRemainingCounter, mockRateLimitResetRepository, systemClock);
+
+        rateLimitDelayTransformer = new RateLimitDelayTransformer(mockRateLimitRemainingCounter,
+                                                                  mockRateLimitResetRepository,
+                                                                  systemClock,
+                                                                  testScheduler);
+
+        publishSubject = PublishSubject.create();
+        observable = publishSubject.asObservable();
     }
 
     @Test
     public void givenRateLimitHasNotBeenExhausted_whenTransforming_thenNoDelayHappens() {
         when(mockRateLimitRemainingCounter.get()).thenReturn(ANY_POSITIVE_REMAINING_RATE_LIMIT);
 
-        Observable actual = rateLimitDelayTransformer.call(mockObservable);
+        Observable actual = rateLimitDelayTransformer.call(observable);
 
-        assertEquals(mockObservable, actual);
+        assertEquals(observable, actual);
     }
 
     @Test
-    public void givenRateLimitHasBeenExhausted_whenTransforming_thenTheSubscriptionIsDelayed() {
+    public void givenRateLimitHasBeenExhausted_whenTransforming_thenTheLimitsAreChecked() {
         when(mockRateLimitRemainingCounter.get()).thenReturn(0);
         when(mockRateLimitResetRepository.get()).thenReturn(ANY_RESET_TIMESTAMP);
 
-        rateLimitDelayTransformer.call(mockObservable);
+        rateLimitDelayTransformer.call(observable);
 
-        long expectedDelayMillis = ANY_RESET_TIMESTAMP - systemClock.currentTimeMillis();
-        verify(mockObservable).delaySubscription(expectedDelayMillis, TimeUnit.MILLISECONDS);
+        verify(mockRateLimitRemainingCounter).get();
+        verify(mockRateLimitResetRepository).get();
+    }
+
+    @Test
+    public void givenRateLimitHasBeenExhaustedAndDelayGoesBy_whenTransforming_thenGetAnItem() {
+        when(mockRateLimitRemainingCounter.get()).thenReturn(0);
+        when(mockRateLimitResetRepository.get()).thenReturn(ANY_RESET_TIMESTAMP);
+
+        Observable<Response<List<Repository>>> delayed = rateLimitDelayTransformer.call(observable);
+        delayed.subscribe(testSubscriber);
+
+        long delayMillis = ANY_RESET_TIMESTAMP - systemClock.currentTimeMillis();
+        testScheduler.advanceTimeBy(delayMillis, TimeUnit.MILLISECONDS);
+
+        Response<List<Repository>> response = Response.success(new ArrayList<>());
+        publishSubject.onNext(response);
+
+        testSubscriber.assertValue(response);
+    }
+
+    @Test
+    public void givenRateLimitHasBeenExhaustedAndNotEnoughTimeGoesBy_whenTransforming_thenGetAnItem() {
+        when(mockRateLimitRemainingCounter.get()).thenReturn(0);
+        when(mockRateLimitResetRepository.get()).thenReturn(ANY_RESET_TIMESTAMP);
+
+        Observable<Response<List<Repository>>> delayed = rateLimitDelayTransformer.call(observable);
+        delayed.subscribe(testSubscriber);
+
+        testSubscriber.assertNoValues();
     }
 
     private static class TestClock implements SystemClock {
