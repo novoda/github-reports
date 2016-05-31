2a1a2091bfe926ee0d2f7c5aefd043cd784561e9
takecare <rui@vazteixeira.org>
2016-05-31 15:31:13 +0100

    use new delay transformer

--- reports-batch/src/main/java/com/novoda/github/reports/batch/repository/GithubRepositoriesService.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/repository/GithubRepositoriesService.java
@@ -3,11 +3,9 @@ package com.novoda.github.reports.batch.repository;
 import com.novoda.github.reports.batch.network.GithubApiService;
 import com.novoda.github.reports.batch.network.GithubServiceFactory;
 import com.novoda.github.reports.batch.network.PagedTransformer;
-import com.novoda.github.reports.batch.network.RateLimitRemainingCounterContainer;
-import com.novoda.github.reports.batch.network.RateLimitRemainingResetRepositoryContainer;
+import com.novoda.github.reports.batch.network.RateLimitDelayTransformer;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import retrofit2.Response;
 import rx.Observable;
@@ -33,22 +31,9 @@ class GithubRepositoriesService implements RepositoryService {
                 .flatMapIterable(Response::body);
     }
 
-    private Observable<Response<List<Repository>>> getPagedRepositoriesFor(String org, Integer page, Integer pageCount) {
-        return delayIfNeededGetPagedRepositoriesFor(org, page, pageCount)
-                .compose(PagedTransformer.newInstance(nextPage -> getPagedRepositoriesFor(org, nextPage, pageCount)));
+    private Observable<Response<List<Repository>>> getPagedRepositoriesFor(String organisation, Integer page, Integer pageCount) {
+        return githubApiService.getRepositoriesResponseForPage(organisation, page, pageCount)
+                .compose(RateLimitDelayTransformer.newInstance())
+                .compose(PagedTransformer.newInstance(nextPage -> getPagedRepositoriesFor(organisation, nextPage, pageCount)));
     }
-
-    private Observable<Response<List<Repository>>> delayIfNeededGetPagedRepositoriesFor(String organisation, Integer page, Integer pageCount) {
-        int numberOfRemainingRequests = RateLimitRemainingCounterContainer.getInstance().get();
-        if (numberOfRemainingRequests == 0) {
-            long resetTimestamp = RateLimitRemainingResetRepositoryContainer.getInstance().get();
-            long delay = resetTimestamp - System.currentTimeMillis();
-            return githubApiService
-                    .getRepositoriesResponseForPage(organisation, page, pageCount)
-                    .delaySubscription(delay, TimeUnit.MILLISECONDS);
-        } else {
-            return githubApiService.getRepositoriesResponseForPage(organisation, page, pageCount);
-        }
-    }
-
 }
