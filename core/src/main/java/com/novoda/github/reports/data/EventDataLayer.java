c6db6617697b387d5abde5081d5f66410f5a9a63
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-02 16:04:44 +0100

    Create DataLayer interface for common operations

--- core/src/main/java/com/novoda/github/reports/data/EventDataLayer.java
+++ core/src/main/java/com/novoda/github/reports/data/EventDataLayer.java
@@ -2,8 +2,6 @@ package com.novoda.github.reports.data;
 
 import com.novoda.github.reports.data.model.Event;
 
-public interface EventDataLayer {
-
-    Event updateOrInsert(Event event) throws DataLayerException;
+public interface EventDataLayer extends DataLayer<Event> {
 
 }
