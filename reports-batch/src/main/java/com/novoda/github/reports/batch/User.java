534edc422aa4d02b5358b412428eb8ffd664cfeb
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-02 16:20:21 +0100

    Add Repository and User setters to help testing calls

--- reports-batch/src/main/java/com/novoda/github/reports/batch/User.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/User.java
@@ -4,16 +4,20 @@ import com.google.gson.annotations.SerializedName;
 
 public class User {
 
-    private int id;
+    private long id;
 
     @SerializedName(value = "login", alternate = {"name"}) // "name" as an alternative due to compatibility issues with the Timeline API
     private String username;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
