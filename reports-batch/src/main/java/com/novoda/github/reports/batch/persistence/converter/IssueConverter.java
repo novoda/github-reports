64a8c84e139ec4b90564609d71842758b80cd601
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-02 16:23:02 +0100

    Write converter for User and Issue

---
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/persistence/converter/IssueConverter.java
@@ -0,0 +1,26 @@
+package com.novoda.github.reports.batch.persistence.converter;
+
+import com.novoda.github.reports.batch.issue.Issue;
+import com.novoda.github.reports.batch.issue.RepositoryIssue;
+import com.novoda.github.reports.batch.repository.Repository;
+import com.novoda.github.reports.data.model.Event;
+import com.novoda.github.reports.data.model.EventType;
+
+public class IssueConverter implements Converter<RepositoryIssue, Event> {
+
+    public static IssueConverter newInstance() {
+        return new IssueConverter();
+    }
+
+    @Override
+    public Event convertFrom(RepositoryIssue repositoryIssue) {
+        Issue issue = repositoryIssue.getIssue();
+        Repository repo = repositoryIssue.getRepository();
+        return Event.create(issue.getId(),
+                            repo.getId(),
+                            issue.getUser().getId(),
+                            issue.getUser().getId(),
+                            EventType.ISSUE_OPEN,
+                            issue.getCreatedAt());
+    }
+}
