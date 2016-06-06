27ec88c988b0dffb0d8a7bfc364ead0ca766c378
takecare <rui@vazteixeira.org>
2016-06-06 14:51:57 +0100

    rename user model

--- reports-batch/src/main/java/com/novoda/github/reports/batch/persistence/PersistUserTransformer.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/persistence/PersistUserTransformer.java
@@ -3,20 +3,20 @@ package com.novoda.github.reports.batch.persistence;
 import com.novoda.github.reports.batch.issue.RepositoryIssue;
 import com.novoda.github.reports.batch.persistence.converter.Converter;
 import com.novoda.github.reports.data.UserDataLayer;
-import com.novoda.github.reports.data.model.User;
+import com.novoda.github.reports.data.model.DatabaseUser;
 
-public class PersistUserTransformer extends PersistTransformer<RepositoryIssue, User> {
+public class PersistUserTransformer extends PersistTransformer<RepositoryIssue, DatabaseUser> {
 
     private final static int USER_BUFFER_SIZE = 100;
 
     public static PersistUserTransformer newInstance(UserDataLayer userDataLayer,
-                                                     Converter<RepositoryIssue, User> converter) {
+                                                     Converter<RepositoryIssue, DatabaseUser> converter) {
         PersistUserOperator operator = PersistUserOperator.newInstance(userDataLayer, converter);
         PersistBuffer buffer = PersistBuffer.newInstance(USER_BUFFER_SIZE);
         return new PersistUserTransformer(operator, buffer);
     }
 
-    private PersistUserTransformer(PersistOperator<RepositoryIssue, User> operator, PersistBuffer buffer) {
+    private PersistUserTransformer(PersistOperator<RepositoryIssue, DatabaseUser> operator, PersistBuffer buffer) {
         super(operator, buffer);
     }
 
