3a50fdc72443777e369a75ee2ce04ce34c95d4f8
takecare <rui@vazteixeira.org>
2016-06-06 14:49:26 +0100

    rename repo model

--- reports-batch/src/main/java/com/novoda/github/reports/batch/persistence/PersistRepositoryTransformer.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/persistence/PersistRepositoryTransformer.java
@@ -3,19 +3,20 @@ package com.novoda.github.reports.batch.persistence;
 import com.novoda.github.reports.batch.persistence.converter.Converter;
 import com.novoda.github.reports.batch.repository.Repository;
 import com.novoda.github.reports.data.RepoDataLayer;
+import com.novoda.github.reports.data.model.DatabaseRepository;
 
-public class PersistRepositoryTransformer extends PersistTransformer<Repository, com.novoda.github.reports.data.model.Repository> {
+public class PersistRepositoryTransformer extends PersistTransformer<Repository, DatabaseRepository> {
 
     private static final int REPOSITORY_BUFFER_SIZE = 100;
 
     public static PersistRepositoryTransformer newInstance(RepoDataLayer repoDataLayer,
-                                                           Converter<Repository, com.novoda.github.reports.data.model.Repository> converter) {
+                                                           Converter<Repository, DatabaseRepository> converter) {
         PersistRepositoriesOperator operator = PersistRepositoriesOperator.newInstance(repoDataLayer, converter);
         PersistBuffer buffer = PersistBuffer.newInstance(REPOSITORY_BUFFER_SIZE);
         return new PersistRepositoryTransformer(operator, buffer);
     }
 
-    private PersistRepositoryTransformer(PersistOperator<Repository, com.novoda.github.reports.data.model.Repository> operator,
+    private PersistRepositoryTransformer(PersistOperator<Repository, DatabaseRepository> operator,
                                          PersistBuffer buffer) {
         super(operator, buffer);
     }
