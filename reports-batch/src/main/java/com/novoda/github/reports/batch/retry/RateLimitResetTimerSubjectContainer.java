78d509e467d661781b19e45c02230cf9bd1bc075
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-06 17:50:28 +0100

    Use uppercase for constant

--- reports-batch/src/main/java/com/novoda/github/reports/batch/retry/RateLimitResetTimerSubjectContainer.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/retry/RateLimitResetTimerSubjectContainer.java
@@ -2,14 +2,14 @@ package com.novoda.github.reports.batch.retry;
 
 public class RateLimitResetTimerSubjectContainer {
 
-    private static final RateLimitResetTimerSubject subject = RateLimitResetTimerSubject.newInstance();
+    private static final RateLimitResetTimerSubject SUBJECT = RateLimitResetTimerSubject.newInstance();
 
     private RateLimitResetTimerSubjectContainer() {
         // non-instantiable
     }
 
     public static RateLimitResetTimerSubject getInstance() {
-        return subject;
+        return SUBJECT;
     }
 
 }
