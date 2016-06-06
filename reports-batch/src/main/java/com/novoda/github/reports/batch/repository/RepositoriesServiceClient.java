a4c286ff03f4291ebde602d7bee5dbc7b58b6165
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-06 17:51:41 +0100

    Simplify dependencies for RetryWhenTokenResets transformer

--- reports-batch/src/main/java/com/novoda/github/reports/batch/repository/RepositoriesServiceClient.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/repository/RepositoriesServiceClient.java
@@ -1,10 +1,8 @@
 package com.novoda.github.reports.batch.repository;
 
-import com.novoda.github.reports.batch.network.RateLimitRemainingResetRepositoryContainer;
-import com.novoda.github.reports.batch.network.RateLimitResetRepository;
 import com.novoda.github.reports.batch.persistence.ConnectionManagerContainer;
-import com.novoda.github.reports.batch.persistence.converter.Converter;
 import com.novoda.github.reports.batch.persistence.PersistRepositoryTransformer;
+import com.novoda.github.reports.batch.persistence.converter.Converter;
 import com.novoda.github.reports.batch.persistence.converter.RepositoryConverter;
 import com.novoda.github.reports.batch.retry.RateLimitResetTimerSubject;
 import com.novoda.github.reports.batch.retry.RateLimitResetTimerSubjectContainer;
@@ -22,7 +20,6 @@ public class RepositoriesServiceClient {
     private final RepoDataLayer repoDataLayer;
     private final Converter<Repository, com.novoda.github.reports.data.model.Repository> converter;
 
-    private final RateLimitResetRepository rateLimitResetRepository;
     private final RateLimitResetTimerSubject rateLimitResetTimerSubject;
 
     public static RepositoriesServiceClient newInstance() {
@@ -30,26 +27,23 @@ public class RepositoriesServiceClient {
         ConnectionManager connectionManager = ConnectionManagerContainer.getConnectionManager();
         RepoDataLayer repoDataLayer = DbRepoDataLayer.newInstance(connectionManager);
         Converter<Repository, com.novoda.github.reports.data.model.Repository> converter = RepositoryConverter.newInstance();
-        RateLimitResetRepository rateLimitResetRepository = RateLimitRemainingResetRepositoryContainer.getInstance();
         RateLimitResetTimerSubject rateLimitResetTimerSubject = RateLimitResetTimerSubjectContainer.getInstance();
-        return new RepositoriesServiceClient(repositoriesService, repoDataLayer, converter, rateLimitResetRepository, rateLimitResetTimerSubject);
+        return new RepositoriesServiceClient(repositoriesService, repoDataLayer, converter, rateLimitResetTimerSubject);
     }
 
     private RepositoriesServiceClient(GithubRepositoriesService repositoryService,
                                       RepoDataLayer repoDataLayer,
                                       Converter<Repository, com.novoda.github.reports.data.model.Repository> converter,
-                                      RateLimitResetRepository rateLimitResetRepository,
                                       RateLimitResetTimerSubject rateLimitResetTimerSubject) {
         this.repositoryService = repositoryService;
         this.repoDataLayer = repoDataLayer;
         this.converter = converter;
-        this.rateLimitResetRepository = rateLimitResetRepository;
         this.rateLimitResetTimerSubject = rateLimitResetTimerSubject;
     }
 
     public Observable<Repository> retrieveRepositoriesFrom(String organisation) {
         return repositoryService.getPagedRepositoriesFor(organisation)
-                .compose(RetryWhenTokenResets.newInstance(rateLimitResetTimerSubject, rateLimitResetRepository))
+                .compose(RetryWhenTokenResets.newInstance(rateLimitResetTimerSubject))
                 .compose(PersistRepositoryTransformer.newInstance(repoDataLayer, converter))
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.immediate());
