74b41799418262faceb5dffd91f37e6d3ccf7920
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-08 15:23:48 +0100

    Add email parameter

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/command/BatchOptions.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/command/BatchOptions.java
@@ -3,6 +3,7 @@ package com.novoda.github.reports.batch.aws.command;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.converters.ISO8601DateConverter;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +15,9 @@ public class BatchOptions {
     @Parameter(names = "--from", description = "Start of range", converter = ISO8601DateConverter.class)
     private Date from;
 
+    @Parameter(names = "--email", description = "Email address to report job completion to", variableArity = true)
+    private List<String> emails = new ArrayList<>();
+
     public String getOrganisation() {
         if (organisation != null && !organisation.isEmpty()) {
             return organisation.get(0);
@@ -23,5 +27,9 @@ public class BatchOptions {
 
     public Date getFrom() {
         return from;
+    }
+
+    public List<String> getEmails() {
+        return emails;
     }
 }
