3988b7069baaaeab3c13dd8a3cd73b4c4342348b
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-08 15:13:04 +0100

    Add dependencies and main

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/Main.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/Main.java
@@ -1,5 +1,7 @@
 package com.novoda.github.reports.batch.aws;
 
+import com.beust.jcommander.JCommander;
+import com.novoda.github.reports.batch.aws.command.BatchOptions;
 import com.novoda.github.reports.data.db.LogHelper;
 
 public class Main {
@@ -13,6 +15,9 @@ public class Main {
     }
 
     private void execute(String[] args) {
+        BatchOptions options = new BatchOptions();
+        JCommander commander = new JCommander(options);
+        commander.parse(args);
     }
 
 }
