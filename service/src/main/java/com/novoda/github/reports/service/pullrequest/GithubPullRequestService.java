a362dc47a16c55773384101bf54d28300c64a86a
takecare <rui@vazteixeira.org>
2016-06-09 17:07:22 +0100

    refactor pr service and add pr service client

--- service/src/main/java/com/novoda/github/reports/service/pullrequest/GithubPullRequestService.java
+++ service/src/main/java/com/novoda/github/reports/service/pullrequest/GithubPullRequestService.java
@@ -1,66 +1,35 @@
 package com.novoda.github.reports.service.pullrequest;
 
-import com.novoda.github.reports.service.network.GithubApiService;
-import com.novoda.github.reports.service.network.PagedTransformer;
 import com.novoda.github.reports.service.issue.GithubComment;
-import com.novoda.github.reports.service.network.DateToISO8601Converter;
+import com.novoda.github.reports.service.network.GithubApiService;
 import com.novoda.github.reports.service.network.GithubServiceContainer;
-import com.novoda.github.reports.service.network.RateLimitDelayTransformer;
 
-import java.util.Date;
 import java.util.List;
 
 import retrofit2.Response;
 import rx.Observable;
 
-public class GithubPullRequestService implements PullRequestService {
-
-    private static final int DEFAULT_PER_PAGE_COUNT = 100;
-    private static final int FIRST_PAGE = 1;
+class GithubPullRequestService implements PullRequestService {
 
     private final GithubApiService githubApiService;
-    private final DateToISO8601Converter dateConverter;
-    private final RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer;
 
     public static GithubPullRequestService newInstance() {
         GithubApiService githubApiService = GithubServiceContainer.getGithubService();
-        DateToISO8601Converter dateConverter = new DateToISO8601Converter();
-        RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer = RateLimitDelayTransformer.newInstance();
-        return new GithubPullRequestService(githubApiService, dateConverter, commentRateLimitDelayTransformer);
+        return new GithubPullRequestService(githubApiService);
     }
 
-    private GithubPullRequestService(GithubApiService githubApiService,
-                                     DateToISO8601Converter dateConverter,
-                                     RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer) {
-
+    private GithubPullRequestService(GithubApiService githubApiService) {
         this.githubApiService = githubApiService;
-        this.dateConverter = dateConverter;
-        this.commentRateLimitDelayTransformer = commentRateLimitDelayTransformer;
     }
 
-    @Override
-    public Observable<GithubComment> getPullRequestReviewCommentsFor(String organisation, String repository, Integer pullRequestNumber, Date since) {
-        String date = dateConverter.toISO8601NoMillisOrNull(since);
-        return getPagedReviewCommentsForPullRequestFor(organisation, repository, pullRequestNumber, date, FIRST_PAGE, DEFAULT_PER_PAGE_COUNT)
-                .flatMapIterable(Response::body);
+    public Observable<Response<List<GithubComment>>> getPullRequestReviewCommentsFor(String organisation,
+                                                                                     String repository,
+                                                                                     int pullRequestNumber,
+                                                                                     String since,
+                                                                                     int page,
+                                                                                     int pageCount) {
+
+        return githubApiService.getReviewCommentsResponseForPullRequestAndPage(organisation, repository, pullRequestNumber, since, page, pageCount);
     }
 
-    private Observable<Response<List<GithubComment>>> getPagedReviewCommentsForPullRequestFor(String organisation,
-                                                                                              String repository,
-                                                                                              Integer pullRequestNumber,
-                                                                                              String since,
-                                                                                              Integer page,
-                                                                                              Integer pageCount) {
-
-        return githubApiService.getReviewCommentsResponseForPullRequestAndPage(organisation, repository, pullRequestNumber, since, page, pageCount)
-                .compose(commentRateLimitDelayTransformer)
-                .compose(PagedTransformer.newInstance(nextPage -> getPagedReviewCommentsForPullRequestFor(
-                        organisation,
-                        repository,
-                        pullRequestNumber,
-                        since,
-                        nextPage,
-                        pageCount
-                )));
-    }
 }
