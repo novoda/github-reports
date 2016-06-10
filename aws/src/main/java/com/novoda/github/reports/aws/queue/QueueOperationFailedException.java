5b0262f8c7deb5bc8d945abb4871e3b1eb8d413f
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-10 12:01:34 +0100

    Test the BasicWorker behaviour

--- aws/src/main/java/com/novoda/github/reports/aws/queue/QueueOperationFailedException.java
+++ aws/src/main/java/com/novoda/github/reports/aws/queue/QueueOperationFailedException.java
@@ -2,7 +2,7 @@ package com.novoda.github.reports.aws.queue;
 
 public class QueueOperationFailedException extends Exception {
 
-    QueueOperationFailedException(String what) {
+    public QueueOperationFailedException(String what) {
         super(String.format("The queue operation \"%s\" has failed.", what));
     }
 
