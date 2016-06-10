53872fff55446a57c764f9c83183fffe59c85bfb
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-10 15:28:30 +0100

    Improve and fix AmazonQueueMessageConverter from and to SQS Message

--- aws/src/main/java/com/novoda/github/reports/aws/queue/AmazonQueueMessageConverter.java
+++ aws/src/main/java/com/novoda/github/reports/aws/queue/AmazonQueueMessageConverter.java
@@ -4,12 +4,15 @@ import com.amazonaws.services.sqs.model.Message;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class AmazonQueueMessageConverter {
+import static com.novoda.github.reports.aws.queue.AmazonRawQueueMessage.Type.*;
 
+class AmazonQueueMessageConverter {
+
+    private static final String ISO_8601_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
     private final Gson gson;
 
-    public static AmazonQueueMessageConverter newInstance() {
-        Gson gson = new GsonBuilder().create();
+    static AmazonQueueMessageConverter newInstance() {
+        Gson gson = new GsonBuilder().setDateFormat(ISO_8601_DATE_TIME_FORMAT).create();
         return new AmazonQueueMessageConverter(gson);
     }
 
@@ -17,22 +20,30 @@ public class AmazonQueueMessageConverter {
         this.gson = gson;
     }
 
-    public AmazonQueueMessage fromMessage(Message message) throws MessageConverterException {
+    AmazonQueueMessage fromMessage(Message message) throws MessageConverterException {
         AmazonRawQueueMessage rawQueueMessage = fromMessageToRaw(message);
-        switch (rawQueueMessage.getType()) {
-            case REPOSITORIES:
+
+        if (rawQueueMessage != null) {
+            AmazonRawQueueMessage.Type messageType = rawQueueMessage.getType();
+            if (messageType == REPOSITORIES) {
                 return toGetRepositoriesMessage(rawQueueMessage, message);
-            case ISSUES:
+            }
+            if (messageType == ISSUES) {
                 return toGetIssuesMessage(rawQueueMessage, message);
-            case COMMENTS:
+            }
+            if (messageType == COMMENTS) {
                 return toGetCommentsMessage(rawQueueMessage, message);
-            case EVENTS:
+            }
+            if (messageType == EVENTS) {
                 return toGetEventsMessage(rawQueueMessage, message);
-            case REVIEW_COMMENTS:
+            }
+            if (messageType == REVIEW_COMMENTS) {
                 return toGetReviewCommentsMessage(rawQueueMessage, message);
-            default:
-                throw new MessageConverterException("Can't convert type " + rawQueueMessage.getType() + ".");
+            }
+            throw new MessageConverterException("Can't convert type " + rawQueueMessage.getType() + ".");
         }
+
+        throw new MessageConverterException("Can't convert an empty or invalid JSON.");
     }
 
     private AmazonRawQueueMessage fromMessageToRaw(Message message) {
@@ -101,33 +112,73 @@ public class AmazonQueueMessageConverter {
         );
     }
 
-    public Message toMessage(AmazonQueueMessage message) {
-        AmazonRawQueueMessage rawQueueMessage = new AmazonRawQueueMessage();
-
-        rawQueueMessage.isTerminal = message.localTerminal();
-        rawQueueMessage.page = message.page();
-
-        if (message instanceof AmazonGetRepositoriesQueueMessage) {
-            rawQueueMessage.organisationName = ((AmazonGetRepositoriesQueueMessage) message).organisationName();
-            rawQueueMessage.since = ((AmazonGetRepositoriesQueueMessage) message).since();
-        }
-        if (message instanceof AmazonGetIssuesQueueMessage) {
-            rawQueueMessage.repositoryId = ((AmazonGetIssuesQueueMessage) message).repositoryId();
-            rawQueueMessage.repositoryName = ((AmazonGetIssuesQueueMessage) message).repositoryName();
-        }
-        if (message instanceof AmazonGetCommentsQueueMessage) {
-            rawQueueMessage.issueNumber = ((AmazonGetCommentsQueueMessage) message).issueNumber();
-        }
-        if (message instanceof AmazonGetEventsQueueMessage) {
-            rawQueueMessage.issueNumber = ((AmazonGetEventsQueueMessage) message).issueNumber();
-        }
-        if (message instanceof AmazonGetReviewCommentsQueueMessage) {
-            rawQueueMessage.issueNumber = ((AmazonGetReviewCommentsQueueMessage) message).issueNumber();
-        }
+    Message toMessage(AmazonQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = toRawMessage(message);
 
         String messageBody = gson.toJson(rawQueueMessage);
         return new Message()
                 .withReceiptHandle(message.receiptHandle())
                 .withBody(messageBody);
+    }
+
+    AmazonRawQueueMessage toRawMessage(AmazonQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = new AmazonRawQueueMessage();
+
+        if (message instanceof AmazonGetRepositoriesQueueMessage) {
+            rawQueueMessage = toRepositoriesRawMessage((AmazonGetRepositoriesQueueMessage) message);
+        } else if (message instanceof AmazonGetIssuesQueueMessage) {
+            rawQueueMessage = toGetIssuesRawMessage((AmazonGetIssuesQueueMessage) message);
+        } else if (message instanceof AmazonGetCommentsQueueMessage) {
+            rawQueueMessage = toGetCommentsRawMessage((AmazonGetCommentsQueueMessage) message);
+        } else if (message instanceof AmazonGetEventsQueueMessage) {
+            rawQueueMessage = toGetEventsRawMessage((AmazonGetEventsQueueMessage) message);
+        } else if (message instanceof AmazonGetReviewCommentsQueueMessage) {
+            rawQueueMessage = toGetReviewCommentsRawMessage((AmazonGetReviewCommentsQueueMessage) message);
+        }
+
+        rawQueueMessage.isTerminal = message.localTerminal();
+        rawQueueMessage.page = message.page();
+
+        return rawQueueMessage;
+    }
+
+    private AmazonRawQueueMessage toGetCommentsRawMessage(GetCommentsQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = toGetGenericEventsRawMessage(message);
+        rawQueueMessage.type = COMMENTS;
+        return rawQueueMessage;
+    }
+
+    private AmazonRawQueueMessage toGetEventsRawMessage(GetCommentsQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = toGetGenericEventsRawMessage(message);
+        rawQueueMessage.type = EVENTS;
+        return rawQueueMessage;
+    }
+
+    private AmazonRawQueueMessage toGetReviewCommentsRawMessage(GetCommentsQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = toGetGenericEventsRawMessage(message);
+        rawQueueMessage.type = REVIEW_COMMENTS;
+        return rawQueueMessage;
+    }
+
+    private AmazonRawQueueMessage toGetGenericEventsRawMessage(GetCommentsQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = toGetIssuesRawMessage(message);
+        rawQueueMessage.issueNumber = message.issueNumber();
+        return rawQueueMessage;
+    }
+
+    private AmazonRawQueueMessage toGetIssuesRawMessage(GetIssuesQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = toRepositoriesRawMessage(message);
+        rawQueueMessage.type = ISSUES;
+        rawQueueMessage.repositoryId = message.repositoryId();
+        rawQueueMessage.repositoryName = message.repositoryName();
+        return rawQueueMessage;
+    }
+
+    private AmazonRawQueueMessage toRepositoriesRawMessage(GetRepositoriesQueueMessage message) {
+        AmazonRawQueueMessage rawQueueMessage = new AmazonRawQueueMessage();
+        rawQueueMessage.type = REPOSITORIES;
+        rawQueueMessage.organisationName = message.organisationName();
+        rawQueueMessage.since = message.since();
+        return rawQueueMessage;
     }
 }
