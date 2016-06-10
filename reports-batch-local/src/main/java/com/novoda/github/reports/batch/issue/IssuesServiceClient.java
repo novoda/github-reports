49e8eccbb60f0563e480e2a65284b7b0e8c59ad1
takecare <rui@vazteixeira.org>
2016-06-10 10:42:38 +0100

    extract comments retrieval from issues srvc client to a new dedicated one

--- reports-batch-local/src/main/java/com/novoda/github/reports/batch/issue/IssuesServiceClient.java
+++ reports-batch-local/src/main/java/com/novoda/github/reports/batch/issue/IssuesServiceClient.java
@@ -10,14 +10,12 @@ import com.novoda.github.reports.data.db.DbEventDataLayer;
 import com.novoda.github.reports.data.db.DbUserDataLayer;
 import com.novoda.github.reports.data.model.Event;
 import com.novoda.github.reports.data.model.User;
-import com.novoda.github.reports.service.issue.GithubComment;
 import com.novoda.github.reports.service.issue.GithubEvent;
 import com.novoda.github.reports.service.issue.GithubIssue;
 import com.novoda.github.reports.service.issue.GithubIssueService;
 import com.novoda.github.reports.service.issue.IssueService;
 import com.novoda.github.reports.service.issue.RepositoryIssue;
 import com.novoda.github.reports.service.issue.RepositoryIssueEvent;
-import com.novoda.github.reports.service.issue.RepositoryIssueEventComment;
 import com.novoda.github.reports.service.issue.RepositoryIssueEventEvent;
 import com.novoda.github.reports.service.network.DateToISO8601Converter;
 import com.novoda.github.reports.service.network.PagedTransformer;
@@ -32,7 +30,6 @@ import com.novoda.github.reports.service.persistence.converter.Converter;
 import com.novoda.github.reports.service.persistence.converter.EventConverter;
 import com.novoda.github.reports.service.persistence.converter.IssueConverter;
 import com.novoda.github.reports.service.persistence.converter.UserConverter;
-import com.novoda.github.reports.batch.pullrequest.PullRequestServiceClient;
 import com.novoda.github.reports.service.repository.GithubRepository;
 
 import java.util.Arrays;
@@ -51,7 +48,6 @@ public class IssuesServiceClient {
     private static final int DEFAULT_PER_PAGE_COUNT = 100;
     private static final int FIRST_PAGE = 1;
     private static final GithubIssue.State DEFAULT_STATE = GithubIssue.State.ALL;
-    private static final Date NO_SINCE_DATE = null;
 
     private static final Set<GithubEvent.Type> EVENT_TYPES_TO_BE_STORED = new HashSet<>(Arrays.asList(
             COMMENTED,
@@ -63,12 +59,10 @@ public class IssuesServiceClient {
     ));
 
     private final IssueService issueService;
-    private final PullRequestServiceClient pullRequestServiceClient;
     private final DateToISO8601Converter dateConverter;
 
     private final RateLimitDelayTransformer<GithubIssue> issueRateLimitDelayTransformer;
     private final RateLimitDelayTransformer<GithubEvent> eventRateLimitDelayTransformer;
-    private final RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer;
     private final RateLimitResetTimerSubject rateLimitResetTimerSubject;
 
     private final EventDataLayer eventDataLayer;
@@ -84,11 +78,8 @@ public class IssuesServiceClient {
         DateToISO8601Converter dateConverter = new DateToISO8601Converter();
         RateLimitDelayTransformer<GithubIssue> issueRateLimitDelayTransformer = RateLimitDelayTransformer.newInstance();
         RateLimitDelayTransformer<GithubEvent> eventRateLimitDelayTransformer = RateLimitDelayTransformer.newInstance();
-        RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer = RateLimitDelayTransformer.newInstance();
 
-        PullRequestServiceClient pullRequestServiceClient = PullRequestServiceClient.newInstance();
         ConnectionManager connectionManager = ConnectionManagerContainer.getConnectionManager();
-
         EventDataLayer eventDataLayer = DbEventDataLayer.newInstance(connectionManager);
         Converter<RepositoryIssue, Event> issueConverter = IssueConverter.newInstance();
         UserDataLayer userDataLayer = DbUserDataLayer.newInstance(connectionManager);
@@ -99,11 +90,9 @@ public class IssuesServiceClient {
         RateLimitResetTimerSubject rateLimitResetTimerSubject = RateLimitResetTimerSubjectContainer.getInstance();
 
         return new IssuesServiceClient(issueService,
-                                       pullRequestServiceClient,
                                        dateConverter,
                                        issueRateLimitDelayTransformer,
                                        eventRateLimitDelayTransformer,
-                                       commentRateLimitDelayTransformer,
                                        rateLimitResetTimerSubject,
                                        eventDataLayer,
                                        issueConverter,
@@ -113,16 +102,10 @@ public class IssuesServiceClient {
                                        eventConverter);
     }
 
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // TODO @RUI breakdown issue service client into issues, events, comments service clients
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
     private IssuesServiceClient(IssueService issueService,
-                               PullRequestServiceClient pullRequestServiceClient,
                                DateToISO8601Converter dateConverter,
                                RateLimitDelayTransformer<GithubIssue> issueRateLimitDelayTransformer,
                                RateLimitDelayTransformer<GithubEvent> eventRateLimitDelayTransformer,
-                               RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer,
                                RateLimitResetTimerSubject rateLimitResetTimerSubject,
                                EventDataLayer eventDataLayer,
                                Converter<RepositoryIssue, Event> issueConverter,
@@ -132,11 +115,9 @@ public class IssuesServiceClient {
                                Converter<RepositoryIssueEvent, Event> eventConverter) {
 
         this.issueService = issueService;
-        this.pullRequestServiceClient = pullRequestServiceClient;
         this.dateConverter = dateConverter;
         this.issueRateLimitDelayTransformer = issueRateLimitDelayTransformer;
         this.eventRateLimitDelayTransformer = eventRateLimitDelayTransformer;
-        this.commentRateLimitDelayTransformer = commentRateLimitDelayTransformer;
         this.rateLimitResetTimerSubject = rateLimitResetTimerSubject;
         this.eventDataLayer = eventDataLayer;
         this.issueConverter = issueConverter;
@@ -145,6 +126,10 @@ public class IssuesServiceClient {
         this.eventUserConverter = eventUserConverter;
         this.eventConverter = eventConverter;
     }
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // TODO @RUI breakdown issue service client into issues, events, comments service clients
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     public Observable<RepositoryIssue> retrieveIssuesFrom(GithubRepository repository, Date since) {
         return getPagedIssuesFor(repository.getOwnerUsername(), repository.getName(), since, FIRST_PAGE, DEFAULT_PER_PAGE_COUNT)
@@ -165,10 +150,6 @@ public class IssuesServiceClient {
         return issueService.getIssuesFor(organisation, repository, DEFAULT_STATE, date, page, pageCount)
                 .compose(issueRateLimitDelayTransformer)
                 .compose(PagedTransformer.newInstance(nextPage -> getPagedIssuesFor(organisation, repository, since, nextPage, pageCount)));
-    }
-
-    public Observable<RepositoryIssueEvent> retrieveEventsFrom(RepositoryIssue repositoryIssue) {
-        return retrieveEventsFrom(repositoryIssue, NO_SINCE_DATE);
     }
 
     public Observable<RepositoryIssueEvent> retrieveEventsFrom(RepositoryIssue repositoryIssue, Date since) {
@@ -201,57 +182,4 @@ public class IssuesServiceClient {
         return EVENT_TYPES_TO_BE_STORED.contains(event.getType());
     }
 
-    public Observable<RepositoryIssueEvent> retrieveCommentsFrom(RepositoryIssue repositoryIssue, Date since) {
-        return Observable.merge(
-                        retrieveCommentsFromIssue(repositoryIssue, since),
-                        retrieveReviewCommentsFromPullRequest(repositoryIssue, since))
-                .map(comment -> RepositoryIssueEventComment.newInstance(repositoryIssue, comment))
-                .compose(PersistEventUserTransformer.newInstance(userDataLayer, eventUserConverter))
-                .compose(PersistEventTransformer.newInstance(eventDataLayer, eventConverter));
-    }
-
-    private Observable<GithubComment> retrieveCommentsFromIssue(RepositoryIssue repositoryIssue, Date since) {
-        String organisation = repositoryIssue.getOwnerUsername();
-        String repository = repositoryIssue.getRepositoryName();
-        int issueNumber = repositoryIssue.getIssueNumber();
-        return getPagedCommentsFor(organisation, repository, issueNumber, since, FIRST_PAGE, DEFAULT_PER_PAGE_COUNT)
-                .flatMapIterable(Response::body)
-                .compose(RetryWhenTokenResets.newInstance(rateLimitResetTimerSubject));
-    }
-
-    private Observable<Response<List<GithubComment>>> getPagedCommentsFor(String organisation,
-                                                                          String repository,
-                                                                          Integer issueNumber,
-                                                                          Date since,
-                                                                          Integer page,
-                                                                          Integer pageCount) {
-
-        String date = dateConverter.toISO8601NoMillisOrNull(since);
-        return issueService.getCommentsFor(organisation, repository, issueNumber, date, page, pageCount)
-                .compose(commentRateLimitDelayTransformer)
-                .compose(PagedTransformer.newInstance(nextPage -> getPagedCommentsFor(
-                        organisation,
-                        repository,
-                        issueNumber,
-                        since,
-                        nextPage,
-                        pageCount
-                )));
-    }
-
-    private Observable<GithubComment> retrieveReviewCommentsFromPullRequest(RepositoryIssue repositoryIssue, Date since) {
-        if (isNotPullRequest(repositoryIssue)) {
-            return Observable.empty();
-        }
-        String organisation = repositoryIssue.getOwnerUsername();
-        String repository = repositoryIssue.getRepositoryName();
-        int issueNumber = repositoryIssue.getIssueNumber();
-        return pullRequestServiceClient
-                .getPullRequestReviewCommentsFor(organisation, repository, issueNumber, since)
-                .compose(RetryWhenTokenResets.newInstance(rateLimitResetTimerSubject));
-    }
-
-    private boolean isNotPullRequest(RepositoryIssue repositoryIssue) {
-        return !repositoryIssue.isPullRequest();
-    }
 }
