7cb046455fe8bca5b8dda42cfa9c586a2e02b877
takecare <rui@vazteixeira.org>
2016-06-10 14:06:36 +0100

    rename static fields for consistency

--- reports-batch-local/src/main/java/com/novoda/github/reports/batch/BatchClient.java
+++ reports-batch-local/src/main/java/com/novoda/github/reports/batch/BatchClient.java
@@ -15,10 +15,10 @@ import rx.Observable;
 
 class BatchClient {
 
-    private static final IssuesServiceClient issueServiceClient = IssuesServiceClient.newInstance();
-    private static final RepositoriesServiceClient repositoriesServiceClient = RepositoriesServiceClient.newInstance();
-    private static final CommentsServiceClient commentsServiceClient = CommentsServiceClient.newInstance();
-    private static final EventsServiceClient eventServiceClient = EventsServiceClient.newInstance();
+    private static final IssuesServiceClient ISSUE_SERVICE_CLIENT = IssuesServiceClient.newInstance();
+    private static final RepositoriesServiceClient REPOSITORY_SERVICE_CLIENT = RepositoriesServiceClient.newInstance();
+    private static final CommentsServiceClient COMMENTS_SERVICE_CLIENT = CommentsServiceClient.newInstance();
+    private static final EventsServiceClient EVENTS_SERVICE_CLIENT = EventsServiceClient.newInstance();
 
     private BatchClient() {
         // non-instantiable
@@ -33,22 +33,22 @@ class BatchClient {
 
     private static Observable.Transformer<? super String, ? extends GithubRepository> retrieveRepositoriesFromOrganizations() {
         return organizationObservable ->
-                organizationObservable.flatMap(repositoriesServiceClient::retrieveRepositoriesFrom);
+                organizationObservable.flatMap(REPOSITORY_SERVICE_CLIENT::retrieveRepositoriesFrom);
     }
 
     private static Observable.Transformer<? super GithubRepository, ? extends RepositoryIssue> retrieveIssuesFromRepositories(Date since) {
         return repositoryObservable ->
-                repositoryObservable.flatMap(repository -> issueServiceClient.retrieveIssuesFrom(repository, since));
+                repositoryObservable.flatMap(repository -> ISSUE_SERVICE_CLIENT.retrieveIssuesFrom(repository, since));
     }
 
     private static Observable.Transformer<? super RepositoryIssue, ? extends RepositoryIssueEvent> retrieveEventsFromIssues(Date since) {
         return repositoryIssueObservable -> {
 
             Observable<RepositoryIssueEvent> comments = repositoryIssueObservable
-                    .flatMap(repositoryIssue -> commentsServiceClient.retrieveCommentsFrom(repositoryIssue, since));
+                    .flatMap(repositoryIssue -> COMMENTS_SERVICE_CLIENT.retrieveCommentsFrom(repositoryIssue, since));
 
             Observable<RepositoryIssueEvent> events = repositoryIssueObservable
-                    .flatMap(repositoryIssue -> eventServiceClient.retrieveEventsFrom(repositoryIssue, since));
+                    .flatMap(repositoryIssue -> EVENTS_SERVICE_CLIENT.retrieveEventsFrom(repositoryIssue, since));
 
             return Observable.merge(comments, events);
         };
