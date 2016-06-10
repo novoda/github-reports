85131649b5a0e8c12f5f61885af97f63718ae041
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-10 15:28:30 +0100

    Improve and fix AmazonQueueMessageConverter from and to SQS Message

--- aws/src/main/java/com/novoda/github/reports/aws/queue/AmazonRawQueueMessage.java
+++ aws/src/main/java/com/novoda/github/reports/aws/queue/AmazonRawQueueMessage.java
@@ -22,6 +22,40 @@ public class AmazonRawQueueMessage {
 
     public boolean isTerminal;
 
+    static AmazonRawQueueMessage create(Type type,
+                                        Long page,
+                                        boolean isTerminal,
+                                        Date since,
+                                        String organisationName,
+                                        String repositoryName,
+                                        Long repositoryId,
+                                        Long issueNumber) {
+
+        return new AmazonRawQueueMessage(type, page, isTerminal, since, organisationName, repositoryName, repositoryId, issueNumber);
+    }
+
+    AmazonRawQueueMessage() {
+        // default
+    }
+
+    private AmazonRawQueueMessage(Type type,
+                                  Long page,
+                                  boolean isTerminal,
+                                  Date since,
+                                  String organisationName,
+                                  String repositoryName,
+                                  Long repositoryId,
+                                  Long issueNumber) {
+        this.type = type;
+        this.page = page;
+        this.isTerminal = isTerminal;
+        this.since = since;
+        this.organisationName = organisationName;
+        this.repositoryName = repositoryName;
+        this.repositoryId = repositoryId;
+        this.issueNumber = issueNumber;
+    }
+
     public Type getType() {
         return type;
     }
@@ -82,4 +116,52 @@ public class AmazonRawQueueMessage {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        AmazonRawQueueMessage that = (AmazonRawQueueMessage) o;
+
+        if (isTerminal != that.isTerminal) {
+            return false;
+        }
+        if (type != that.type) {
+            return false;
+        }
+        if (organisationName != null ? !organisationName.equals(that.organisationName) : that.organisationName != null) {
+            return false;
+        }
+        if (repositoryName != null ? !repositoryName.equals(that.repositoryName) : that.repositoryName != null) {
+            return false;
+        }
+        if (repositoryId != null ? !repositoryId.equals(that.repositoryId) : that.repositoryId != null) {
+            return false;
+        }
+        if (since != null ? !since.equals(that.since) : that.since != null) {
+            return false;
+        }
+        if (issueNumber != null ? !issueNumber.equals(that.issueNumber) : that.issueNumber != null) {
+            return false;
+        }
+        return page != null ? page.equals(that.page) : that.page == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type != null ? type.hashCode() : 0;
+        result = 31 * result + (organisationName != null ? organisationName.hashCode() : 0);
+        result = 31 * result + (repositoryName != null ? repositoryName.hashCode() : 0);
+        result = 31 * result + (repositoryId != null ? repositoryId.hashCode() : 0);
+        result = 31 * result + (since != null ? since.hashCode() : 0);
+        result = 31 * result + (issueNumber != null ? issueNumber.hashCode() : 0);
+        result = 31 * result + (page != null ? page.hashCode() : 0);
+        result = 31 * result + (isTerminal ? 1 : 0);
+        return result;
+    }
 }
