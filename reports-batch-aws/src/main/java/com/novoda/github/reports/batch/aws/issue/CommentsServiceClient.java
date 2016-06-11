5f741546ea23a80eba0fbdbeb83a6b4d77e8ebad
takecare <rui@vazteixeira.org>
2016-06-11 16:16:13 +0100

    add comments service client

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/issue/CommentsServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/issue/CommentsServiceClient.java
@@ -1,4 +1,41 @@
 package com.novoda.github.reports.batch.aws.issue;
 
+import com.novoda.github.reports.service.issue.GithubComment;
+import com.novoda.github.reports.service.issue.GithubIssueService;
+import com.novoda.github.reports.service.issue.IssueService;
+import com.novoda.github.reports.service.issue.RepositoryIssue;
+import com.novoda.github.reports.service.network.DateToISO8601Converter;
+
+import java.util.Date;
+
+import retrofit2.Response;
+import rx.Observable;
+
 public class CommentsServiceClient {
+
+    private static final int DEFAULT_PER_PAGE_COUNT = 100;
+
+    private final IssueService issueService;
+    private final DateToISO8601Converter dateConverter;
+
+    public static CommentsServiceClient newInstance() {
+        IssueService issueService = GithubIssueService.newInstance();
+                DateToISO8601Converter dateConverter = new DateToISO8601Converter();
+        return new CommentsServiceClient(issueService, dateConverter);
+    }
+
+    private CommentsServiceClient(IssueService issueService, DateToISO8601Converter dateConverter) {
+        this.issueService = issueService;
+        this.dateConverter = dateConverter;
+    }
+
+    public Observable<GithubComment> retrieveCommentsFrom(RepositoryIssue repositoryIssue, Date since, int page) {
+        String organisation = repositoryIssue.getOwnerUsername();
+        String repository = repositoryIssue.getRepositoryName();
+        int issueNumber = repositoryIssue.getIssueNumber();
+        String date = dateConverter.toISO8601NoMillisOrNull(since);
+        return issueService.getCommentsFor(organisation, repository, issueNumber, date, page, DEFAULT_PER_PAGE_COUNT)
+                .flatMapIterable(Response::body);
+    }
+
 }
