529acc73eaf26bdccaad49daab3d8c3d9db461c8
takecare <rui@vazteixeira.org>
2016-06-13 12:12:32 +0100

    use issue persister in aws batch

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/issue/IssuesServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/issue/IssuesServiceClient.java
@@ -3,7 +3,9 @@ package com.novoda.github.reports.batch.aws.issue;
 import com.novoda.github.reports.service.issue.GithubIssue;
 import com.novoda.github.reports.service.issue.GithubIssueService;
 import com.novoda.github.reports.service.issue.IssueService;
+import com.novoda.github.reports.service.issue.RepositoryIssue;
 import com.novoda.github.reports.service.network.DateToISO8601Converter;
+import com.novoda.github.reports.service.persistence.IssuePersister;
 import com.novoda.github.reports.service.repository.GithubRepository;
 
 import java.util.Date;
@@ -18,24 +20,29 @@ public class IssuesServiceClient {
 
     private final IssueService issueService;
     private final DateToISO8601Converter dateConverter;
+    private final IssuePersister issuePersister;
 
     public static IssuesServiceClient newInstance() {
         IssueService issueService = GithubIssueService.newInstance();
         DateToISO8601Converter dateConverter = new DateToISO8601Converter();
-        return new IssuesServiceClient(issueService, dateConverter);
+        IssuePersister issuePersister = IssuePersister.newInstance();
+        return new IssuesServiceClient(issueService, dateConverter, issuePersister);
     }
 
-    private IssuesServiceClient(IssueService issueService, DateToISO8601Converter dateConverter) {
+    private IssuesServiceClient(IssueService issueService, DateToISO8601Converter dateConverter, IssuePersister issuePersister) {
         this.issueService = issueService;
         this.dateConverter = dateConverter;
+        this.issuePersister = issuePersister;
     }
 
-    public Observable<GithubIssue> retrieveIssuesFrom(GithubRepository repository, Date since, int page) {
+    public Observable<RepositoryIssue> retrieveIssuesFrom(GithubRepository repository, Date since, int page) {
         String organisation = repository.getOwnerUsername();
         String repositoryName = repository.getName();
         String date = dateConverter.toISO8601NoMillisOrNull(since);
         return issueService.getIssuesFor(organisation, repositoryName, DEFAULT_STATE, date, page, DEFAULT_PER_PAGE_COUNT)
-                .flatMapIterable(Response::body);
+                .flatMapIterable(Response::body)
+                .map(issue -> new RepositoryIssue(repository, issue))
+                .compose(issuePersister);
     }
 
 }
