7e9781ca22778a368bc25e13f37058a9dde6c219
Francesco Pontillo <francescopontillo@gmail.com>
2016-06-14 16:04:55 +0100

    Add recipients to email configurations

--- aws/src/main/java/com/novoda/github/reports/aws/configuration/EmailNotifierConfiguration.java
+++ aws/src/main/java/com/novoda/github/reports/aws/configuration/EmailNotifierConfiguration.java
@@ -3,21 +3,31 @@ package com.novoda.github.reports.aws.configuration;
 import com.google.auto.value.AutoValue;
 import com.novoda.github.reports.aws.properties.EmailCredentialsReader;
 
+import java.util.List;
+
 @AutoValue
 public abstract class EmailNotifierConfiguration implements NotifierConfiguration {
 
-    public static EmailNotifierConfiguration create(String host, String port, boolean useSsl, String from, String username, String password) {
-        return new AutoValue_EmailNotifierConfiguration(host, port, useSsl, from, username, password);
+    public static EmailNotifierConfiguration create(String host,
+                                                    String port,
+                                                    boolean useSsl,
+                                                    String from,
+                                                    String username,
+                                                    String password,
+                                                    List<String> to) {
+
+        return new AutoValue_EmailNotifierConfiguration(host, port, useSsl, from, username, password, to);
     }
 
-    public static EmailNotifierConfiguration create(EmailCredentialsReader emailCredentialsReader) {
+    public static EmailNotifierConfiguration create(EmailCredentialsReader emailCredentialsReader, List<String> to) {
         return create(
                 emailCredentialsReader.getHost(),
                 emailCredentialsReader.getPort(),
                 emailCredentialsReader.useSsl(),
                 emailCredentialsReader.getFrom(),
                 emailCredentialsReader.getUsername(),
-                emailCredentialsReader.getPassword()
+                emailCredentialsReader.getPassword(),
+                to
         );
     }
 
@@ -32,5 +42,7 @@ public abstract class EmailNotifierConfiguration implements NotifierConfiguratio
     abstract String username();
 
     abstract String password();
+
+    abstract List<String> to();
 
 }
