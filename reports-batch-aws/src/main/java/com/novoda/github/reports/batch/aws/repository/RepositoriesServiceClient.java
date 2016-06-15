faf647bf7128bc2e33e27a282e6b20e7b6395f3c
takecare <rui@vazteixeira.org>
2016-06-15 14:08:04 +0100

    handle next messages: next pages and get issues

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/repository/RepositoriesServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/repository/RepositoriesServiceClient.java
@@ -1,63 +1,113 @@
 package com.novoda.github.reports.batch.aws.repository;
 
+import com.novoda.github.reports.aws.queue.AmazonGetIssuesQueueMessage;
+import com.novoda.github.reports.aws.queue.AmazonGetRepositoriesQueueMessage;
+import com.novoda.github.reports.aws.queue.AmazonQueueMessage;
 import com.novoda.github.reports.batch.aws.persistence.PersistOperator;
 import com.novoda.github.reports.data.RepoDataLayer;
 import com.novoda.github.reports.data.db.ConnectionManager;
 import com.novoda.github.reports.data.db.DbRepoDataLayer;
 import com.novoda.github.reports.data.model.Repository;
+import com.novoda.github.reports.service.network.LastPageExtractor;
+import com.novoda.github.reports.service.network.NextPageExtractor;
 import com.novoda.github.reports.service.persistence.ConnectionManagerContainer;
-import com.novoda.github.reports.service.persistence.PersistRepositoryTransformer;
 import com.novoda.github.reports.service.persistence.converter.Converter;
 import com.novoda.github.reports.service.persistence.converter.RepositoryConverter;
-import com.novoda.github.reports.service.repository.GithubRepositoryService;
 import com.novoda.github.reports.service.repository.GithubRepository;
+import com.novoda.github.reports.service.repository.GithubRepositoryService;
 import com.novoda.github.reports.service.repository.RepositoryService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import retrofit2.Response;
 import rx.Observable;
+import rx.functions.Func1;
 
 public class RepositoriesServiceClient {
 
     private static final int DEFAULT_PER_PAGE_COUNT = 100;
 
     private final RepositoryService repositoryService;
-    private final PersistRepositoryTransformer persistRepositoryTransformer;
-
     private final RepoDataLayer repoDataLayer;
     private final Converter<GithubRepository, Repository> converter;
+
+    private final NextPageExtractor nextPageExtractor;
+    private final LastPageExtractor lastPageExtractor;
 
     public static RepositoriesServiceClient newInstance() {
         RepositoryService repositoriesService = GithubRepositoryService.newInstance();
         ConnectionManager connectionManager = ConnectionManagerContainer.getConnectionManager();
         RepoDataLayer repoDataLayer = DbRepoDataLayer.newInstance(connectionManager);
         Converter<GithubRepository, Repository> converter = RepositoryConverter.newInstance();
-        PersistRepositoryTransformer persistRepositoryTransformer = PersistRepositoryTransformer.newInstance(repoDataLayer, converter);
-        return new RepositoriesServiceClient(repositoriesService, persistRepositoryTransformer, repoDataLayer, converter);
+        NextPageExtractor nextPageExtractor = NextPageExtractor.newInstance();
+        LastPageExtractor lastPageExtractor = LastPageExtractor.newInstance();
+        return new RepositoriesServiceClient(repositoriesService, repoDataLayer, converter, nextPageExtractor, lastPageExtractor);
     }
 
     private RepositoriesServiceClient(RepositoryService repositoryService,
-                                      PersistRepositoryTransformer persistRepositoryTransformer,
                                       RepoDataLayer repoDataLayer,
-                                      Converter<GithubRepository, Repository> converter) {
+                                      Converter<GithubRepository, Repository> converter,
+                                      NextPageExtractor nextPageExtractor,
+                                      LastPageExtractor lastPageExtractor) {
 
         this.repositoryService = repositoryService;
-        this.persistRepositoryTransformer = persistRepositoryTransformer;
         this.repoDataLayer = repoDataLayer;
         this.converter = converter;
-    }
-
-    public Observable<GithubRepository> getRepositoriesFor(String organisation, int page) {
-        return repositoryService.getRepositoriesFor(organisation, page, DEFAULT_PER_PAGE_COUNT)
-                .flatMapIterable(Response::body)
-                .compose(persistRepositoryTransformer);
-
+        this.nextPageExtractor = nextPageExtractor;
+        this.lastPageExtractor = lastPageExtractor;
     }
 
     public Observable<Response<List<GithubRepository>>> getRepositoriesResponseFor(String organisation, int page) {
         return repositoryService.getRepositoriesFor(organisation, page, DEFAULT_PER_PAGE_COUNT)
-                .lift(new PersistOperator<>(repoDataLayer, converter)); // no need to buffer 'cause we already have a list...
+                .lift(new PersistOperator<>(repoDataLayer, converter));
+    }
+
+    public Observable<AmazonQueueMessage> getRepositoriesFor(AmazonGetRepositoriesQueueMessage message) {
+        return repositoryService
+                .getRepositoriesFor(message.organisationName(), Math.toIntExact(message.page()), DEFAULT_PER_PAGE_COUNT)
+                .lift(new PersistOperator<>(repoDataLayer, converter))
+                .flatMap(new Func1<Response<List<GithubRepository>>, Observable<AmazonQueueMessage>>() {
+                    @Override
+                    public Observable<AmazonQueueMessage> call(Response<List<GithubRepository>> response) {
+                        List<AmazonQueueMessage> messages = new ArrayList<>();
+
+                        if (message.localTerminal()) {
+                            Optional<Integer> nextPage = nextPageExtractor.getNextPageFrom(response);
+                            Optional<Integer> lastPage = lastPageExtractor.getLastPageFrom(response);
+
+                            if (nextPage.isPresent()) {
+                                int next = nextPage.get();
+                                int last = lastPage.get();
+
+                                for (int i = next; i <= last; i++) {
+                                    messages.add(
+                                            AmazonGetRepositoriesQueueMessage.create(
+                                                    i != last,
+                                                    (long) i,
+                                                    message.receiptHandle(),
+                                                    message.organisationName(),
+                                                    message.sinceOrNull()
+                                            ));
+                                }
+                            }
+                        }
+
+                        List<GithubRepository> repositories = response.body();
+                        repositories.forEach(repository -> messages.add(AmazonGetIssuesQueueMessage.create(
+                                true,
+                                1L,
+                                message.receiptHandle(),
+                                message.organisationName(),
+                                message.sinceOrNull(),
+                                repository.getId(),
+                                repository.getName()
+                        )));
+
+                        return Observable.from(messages);
+                    }
+                });
     }
 
 }
