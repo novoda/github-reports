377f1de2c4fba654e3b07cfc281a8f01e3a42d1c
takecare <rui@vazteixeira.org>
2016-06-15 10:38:10 +0100

    persist repos but return the original response

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/repository/RepositoriesServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/repository/RepositoriesServiceClient.java
@@ -1,5 +1,6 @@
 package com.novoda.github.reports.batch.aws.repository;
 
+import com.novoda.github.reports.batch.aws.persistence.PersistOperator;
 import com.novoda.github.reports.data.RepoDataLayer;
 import com.novoda.github.reports.data.db.ConnectionManager;
 import com.novoda.github.reports.data.db.DbRepoDataLayer;
@@ -12,6 +13,8 @@ import com.novoda.github.reports.service.repository.GithubRepositoryService;
 import com.novoda.github.reports.service.repository.GithubRepository;
 import com.novoda.github.reports.service.repository.RepositoryService;
 
+import java.util.List;
+
 import retrofit2.Response;
 import rx.Observable;
 
@@ -22,24 +25,39 @@ public class RepositoriesServiceClient {
     private final RepositoryService repositoryService;
     private final PersistRepositoryTransformer persistRepositoryTransformer;
 
+    private final RepoDataLayer repoDataLayer;
+    private final Converter<GithubRepository, Repository> converter;
+
     public static RepositoriesServiceClient newInstance() {
         RepositoryService repositoriesService = GithubRepositoryService.newInstance();
         ConnectionManager connectionManager = ConnectionManagerContainer.getConnectionManager();
         RepoDataLayer repoDataLayer = DbRepoDataLayer.newInstance(connectionManager);
         Converter<GithubRepository, Repository> converter = RepositoryConverter.newInstance();
         PersistRepositoryTransformer persistRepositoryTransformer = PersistRepositoryTransformer.newInstance(repoDataLayer, converter);
-        return new RepositoriesServiceClient(repositoriesService, persistRepositoryTransformer);
+        return new RepositoriesServiceClient(repositoriesService, persistRepositoryTransformer, repoDataLayer, converter);
     }
 
-    private RepositoriesServiceClient(RepositoryService repositoryService, PersistRepositoryTransformer persistRepositoryTransformer) {
+    private RepositoriesServiceClient(RepositoryService repositoryService,
+                                      PersistRepositoryTransformer persistRepositoryTransformer,
+                                      RepoDataLayer repoDataLayer,
+                                      Converter<GithubRepository, Repository> converter) {
+
         this.repositoryService = repositoryService;
         this.persistRepositoryTransformer = persistRepositoryTransformer;
+        this.repoDataLayer = repoDataLayer;
+        this.converter = converter;
     }
 
     public Observable<GithubRepository> getRepositoriesFor(String organisation, int page) {
         return repositoryService.getRepositoriesFor(organisation, page, DEFAULT_PER_PAGE_COUNT)
                 .flatMapIterable(Response::body)
                 .compose(persistRepositoryTransformer);
+
+    }
+
+    public Observable<Response<List<GithubRepository>>> getRepositoriesResponseFor(String organisation, int page) {
+        return repositoryService.getRepositoriesFor(organisation, page, DEFAULT_PER_PAGE_COUNT)
+                .lift(new PersistOperator<>(repoDataLayer, converter)); // no need to buffer 'cause we already have a list...
 
     }
 
