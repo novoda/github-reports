06cc2400029d0c718c2f9ae7b41e844b0558ee42
takecare <rui@vazteixeira.org>
2016-06-17 15:14:52 +0100

    remove old code that used the pr service client

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/issue/ReviewCommentsServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/issue/ReviewCommentsServiceClient.java
@@ -2,31 +2,34 @@ package com.novoda.github.reports.batch.aws.issue;
 
 import com.novoda.github.reports.aws.queue.AmazonGetReviewCommentsQueueMessage;
 import com.novoda.github.reports.aws.queue.AmazonQueueMessage;
-import com.novoda.github.reports.batch.aws.pullrequest.PullRequestServiceClient;
+import com.novoda.github.reports.aws.queue.QueueMessage;
+import com.novoda.github.reports.service.issue.GithubComment;
 import com.novoda.github.reports.service.issue.RepositoryIssue;
 import com.novoda.github.reports.service.issue.RepositoryIssueEvent;
-import com.novoda.github.reports.service.issue.RepositoryIssueEventComment;
+import com.novoda.github.reports.service.network.DateToISO8601Converter;
 import com.novoda.github.reports.service.persistence.RepositoryIssueEventPersistTransformer;
+import com.novoda.github.reports.service.pullrequest.PullRequestService;
 
 import java.util.Date;
+import java.util.List;
 
+import retrofit2.Response;
 import rx.Observable;
 
 public class ReviewCommentsServiceClient {
 
-    private final PullRequestServiceClient pullRequestServiceClient;
+    private static final int DEFAULT_PER_PAGE_COUNT = 100;
+
+    private final PullRequestService pullRequestService;
+    private final DateToISO8601Converter dateConverter;
     private final RepositoryIssueEventPersistTransformer repositoryIssueEventPersistTransformer;
 
-    public static ReviewCommentsServiceClient newInstance() {
-        PullRequestServiceClient pullRequestServiceClient = PullRequestServiceClient.newInstance();
-        RepositoryIssueEventPersistTransformer repositoryIssueEventPersistTransformer = RepositoryIssueEventPersistTransformer.newInstance();
-        return new ReviewCommentsServiceClient(pullRequestServiceClient, repositoryIssueEventPersistTransformer);
-    }
+    public ReviewCommentsServiceClient(PullRequestService pullRequestService,
+                                       DateToISO8601Converter dateConverter,
+                                       RepositoryIssueEventPersistTransformer repositoryIssueEventPersistTransformer) {
 
-    private ReviewCommentsServiceClient(PullRequestServiceClient pullRequestServiceClient,
-                                        RepositoryIssueEventPersistTransformer repositoryIssueEventPersistTransformer) {
-
-        this.pullRequestServiceClient = pullRequestServiceClient;
+        this.pullRequestService = pullRequestService;
+        this.dateConverter = dateConverter;
         this.repositoryIssueEventPersistTransformer = repositoryIssueEventPersistTransformer;
     }
 
@@ -38,19 +41,44 @@ public class ReviewCommentsServiceClient {
         String organisation = repositoryIssue.getOwnerUsername();
         String repository = repositoryIssue.getRepositoryName();
         int issueNumber = repositoryIssue.getIssueNumber();
-        return pullRequestServiceClient.getPullRequestReviewCommentsFor(organisation, repository, issueNumber, since, page)
-                .map(comment -> new RepositoryIssueEventComment(repositoryIssue, comment))
-                .compose(repositoryIssueEventPersistTransformer);
-    }
-
-    public Observable<AmazonQueueMessage> retrieveReviewCommentsFromPullRequest(AmazonGetReviewCommentsQueueMessage message) {
-        // TODO
+        //return pullRequestServiceClient.getPullRequestReviewCommentsFor(organisation, repository, issueNumber, since, page)
+        //        .map(comment -> new RepositoryIssueEventComment(repositoryIssue, comment))
+        //        .compose(repositoryIssueEventPersistTransformer);
         return Observable.empty();
     }
-
 
     private boolean isNotPullRequest(RepositoryIssue repositoryIssue) {
         return !repositoryIssue.isPullRequest();
     }
+
+    public Observable<AmazonQueueMessage> retrieveReviewCommentsFromPullRequest(AmazonGetReviewCommentsQueueMessage message) {
+        // TODO
+
+        String date = dateConverter.toISO8601NoMillisOrNull(message.sinceOrNull());
+        Observable o =
+                pullRequestService.getPullRequestReviewCommentsFor(
+                        message.organisationName(),
+                        message.repositoryName(),
+                        issueNumberFrom(message),
+                        date,
+                        pageFrom(message),
+                        DEFAULT_PER_PAGE_COUNT
+                )
+                //        .compose(new TransformToRepositoryIssueEvent())
+                //.compose(ResponseRepositoryIssuePersistTransformer.newInstance())
+                //.compose(NextMessagesReviewCommentsTransformer.newInstance(message))
+                ;
+
+        return Observable.empty();
+    }
+
+    private int pageFrom(QueueMessage message) {
+        return Math.toIntExact(message.page());
+    }
+
+    private int issueNumberFrom(AmazonGetReviewCommentsQueueMessage message) {
+        return Math.toIntExact(message.issueNumber());
+    }
+
 
 }
