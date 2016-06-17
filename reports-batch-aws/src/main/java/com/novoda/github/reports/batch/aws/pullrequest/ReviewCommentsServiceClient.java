0bb8043cd586529ab1fa89640b22fb11dcf45cea
takecare <rui@vazteixeira.org>
2016-06-17 15:56:21 +0100

    remove old, useless code

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/pullrequest/ReviewCommentsServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/pullrequest/ReviewCommentsServiceClient.java
@@ -3,13 +3,8 @@ package com.novoda.github.reports.batch.aws.pullrequest;
 import com.novoda.github.reports.aws.queue.AmazonGetReviewCommentsQueueMessage;
 import com.novoda.github.reports.aws.queue.AmazonQueueMessage;
 import com.novoda.github.reports.aws.queue.QueueMessage;
-import com.novoda.github.reports.service.issue.RepositoryIssue;
-import com.novoda.github.reports.service.issue.RepositoryIssueEvent;
 import com.novoda.github.reports.service.network.DateToISO8601Converter;
-import com.novoda.github.reports.service.persistence.RepositoryIssueEventPersistTransformer;
 import com.novoda.github.reports.service.pullrequest.PullRequestService;
-
-import java.util.Date;
 
 import rx.Observable;
 
@@ -19,33 +14,10 @@ public class ReviewCommentsServiceClient {
 
     private final PullRequestService pullRequestService;
     private final DateToISO8601Converter dateConverter;
-    private final RepositoryIssueEventPersistTransformer repositoryIssueEventPersistTransformer;
-
-    public ReviewCommentsServiceClient(PullRequestService pullRequestService,
-                                       DateToISO8601Converter dateConverter,
-                                       RepositoryIssueEventPersistTransformer repositoryIssueEventPersistTransformer) {
-
+    
+    ReviewCommentsServiceClient(PullRequestService pullRequestService, DateToISO8601Converter dateConverter) {
         this.pullRequestService = pullRequestService;
         this.dateConverter = dateConverter;
-        this.repositoryIssueEventPersistTransformer = repositoryIssueEventPersistTransformer;
-    }
-
-    @Deprecated
-    public Observable<RepositoryIssueEvent> retrieveReviewCommentsFromPullRequest(RepositoryIssue repositoryIssue, Date since, int page) {
-        if (isNotPullRequest(repositoryIssue)) {
-            return Observable.empty();
-        }
-        String organisation = repositoryIssue.getOwnerUsername();
-        String repository = repositoryIssue.getRepositoryName();
-        int issueNumber = repositoryIssue.getIssueNumber();
-        //return pullRequestServiceClient.getPullRequestReviewCommentsFor(organisation, repository, issueNumber, since, page)
-        //        .map(comment -> new RepositoryIssueEventComment(repositoryIssue, comment))
-        //        .compose(repositoryIssueEventPersistTransformer);
-        return Observable.empty();
-    }
-
-    private boolean isNotPullRequest(RepositoryIssue repositoryIssue) {
-        return !repositoryIssue.isPullRequest();
     }
 
     public Observable<AmazonQueueMessage> retrieveReviewCommentsFromPullRequest(AmazonGetReviewCommentsQueueMessage message) {
