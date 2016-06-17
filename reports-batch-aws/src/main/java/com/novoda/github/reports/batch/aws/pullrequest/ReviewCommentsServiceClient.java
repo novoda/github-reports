0293403749ddc28dcda79bbcbd08cd413e303022
takecare <rui@vazteixeira.org>
2016-06-17 15:40:24 +0100

    move TransformToRepositoryIssueEvent to its own class

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/pullrequest/ReviewCommentsServiceClient.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/pullrequest/ReviewCommentsServiceClient.java
@@ -3,22 +3,14 @@ package com.novoda.github.reports.batch.aws.pullrequest;
 import com.novoda.github.reports.aws.queue.AmazonGetReviewCommentsQueueMessage;
 import com.novoda.github.reports.aws.queue.AmazonQueueMessage;
 import com.novoda.github.reports.aws.queue.QueueMessage;
-import com.novoda.github.reports.service.issue.GithubComment;
-import com.novoda.github.reports.service.issue.GithubIssue;
 import com.novoda.github.reports.service.issue.RepositoryIssue;
 import com.novoda.github.reports.service.issue.RepositoryIssueEvent;
-import com.novoda.github.reports.service.issue.RepositoryIssueEventComment;
 import com.novoda.github.reports.service.network.DateToISO8601Converter;
 import com.novoda.github.reports.service.persistence.RepositoryIssueEventPersistTransformer;
 import com.novoda.github.reports.service.pullrequest.PullRequestService;
-import com.novoda.github.reports.service.repository.GithubRepository;
 
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import okhttp3.Headers;
-import retrofit2.Response;
 import rx.Observable;
 
 public class ReviewCommentsServiceClient {
@@ -83,32 +75,6 @@ public class ReviewCommentsServiceClient {
 
     private int issueNumberFrom(AmazonGetReviewCommentsQueueMessage message) {
         return Math.toIntExact(message.issueNumber());
-    }
-
-    class TransformToRepositoryIssueEvent implements Observable.Transformer<Response<List<GithubComment>>, Response<List<RepositoryIssueEvent>>> {
-
-        private long repositoryId;
-        private int issueNumber;
-
-        TransformToRepositoryIssueEvent(AmazonGetReviewCommentsQueueMessage message) {
-            this.repositoryId = message.repositoryId();
-            this.issueNumber = Math.toIntExact(message.issueNumber());
-        }
-
-        @Override
-        public Observable<Response<List<RepositoryIssueEvent>>> call(Observable<Response<List<GithubComment>>> responseObservable) {
-            return responseObservable.map(response -> {
-                Headers headers = response.headers();
-                List<RepositoryIssueEvent> body = response.body().stream()
-                        .map(githubComment -> new RepositoryIssueEventComment(getRepositoryIssue(), githubComment))
-                        .collect(Collectors.toList());
-                return Response.success(body, headers);
-            });
-        }
-
-        private RepositoryIssue getRepositoryIssue() {
-            return new RepositoryIssue(new GithubRepository(repositoryId), new GithubIssue(issueNumber));
-        }
     }
 
 }
