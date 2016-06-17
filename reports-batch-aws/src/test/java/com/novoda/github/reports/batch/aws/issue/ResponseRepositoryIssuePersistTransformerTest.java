ae987305d859507cf52a24d16e8cbdec5ee357d3
takecare <rui@vazteixeira.org>
2016-06-17 17:20:44 +0100

    fix test: add missing param in ctor

--- reports-batch-aws/src/test/java/com/novoda/github/reports/batch/aws/issue/ResponseRepositoryIssuePersistTransformerTest.java
+++ reports-batch-aws/src/test/java/com/novoda/github/reports/batch/aws/issue/ResponseRepositoryIssuePersistTransformerTest.java
@@ -39,8 +39,8 @@ public class ResponseRepositoryIssuePersistTransformerTest {
     public void setUp() {
         initMocks(this);
 
-        RepositoryIssue aRepositoryIssue = new RepositoryIssue(new GithubRepository(1L), new GithubIssue());
-        RepositoryIssue anotherRepositoryIssue = new RepositoryIssue(new GithubRepository(2L), new GithubIssue());
+        RepositoryIssue aRepositoryIssue = new RepositoryIssue(new GithubRepository(1L), new GithubIssue(1);
+        RepositoryIssue anotherRepositoryIssue = new RepositoryIssue(new GithubRepository(2L), new GithubIssue(2));
 
         List<RepositoryIssue> list = Arrays.asList(aRepositoryIssue, anotherRepositoryIssue);
         response = Response.success(list);
