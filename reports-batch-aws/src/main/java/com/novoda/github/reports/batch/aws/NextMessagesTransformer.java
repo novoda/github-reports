d0830d0388f664f29a721a66cabd71597445a482
takecare <rui@vazteixeira.org>
2016-06-17 11:22:23 +0100

    use constsnts

--- reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/NextMessagesTransformer.java
+++ reports-batch-aws/src/main/java/com/novoda/github/reports/batch/aws/NextMessagesTransformer.java
@@ -15,6 +15,9 @@ import rx.functions.Func1;
 public abstract class NextMessagesTransformer<T, M extends AmazonQueueMessage>
         implements Observable.Transformer<Response<List<T>>, AmazonQueueMessage> {
 
+    protected static final long FIRST_PAGE = 1L;
+    protected static final boolean ALWAYS_TERMINAL_MESSAGE = true;
+
     private final NextPageExtractor nextPageExtractor;
     private final LastPageExtractor lastPageExtractor;
 
