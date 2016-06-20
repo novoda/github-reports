4dffa877dbe27bb861dceeb92356d284111c9cd4
takecare <rui@vazteixeira.org>
2016-06-20 17:30:18 +0100

    make oauth token interceptor support other api tokens as before it was only supporting the github oauth token (hardcoded)

--- service/src/test/java/com/novoda/github/reports/service/network/OAuthTokenInterceptorTest.java
+++ service/src/test/java/com/novoda/github/reports/service/network/OAuthTokenInterceptorTest.java
@@ -3,7 +3,6 @@ package com.novoda.github.reports.service.network;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.stubbing.Answer;
 
 import okhttp3.Interceptor;
 import okhttp3.Protocol;
@@ -18,11 +17,12 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class OAuthTokenInterceptorTest {
 
     private static final String ANY_TOKEN = "h4x0r";
+    private static final String ANY_TOKEN_PREFIX = "pre";
     private static final String ANY_URL = "http://google.pt";
     private static final Protocol ANY_PROTOCOL = Protocol.HTTP_1_1;
     private static final int ANY_STATUS_CODE = 200;
 
-    private OAuthTokenInterceptor interceptor = new OAuthTokenInterceptor(ANY_TOKEN);
+    private OAuthTokenInterceptor interceptor = new OAuthTokenInterceptor(ANY_TOKEN_PREFIX, ANY_TOKEN);
 
     @Mock
     Interceptor.Chain mockChain;
@@ -35,7 +35,7 @@ public class OAuthTokenInterceptorTest {
         when(mockChain.request()).thenReturn(oldRequest);
 
         when(mockChain.proceed(any(Request.class))).thenAnswer(
-                (Answer<Response>) invocation -> {
+                invocation -> {
                     Request request = (Request) invocation.getArguments()[0];
                     return new Response.Builder()
                             .protocol(ANY_PROTOCOL)
@@ -51,6 +51,6 @@ public class OAuthTokenInterceptorTest {
         Response response = interceptor.intercept(mockChain);
 
         String actual = response.request().headers().get("Authorization");
-        assertEquals("token " + ANY_TOKEN, actual);
+        assertEquals(ANY_TOKEN_PREFIX + " " + ANY_TOKEN, actual);
     }
 }
