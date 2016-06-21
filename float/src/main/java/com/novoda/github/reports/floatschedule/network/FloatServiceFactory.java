449742963c9cde3f19c72bd40430da618501f6c6
takecare <rui@vazteixeira.org>
2016-06-21 11:40:43 +0100

    add factory methods to build FloatServiceFactory instances

--- float/src/main/java/com/novoda/github/reports/floatschedule/network/FloatServiceFactory.java
+++ float/src/main/java/com/novoda/github/reports/floatschedule/network/FloatServiceFactory.java
@@ -1,5 +1,7 @@
 package com.novoda.github.reports.floatschedule.network;
 
+import com.novoda.github.reports.network.HttpClientFactory;
+import com.novoda.github.reports.network.OkHttpClientFactory;
 import com.novoda.github.reports.network.ServiceFactory;
 
 import okhttp3.OkHttpClient;
@@ -9,6 +11,23 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class FloatServiceFactory extends ServiceFactory<FloatApiService> {
 
     private static final String FLOAT_ENDPOINT = "https://api.float.com/api/v1/";
+
+    public static FloatServiceFactory newInstance() {
+        HttpClientFactory httpClientFactory = OkHttpClientFactory.newInstance();
+        return newInstance(httpClientFactory);
+    }
+
+    public static FloatServiceFactory newCachingInstance() {
+        HttpClientFactory httpClientFactory = OkHttpClientFactory.newCachingInstance();
+        return newInstance(httpClientFactory);
+    }
+
+    private static FloatServiceFactory newInstance(HttpClientFactory httpClientFactory) {
+        OkHttpClient okHttpClient = httpClientFactory.createClient();
+        GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create();
+        RxJavaCallAdapterFactory rxJavaCallAdapterFactory = RxJavaCallAdapterFactory.create();
+        return new FloatServiceFactory(okHttpClient, gsonConverterFactory, rxJavaCallAdapterFactory);
+    }
 
     private FloatServiceFactory(OkHttpClient okHttpClient,
                                 GsonConverterFactory gsonConverterFactory,
