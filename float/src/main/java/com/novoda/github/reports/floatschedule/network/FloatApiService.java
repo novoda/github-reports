c5cf67388bebb38dc0e55f357b7875b24e4c5966
takecare <rui@vazteixeira.org>
2016-06-21 16:05:32 +0100

    fix return type of query as we're now using a 'container' class: People

--- float/src/main/java/com/novoda/github/reports/floatschedule/network/FloatApiService.java
+++ float/src/main/java/com/novoda/github/reports/floatschedule/network/FloatApiService.java
@@ -3,8 +3,6 @@ package com.novoda.github.reports.floatschedule.network;
 import com.novoda.github.reports.floatschedule.people.People;
 import com.novoda.github.reports.floatschedule.project.Projects;
 
-import java.util.List;
-
 import retrofit2.Response;
 import retrofit2.http.GET;
 import rx.Observable;
@@ -12,7 +10,7 @@ import rx.Observable;
 public interface FloatApiService {
 
     @GET("people")
-    Observable<Response<List<People>>> getPeople();
+    Observable<Response<People>> getPeople();
 
     @GET("projects")
     Observable<Response<Projects>> getProjects();
