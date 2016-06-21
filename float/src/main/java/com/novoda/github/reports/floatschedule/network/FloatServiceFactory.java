23dd3c2164966a940fee6fcbf4b124e670d219fc
takecare <rui@vazteixeira.org>
2016-06-21 11:35:24 +0100

    make FloatServiceFactory extend ServiceFactory

--- float/src/main/java/com/novoda/github/reports/floatschedule/network/FloatServiceFactory.java
+++ float/src/main/java/com/novoda/github/reports/floatschedule/network/FloatServiceFactory.java
@@ -1,24 +1,30 @@
 package com.novoda.github.reports.floatschedule.network;
 
+import com.novoda.github.reports.network.ServiceFactory;
+
 import okhttp3.OkHttpClient;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class FloatServiceFactory {
+public class FloatServiceFactory extends ServiceFactory<FloatApiService> {
 
     private static final String FLOAT_ENDPOINT = "https://api.float.com/api/v1/";
 
-    private final OkHttpClient okHttpClient;
-    private final GsonConverterFactory gsonConverterFactory;
-    private final RxJavaCallAdapterFactory rxJavaCallAdapterFactory;
+    private FloatServiceFactory(OkHttpClient okHttpClient,
+                                GsonConverterFactory gsonConverterFactory,
+                                RxJavaCallAdapterFactory rxJavaCallAdapterFactory) {
 
-    public FloatServiceFactory(OkHttpClient okHttpClient,
-                               GsonConverterFactory gsonConverterFactory,
-                               RxJavaCallAdapterFactory rxJavaCallAdapterFactory) {
+        super(okHttpClient, gsonConverterFactory, rxJavaCallAdapterFactory);
+    }
 
-        this.okHttpClient = okHttpClient;
-        this.gsonConverterFactory = gsonConverterFactory;
-        this.rxJavaCallAdapterFactory = rxJavaCallAdapterFactory;
+    @Override
+    protected Class<FloatApiService> getServiceClass() {
+        return FloatApiService.class;
+    }
+
+    @Override
+    protected String getBaseEndpoint() {
+        return FLOAT_ENDPOINT;
     }
 
 }
