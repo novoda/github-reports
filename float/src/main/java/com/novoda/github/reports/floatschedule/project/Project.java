77ff7d6130841dc467d6ca02a52c67ef7c09bedd
takecare <rui@vazteixeira.org>
2016-06-21 14:19:00 +0100

    add project name field to project pojo

--- float/src/main/java/com/novoda/github/reports/floatschedule/project/Project.java
+++ float/src/main/java/com/novoda/github/reports/floatschedule/project/Project.java
@@ -1,4 +1,12 @@
 package com.novoda.github.reports.floatschedule.project;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Project {
+
+    @Expose
+    @SerializedName("project_name")
+    private String projectName;
+
 }
