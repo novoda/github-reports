4d3a1641d77084cf525e13c40dc9a6094384245f
takecare <rui@vazteixeira.org>
2016-06-22 10:06:01 +0100

    add methods to debug/test teh service clients

--- float/src/main/java/com/novoda/github/reports/floatschedule/Main.java
+++ float/src/main/java/com/novoda/github/reports/floatschedule/Main.java
@@ -1,11 +1,90 @@
 package com.novoda.github.reports.floatschedule;
 
+import com.novoda.github.reports.floatschedule.people.PeopleServiceClient;
+import com.novoda.github.reports.floatschedule.people.Person;
+import com.novoda.github.reports.floatschedule.project.Project;
+import com.novoda.github.reports.floatschedule.project.ProjectServiceClient;
+import com.novoda.github.reports.floatschedule.task.Task;
+import com.novoda.github.reports.floatschedule.task.TaskServiceClient;
+
+import rx.Subscriber;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("DONE");
+        //getPersons();
+        //getProjects();
+        getTasks();
 
+        System.out.println("... DONE!");
+
+    }
+
+    private static void getTasks() {
+        TaskServiceClient taskServiceClient = TaskServiceClient.newInstance();
+        taskServiceClient.getTasks()
+                .toBlocking()
+                .subscribe(new Subscriber<Task>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println(">>> COMPLETED");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println(">>> ERROR: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Task task) {
+                        System.out.println(">>> TASK: " + task);
+                    }
+                });
+    }
+
+    private static void getProjects() {
+        ProjectServiceClient projectServiceClient = ProjectServiceClient.newInstance();
+        projectServiceClient.getProjects()
+                .toBlocking()
+                .subscribe(new Subscriber<Project>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println(">>> COMPLETED");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println(">>> ERROR: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Project project) {
+                        System.out.println(">>> PROJECT: " + project);
+                    }
+                });
+    }
+
+    private static void getPersons() {
+        PeopleServiceClient peopleServiceClient = PeopleServiceClient.newInstance();
+        peopleServiceClient.getPersons()
+                .toBlocking()
+                .subscribe(new Subscriber<Person>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println(">>> COMPLETED");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println(">>> ERROR: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Person person) {
+                        System.out.println(">>> PERSON: " + person);
+                    }
+                });
     }
 
 }
