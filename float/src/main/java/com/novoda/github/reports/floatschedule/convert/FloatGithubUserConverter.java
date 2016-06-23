94aeb929c48a7715425382f89181f05db855d4ed
takecare <rui@vazteixeira.org>
2016-06-23 15:45:27 +0100

    implement calls to get float/github usernames

--- float/src/main/java/com/novoda/github/reports/floatschedule/convert/FloatGithubUserConverter.java
+++ float/src/main/java/com/novoda/github/reports/floatschedule/convert/FloatGithubUserConverter.java
@@ -1,12 +1,47 @@
 package com.novoda.github.reports.floatschedule.convert;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Locale;
+import java.util.Map;
+
+import org.jetbrains.annotations.Nullable;
+
 public class FloatGithubUserConverter {
 
     private final JsonMapReader jsonMapReader;
+    private Map<String, String> floatToGithubUsers;
 
-    public FloatGithubUserConverter(JsonMapReader jsonMapReader) {
+    FloatGithubUserConverter(JsonMapReader jsonMapReader) {
         this.jsonMapReader = jsonMapReader;
     }
 
+    @Nullable
+    public String getFloatUser(String githubUsername) throws IOException {
+        readIfNeeded();
+        final String[] match = { null };
+        floatToGithubUsers.forEach((floatName, githubName) -> {
+            if (githubName.equalsIgnoreCase(githubUsername)) {
+                match[0] = floatName;
+            }
+        });
+        return match[0];
+    }
 
+    private void readIfNeeded() throws IOException {
+        if (!floatToGithubUsers.isEmpty()) {
+            return;
+        }
+        try {
+            floatToGithubUsers = jsonMapReader.readFromResource("users.json");
+        } catch (URISyntaxException | IOException e) {
+            throw new IOException("Could not read users from file.");
+        }
+    }
+
+    @Nullable
+    public String getGithubUser(String floatName) throws IOException {
+        readIfNeeded();
+        return floatToGithubUsers.get(floatName.toLowerCase(Locale.UK));
+    }
 }
