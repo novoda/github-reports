dcfd428c918ba70a553c1bb6f53d3aee5ed39136
takecare <rui@vazteixeira.org>
2016-06-23 15:48:01 +0100

    add float <-> github project/repo converter

---
+++ float/src/main/java/com/novoda/github/reports/floatschedule/convert/FloatGithubProjectConverter.java
@@ -0,0 +1,12 @@
+package com.novoda.github.reports.floatschedule.convert;
+
+public class FloatGithubProjectConverter {
+
+    private final JsonMapReader jsonMapReader;
+
+    FloatGithubProjectConverter(JsonMapReader jsonMapReader) {
+        this.jsonMapReader = jsonMapReader;
+    }
+
+    // TODO
+}
