15a949eb646fffa6dbee8d0e4e9db58f694a52fa
takecare <rui@vazteixeira.org>
2016-06-23 15:44:45 +0100

    add github <-> float username converter

---
+++ float/src/main/java/com/novoda/github/reports/floatschedule/convert/FloatGithubUserConverter.java
@@ -0,0 +1,12 @@
+package com.novoda.github.reports.floatschedule.convert;
+
+public class FloatGithubUserConverter {
+
+    private final JsonMapReader jsonMapReader;
+
+    public FloatGithubUserConverter(JsonMapReader jsonMapReader) {
+        this.jsonMapReader = jsonMapReader;
+    }
+
+
+}
