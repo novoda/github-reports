e40f37153bd4b7566ffb08b5691df5add7f5593f
takecare <rui@vazteixeira.org>
2016-06-28 10:29:59 +0100

    add a couple more tests around float service client

--- float/src/test/java/com/novoda/github/reports/floatschedule/FloatServiceClientTest.java
+++ float/src/test/java/com/novoda/github/reports/floatschedule/FloatServiceClientTest.java
@@ -2,12 +2,15 @@ package com.novoda.github.reports.floatschedule;
 
 import com.novoda.github.reports.floatschedule.convert.FloatGithubProjectConverter;
 import com.novoda.github.reports.floatschedule.convert.FloatGithubUserConverter;
+import com.novoda.github.reports.floatschedule.convert.NoMatchFoundException;
 import com.novoda.github.reports.floatschedule.people.PeopleServiceClient;
 import com.novoda.github.reports.floatschedule.people.Person;
 import com.novoda.github.reports.floatschedule.task.Task;
 import com.novoda.github.reports.floatschedule.task.TaskServiceClient;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -18,6 +21,8 @@ import rx.Observable;
 import rx.observers.TestSubscriber;
 import rx.schedulers.Schedulers;
 
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -26,7 +31,12 @@ public class FloatServiceClientTest {
 
     private static final String ANY_START_DATE = "";
     private static final int ANY_NUMBER_OF_WEEKS = 0;
-    private static final int ANY_PERSON_ID = 0;
+    private static final int ANY_PERSON_ID = 23;
+    private static final String ANY_FLOAT_USERNAME = "imbecil";
+    private static final String ANY_GITHUB_USERNAME = "palerma";
+    private static final String ANY_FLOAT_PROJECT_NAME = "palerma";
+    private static final String ANY_GITHUB_REPO_NAME = "lixo";
+    private static final String ANY_OTHER_GITHUB_REPO_NAME = "nojo";
 
     @Mock
     FloatGithubUserConverter mockFloatGithubUserConverter;
@@ -52,12 +62,14 @@ public class FloatServiceClientTest {
 
         givenPersons();
 
-        when(mockFloatGithubUserConverter.getGithubUser("floatUsername")).thenReturn("githubName");
-        when(mockFloatGithubProjectConverter.getRepositories("floatProject")).thenReturn(Arrays.asList("repo1", "repo2"));
+        when(mockFloatGithubUserConverter.getGithubUser(ANY_FLOAT_USERNAME)).thenReturn(ANY_GITHUB_USERNAME);
+        when(mockFloatGithubUserConverter.getFloatUser(ANY_GITHUB_USERNAME)).thenReturn(ANY_FLOAT_USERNAME);
+        when(mockFloatGithubProjectConverter.getRepositories(ANY_FLOAT_PROJECT_NAME)).thenReturn(Arrays.asList(ANY_GITHUB_REPO_NAME,
+                                                                                                               ANY_OTHER_GITHUB_REPO_NAME));
     }
 
     private void givenTasks(String startDate, int numberOfWeeks, int personId) {
-        List<Task> tasks = Arrays.asList(givenATask("a"), givenATask("b"), givenATask("c"), givenATask("floatProject"));
+        List<Task> tasks = Arrays.asList(givenATask("a"), givenATask("b"), givenATask("c"), givenATask(ANY_FLOAT_PROJECT_NAME));
         Observable<Task> mockTasksObservable = Observable.from(tasks);
         when(mockTaskServiceClient.getTasks(startDate, numberOfWeeks, personId)).thenReturn(mockTasksObservable);
     }
@@ -69,7 +81,7 @@ public class FloatServiceClientTest {
     }
 
     private void givenPersons() {
-        List<Person> persons = Arrays.asList(givenAPerson(1, "um"), givenAPerson(2, "dois"), givenAPerson(3, "floatUsername"));
+        List<Person> persons = Arrays.asList(givenAPerson(1, "um"), givenAPerson(2, "dois"), givenAPerson(ANY_PERSON_ID, ANY_FLOAT_USERNAME));
         Observable<Person> mockPersonsObservable = Observable.from(persons);
         when(mockPeopleServiceClient.getPersons()).thenReturn(mockPersonsObservable);
     }
@@ -81,21 +93,42 @@ public class FloatServiceClientTest {
         return aPerson;
     }
 
-    // what do we want to assert?
-    // . repo names are matched properly and emitted
-    // . nothing emitted when there are no matches
-    // . the tasks have to be in the date range provided
-
     @Test
-    public void givenPersonsAndTask_whenGettingRepositoryNamesForFloatUser_thenTheExpectedRepositoryNamesAreEmitted() {
-        givenTasks(ANY_START_DATE, ANY_NUMBER_OF_WEEKS, 3);
+    public void givenPersonsAndTasks_whenGettingRepositoryNamesForFloatUser_thenTheExpectedRepositoryNamesAreEmitted() {
+        givenTasks(ANY_START_DATE, ANY_NUMBER_OF_WEEKS, ANY_PERSON_ID);
 
         TestSubscriber<String> testSubscriber = new TestSubscriber<>();
-        floatServiceClient.getRepositoryNamesForFloatUser("floatUsername", ANY_START_DATE, ANY_NUMBER_OF_WEEKS)
+        floatServiceClient.getRepositoryNamesForFloatUser(ANY_FLOAT_USERNAME, ANY_START_DATE, ANY_NUMBER_OF_WEEKS)
                 .subscribeOn(Schedulers.immediate())
                 .subscribe(testSubscriber);
 
-        testSubscriber.assertReceivedOnNext(Arrays.asList("repo1", "repo2"));
+        testSubscriber.assertReceivedOnNext(Arrays.asList(ANY_GITHUB_REPO_NAME, ANY_OTHER_GITHUB_REPO_NAME));
     }
 
+    @Test
+    public void givenPersonsAndTasks_whenGettingRepositoryNamesForGithubUserWithNoTasksAssigned_thenNoItemsAreEmitted() {
+        Observable<Task> mockTasksObservable = Observable.from(Collections.emptyList());
+        when(mockTaskServiceClient.getTasks(anyString(), anyInt(), anyInt())).thenReturn(mockTasksObservable);
+
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        floatServiceClient.getRepositoryNamesForFloatUser(ANY_FLOAT_USERNAME, ANY_START_DATE, ANY_NUMBER_OF_WEEKS)
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertNoValues();
+    }
+
+    @Test
+    public void givenPersonsAndTasks_whenGettingRepositoryNamesForGithubUser_thenTheExpectedRepositoryNamesAreEmitted()
+            throws IOException, NoMatchFoundException {
+
+        givenTasks(ANY_START_DATE, ANY_NUMBER_OF_WEEKS, ANY_PERSON_ID);
+
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        floatServiceClient.getRepositoryNamesForGithubUser(ANY_GITHUB_USERNAME, ANY_START_DATE, ANY_NUMBER_OF_WEEKS)
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertReceivedOnNext(Arrays.asList(ANY_GITHUB_REPO_NAME, ANY_OTHER_GITHUB_REPO_NAME));
+    }
 }
