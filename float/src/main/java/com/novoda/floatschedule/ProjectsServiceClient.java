13db36e968591c6b0d604c6ec2aaf0c48cd72606
takecare <rui@vazteixeira.org>
2016-06-29 15:20:41 +0100

    get all github repo names for a list of float projects

--- float/src/main/java/com/novoda/floatschedule/ProjectsServiceClient.java
+++ float/src/main/java/com/novoda/floatschedule/ProjectsServiceClient.java
@@ -3,6 +3,8 @@ package com.novoda.floatschedule;
 import com.novoda.floatschedule.reader.ProjectsReader;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import rx.Observable;
 
@@ -22,6 +24,22 @@ class ProjectsServiceClient {
         try {
             readIfNeeded();
             return Observable.from(projectsReader.getContent().keySet());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Observable.empty();
+    }
+
+    Observable<String> getAllGithubRepositoryNames(List<String> floatProjectNames) {
+        try {
+            readIfNeeded();
+
+            List<String> repositoryNames = floatProjectNames.stream()
+                    .flatMap(floatProjectName -> projectsReader.getContent().get(floatProjectName).stream())
+                    .distinct()
+                    .collect(Collectors.toList());
+
+            return Observable.from(repositoryNames);
         } catch (IOException e) {
             e.printStackTrace();
         }
