d826794a55c89d59efe9c11b79cc1fc7e7f8418e
takecare <rui@vazteixeira.org>
2016-06-29 15:20:41 +0100

    get all github repo names for a list of float projects

--- float/src/test/java/com/novoda/floatschedule/ProjectsServiceClientTest.java
+++ float/src/test/java/com/novoda/floatschedule/ProjectsServiceClientTest.java
@@ -3,6 +3,7 @@ package com.novoda.floatschedule;
 import com.novoda.floatschedule.reader.ProjectsReader;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,19 +34,20 @@ public class ProjectsServiceClientTest {
         initMocks(this);
         Map<String, List<String>> content = new HashMap<>(1);
         content.put("projecto", Arrays.asList("fazer nenhum", "olhar para o tecto"));
-        content.put("trabalho", Arrays.asList("faz de conta"));
+        content.put("trabalho", Collections.singletonList("faz de conta"));
+        content.put("empreendimento", Arrays.asList("olhar para o tecto", "produzir zero"));
         when(mockProjectsReader.getContent()).thenReturn(content);
     }
 
     @Test
-    public void givenFileWithProjectsAndRepositories_whenGettingAllTheFloatProjectNames_thenTheyAreEmitted() throws Exception {
+    public void givenThereIsContent_whenGettingAllTheFloatProjectNames_thenTheyAreEmitted() throws Exception {
 
         TestSubscriber<String> testSubscriber = new TestSubscriber<>();
         projectsServiceClient.getAllFloatProjectNames()
                 .subscribeOn(Schedulers.immediate())
                 .subscribe(testSubscriber);
 
-        testSubscriber.assertValues("trabalho", "projecto");
+        testSubscriber.assertValues("empreendimento", "trabalho", "projecto");
     }
 
     @Test
@@ -62,6 +64,46 @@ public class ProjectsServiceClientTest {
         when(mockProjectsReader.hasContent()).thenReturn(true);
 
         projectsServiceClient.getAllFloatProjectNames();
+
+        verify(mockProjectsReader, VerificationModeFactory.times(0)).read();
+    }
+
+    @Test
+    public void givenThereIsContent_whenGettingRepositoryNamesForFloatProjects_thenTheNamesAreEmitted() throws Exception {
+
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        projectsServiceClient.getAllGithubRepositoryNames(Collections.singletonList("trabalho"))
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertValues("faz de conta");
+    }
+
+    @Test
+    public void givenThereIsContent_whenGettingRepositoryNamesForFloatProjects_thenTheNamesAreEmittedWithoutDuplicates() throws Exception {
+
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        projectsServiceClient.getAllGithubRepositoryNames(Arrays.asList("projecto", "empreendimento"))
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertValues("fazer nenhum", "olhar para o tecto", "produzir zero");
+    }
+
+    @Test
+    public void givenThereIsNoContent_whenGettingRepositoryNamesForFloatProjects_thenTheContentIsRead() throws Exception {
+        when(mockProjectsReader.hasContent()).thenReturn(false);
+
+        projectsServiceClient.getAllGithubRepositoryNames(Collections.singletonList("empreendimento"));
+
+        verify(mockProjectsReader).read();
+    }
+
+    @Test
+    public void givenThereIsNoContent_whenGettingRepositoryNamesForFloatProjects_thenTheContentIsNotReadAgain() throws Exception {
+        when(mockProjectsReader.hasContent()).thenReturn(true);
+
+        projectsServiceClient.getAllGithubRepositoryNames(Collections.singletonList("empreendimento"));
 
         verify(mockProjectsReader, VerificationModeFactory.times(0)).read();
     }
