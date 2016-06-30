13841138c638ed834dc78ebf4d40eff1380f5d45
takecare <rui@vazteixeira.org>
2016-06-30 17:02:57 +0100

    add getGithubUsernamesAssignedToRepositories

--- float/src/main/java/com/novoda/floatschedule/AssignmentServiceClient.java
+++ float/src/main/java/com/novoda/floatschedule/AssignmentServiceClient.java
@@ -1,11 +1,14 @@
 package com.novoda.floatschedule;
 
+import com.novoda.floatschedule.convert.FloatGithubProjectConverter;
 import com.novoda.floatschedule.convert.FloatGithubUserConverter;
 import com.novoda.floatschedule.task.Task;
 import com.novoda.floatschedule.task.TaskServiceClient;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import rx.Observable;
 import rx.functions.Func1;
@@ -18,15 +21,37 @@ public class AssignmentServiceClient {
 
     private final TaskServiceClient taskServiceClient;
     private final FloatGithubUserConverter floatGithubUserConverter;
+    private final FloatGithubProjectConverter floatGithubProjectConverter;
 
-    AssignmentServiceClient(TaskServiceClient taskServiceClient, FloatGithubUserConverter floatGithubUserConverter) {
+    AssignmentServiceClient(TaskServiceClient taskServiceClient,
+                            FloatGithubUserConverter floatGithubUserConverter,
+                            FloatGithubProjectConverter floatGithubProjectConverter) {
+
         this.taskServiceClient = taskServiceClient;
         this.floatGithubUserConverter = floatGithubUserConverter;
+        this.floatGithubProjectConverter = floatGithubProjectConverter;
     }
 
     public Observable<String> getGithubUsernamesAssignedToRepositories(List<String> repositoryNames) {
-        // TODO
-        return Observable.empty();
+        List<String> floatProjectNames = getFloatProjectNamesFrom(repositoryNames);
+        return getGithubUsernamesAssignedToProjects(floatProjectNames);
+    }
+
+    private List<String> getFloatProjectNamesFrom(List<String> repositoryNames) {
+        return repositoryNames.stream()
+                .map(this::getFloatProjectName)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private Optional<String> getFloatProjectName(String repositoryName) {
+        try {
+            return Optional.of(floatGithubProjectConverter.getFloatProject(repositoryName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
     }
 
     public Observable<String> getGithubUsernamesAssignedToProjects(List<String> floatProjectNames) {
