c115f38c16aaff9554c6bb77ab87376156520806
takecare <rui@vazteixeira.org>
2016-06-30 15:59:08 +0100

    add more tests around getting githuber usernames for float projects

--- float/src/test/java/com/novoda/floatschedule/AssignmentServiceClientTest.java
+++ float/src/test/java/com/novoda/floatschedule/AssignmentServiceClientTest.java
@@ -25,8 +25,6 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 public class AssignmentServiceClientTest {
 
-    private static final String ANY_START_DATE = "YYYY-MM-dd";
-    private static final int ANY_NUMBER_OF_WEEKS = 88;
     private static final Integer NO_PERSON_ID = null;
 
     @Mock
@@ -41,19 +39,16 @@ public class AssignmentServiceClientTest {
     @Before
     public void setUp() throws Exception {
         initMocks(this);
-        givenTasks();
-        givenGithubUser("gitA", "persA");
-        givenGithubUser("gitB", "persB");
-        givenGithubUser("gitC", "persC");
-        givenGithubUser("gitD", "persD");
+
+        List<Task> tasks = Arrays.asList(givenATask("proj1", "persA"), givenATask("proj1", "persD"), givenATask("proj2", "persB"),
+                                         givenATask("proj3", "persC"), givenATask("proj4", "persA"));
+
+        givenTasks(tasks);
+        givenGithubUsersFor(tasks);
     }
 
-    private void givenTasks() {
-        List<Task> tasks = Arrays.asList(givenATask("proj1", "persA"),
-                                         givenATask("proj1", "persD"),
-                                         givenATask("proj2", "persB"),
-                                         givenATask("proj3", "persC"),
-                                         givenATask("proj4", "persA"));
+    private void givenTasks(List<Task> tasks) {
+
         Observable<Task> mockTasksObservable = Observable.from(tasks);
         when(mockTaskServiceClient.getTasks(anyString(), anyInt(), eq(NO_PERSON_ID))).thenReturn(mockTasksObservable);
     }
@@ -65,19 +60,48 @@ public class AssignmentServiceClientTest {
         return aTask;
     }
 
-    private void givenGithubUser(String githubUsername, String floatUsername) throws IOException {
-        when(mockFloatGithubUserConverter.getGithubUser(floatUsername)).thenReturn(githubUsername);
+    private void givenGithubUsersFor(List<Task> tasks) {
+        tasks.forEach(task -> givenGithubUser(task.getPersonName() + "_github", task.getPersonName()));
+    }
+
+    private void givenGithubUser(String githubUsername, String floatUsername) {
+        try {
+            when(mockFloatGithubUserConverter.getGithubUser(floatUsername)).thenReturn(githubUsername);
+        } catch (IOException e) {
+            // nothing
+        }
     }
 
     @Test
-    public void getGithubUsernamesAssignedToProjects() throws Exception {
+    public void givenTasksAndUsers_whenGettingGithubUsernamesForAProject_thenTheCorrectUsernameIsEmitted()  {
 
         TestSubscriber<String> testSubscriber = new TestSubscriber<>();
         assignmentServiceClient.getGithubUsernamesAssignedToProjects(Collections.singletonList("proj2"))
                 .subscribeOn(Schedulers.immediate())
                 .subscribe(testSubscriber);
 
-        testSubscriber.assertValues("gitB");
+        testSubscriber.assertValues("persB_github");
     }
 
+    @Test
+    public void givenTasksAndUsers_whenGettingGithubUsernamesForProjects_thenTheCorrectUsernamesAreEmittedWithoutDuplicates() {
+
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        assignmentServiceClient.getGithubUsernamesAssignedToProjects(Arrays.asList("proj1", "proj4"))
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertValues("persA_github", "persD_github");
+    }
+
+    @Test
+    public void givenTasksAndUsers_whenGettingGithubUsernamesForANonExistentProject_thenTheNothingIsEmitted() {
+
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        assignmentServiceClient.getGithubUsernamesAssignedToProjects(Collections.singletonList("proj88"))
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertNoValues();
+    }
 }
