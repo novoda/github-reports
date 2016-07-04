f7e7cfb6f6319f0c92d2409c130a7aed6d19df9d
takecare <rui@vazteixeira.org>
2016-07-04 10:54:31 +0100

    simplify getGithubUser() and extract lambdas to methods so we can provide proper, explainatory names

--- float/src/main/java/com/novoda/floatschedule/convert/FloatGithubUserConverter.java
+++ float/src/main/java/com/novoda/floatschedule/convert/FloatGithubUserConverter.java
@@ -4,6 +4,7 @@ import com.novoda.floatschedule.reader.UsersReader;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class FloatGithubUserConverter {
@@ -20,28 +21,11 @@ public class FloatGithubUserConverter {
 
     public String getFloatUser(String githubUsername) throws IOException, NoMatchFoundException {
         readIfNeeded();
-
         return usersReader.getContent().entrySet().stream()
-                .filter(entry -> entry.getValue().equalsIgnoreCase(githubUsername))
+                .filter(byGithubUsername(githubUsername))
                 .findFirst()
                 .map(Map.Entry::getKey)
-                .orElseThrow((Supplier<RuntimeException>) () -> new NoMatchFoundException(githubUsername));
-    }
-
-    public String getGithubUser(String floatName) throws IOException, NoMatchFoundException {
-        readIfNeeded();
-        final String[] match = { null };
-        usersReader.getContent().entrySet()
-                .stream()
-                .filter(entry -> entry.getKey().equalsIgnoreCase(floatName))
-                .findFirst()
-                .ifPresent(entry -> match[0] = entry.getValue());
-
-        if (match[0] == null) {
-            throw new NoMatchFoundException(floatName);
-        }
-
-        return match[0];
+                .orElseThrow(createNoMatchFoundException(githubUsername));
     }
 
     private void readIfNeeded() throws IOException {
@@ -49,5 +33,27 @@ public class FloatGithubUserConverter {
             return;
         }
         usersReader.read();
+    }
+
+    private Predicate<Map.Entry<String, String>> byGithubUsername(String githubUsername) {
+        return entry -> entry.getValue().equalsIgnoreCase(githubUsername);
+    }
+
+    private Supplier<RuntimeException> createNoMatchFoundException(String username) {
+        return () -> new NoMatchFoundException(username);
+    }
+
+    public String getGithubUser(String floatName) throws IOException, NoMatchFoundException {
+        readIfNeeded();
+        return usersReader.getContent().entrySet()
+                .stream()
+                .filter(byFloatUsername(floatName))
+                .findFirst()
+                .map(Map.Entry::getValue)
+                .orElseThrow(createNoMatchFoundException(floatName));
+    }
+
+    private Predicate<Map.Entry<String, String>> byFloatUsername(String floatName) {
+        return entry -> entry.getKey().equalsIgnoreCase(floatName);
     }
 }
