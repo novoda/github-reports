b60eacdeac0f22ac815366566eb81fd2ca50d66d
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-06 18:24:06 +0100

    Retrieve organisation users from lambda

---
+++ web-service/src/main/java/com/novoda/github/reports/web/lambda/GetOrganisationUsersAction.java
@@ -0,0 +1,22 @@
+package com.novoda.github.reports.web.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.novoda.github.reports.reader.UsersServiceClient;
+
+import java.util.List;
+
+public class GetOrganisationUsersAction implements RequestHandler<Void, List<String>> {
+
+    private final UsersServiceClient userServiceClient;
+
+    public GetOrganisationUsersAction() {
+        userServiceClient = UsersServiceClient.newInstance();
+    }
+
+    @Override
+    public List<String> handleRequest(Void input, Context context) {
+        return userServiceClient.getAllGithubUsers().toList().toBlocking().first();
+    }
+
+}
