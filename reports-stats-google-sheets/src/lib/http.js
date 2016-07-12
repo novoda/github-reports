f3a4f617674a73544e9f721f067da7b7ed7da616
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-12 12:17:35 +0100

    Serialise query params into URL, encode URL before request

--- reports-stats-google-sheets/src/lib/http.js
+++ reports-stats-google-sheets/src/lib/http.js
@@ -3,10 +3,44 @@
 /* exported Http */
 function Http() {
 
-  Http.prototype.fetch = function(url, params) {
-    var response = UrlFetchApp.fetch(url, params || {});
+  Http.prototype.fetch = function(url, query) {
+    var encodedUrl = encodeURI(this.getUrlWithQuery(url, query));
+    var response = UrlFetchApp.fetch(encodedUrl);
     var json = response.getContentText();
     return JSON.parse(json);
   };
+
+  Http.prototype.getUrlWithQuery = function(url, query) {
+    var parts = Object
+      .keys(query || {})
+      .map(function(key) {
+        var original = query[key];
+        var value = original;
+
+        if (original instanceof Date) {
+          value = original.toISOString();
+        } else if (original instanceof Array) {
+          value = '[' + stringifyArray(original) + ']';
+        }
+
+        return key + '=' + value;
+      })
+      .join('&');
+    if (parts) {
+      parts = '?' + parts;
+    }
+    return url + parts;
+  };
+
+  function stringifyArray(values) {
+    return values
+      .map(function(value) {
+        if (typeof value === 'string') {
+          return '"' + value + '"';
+        }
+        return value;
+      })
+      .join(',');
+  }
 
 }
