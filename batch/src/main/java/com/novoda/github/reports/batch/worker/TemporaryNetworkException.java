e1672c78cebd968928d132e7c6241d54c85745f1
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-12 09:36:30 +0100

    Rename TemporaryNetworkException to RetriableNetworkException

--- batch/src/main/java/com/novoda/github/reports/batch/worker/TemporaryNetworkException.java
+++
@@ -1,9 +0,0 @@
-package com.novoda.github.reports.batch.worker;
-
-public class TemporaryNetworkException extends Exception {
-
-    public TemporaryNetworkException(Throwable cause) {
-        super(cause);
-    }
-
-}
