1f0b7093a78d4da370064cbe3e4521d2e70340a8
takecare <rui@vazteixeira.org>
2016-07-15 16:46:57 +0100

    rename weeks calculator to number of weeks calculator

--- float/src/main/java/com/novoda/floatschedule/convert/NumberOfWeeksCalculator.java
+++ float/src/main/java/com/novoda/floatschedule/convert/NumberOfWeeksCalculator.java
@@ -5,13 +5,13 @@ import java.time.Period;
 import java.util.Calendar;
 import java.util.Date;
 
-public class WeeksCalculator {
+public class NumberOfWeeksCalculator {
 
     private static final int NUMBER_OF_DAYS_IN_WEEK = 7;
 
-    public int getNumberOfWeeksIn(Date startDate, Date endDate) {
-        LocalDate start = getLocalDateFrom(startDate);
-        LocalDate end = getLocalDateFrom(endDate);
+    public int getNumberOfWeeksIn(Date startDateInclusive, Date endDateExlusive) {
+        LocalDate start = getLocalDateFrom(startDateInclusive);
+        LocalDate end = getLocalDateFrom(endDateExlusive);
         Period period = Period.between(start, end);
         return period.getDays() / NUMBER_OF_DAYS_IN_WEEK;
     }
