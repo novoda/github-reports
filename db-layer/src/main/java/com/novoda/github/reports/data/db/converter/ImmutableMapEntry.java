66b7780befe087236becb025bc64644aea285604
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-19 16:49:57 +0100

    Reduce visibility of ImmutableMapEntry

--- db-layer/src/main/java/com/novoda/github/reports/data/db/converter/ImmutableMapEntry.java
+++ db-layer/src/main/java/com/novoda/github/reports/data/db/converter/ImmutableMapEntry.java
@@ -2,7 +2,7 @@ package com.novoda.github.reports.data.db.converter;
 
 import java.util.Map;
 
-public abstract class ImmutableMapEntry<K, V> implements Map.Entry<K, V> {
+abstract class ImmutableMapEntry<K, V> implements Map.Entry<K, V> {
 
     @Override
     public V setValue(V value) {
