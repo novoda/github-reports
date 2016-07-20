f9c4aee6d830eea1a23b106aafac40287aac33dd
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-20 10:52:31 +0100

    Add conversion to Date, fix date format, add tests

--- float/src/main/java/com/novoda/floatschedule/convert/FloatDateConverter.java
+++ float/src/main/java/com/novoda/floatschedule/convert/FloatDateConverter.java
@@ -1,13 +1,32 @@
 package com.novoda.floatschedule.convert;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FloatDateConverter {
 
-    private static final SimpleDateFormat FLOAT_DATE_FORMAT = new SimpleDateFormat("YYYY-MM-dd");
+    private static final SimpleDateFormat FLOAT_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final Date NO_DATE = null;
 
     public String toFloatDateFormat(Date date) {
         return FLOAT_DATE_FORMAT.format(date);
     }
+
+    public Date fromFloatDateFormatOrNoDate(String date) {
+        try {
+            return fromFloatDateFormat(date);
+        } catch (InvalidFloatDateException e) {
+            return NO_DATE;
+        }
+    }
+
+    Date fromFloatDateFormat(String date) throws InvalidFloatDateException {
+        try {
+            return FLOAT_DATE_FORMAT.parse(date);
+        } catch (ParseException e) {
+            throw new InvalidFloatDateException(date, e);
+        }
+    }
+
 }
