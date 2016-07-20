3f259e453dd1d2f2b8665e6165960f330dbf77fa
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-20 10:52:31 +0100

    Add conversion to Date, fix date format, add tests

--- float/src/test/java/com/novoda/floatschedule/convert/FloatDateConverterTest.java
+++ float/src/test/java/com/novoda/floatschedule/convert/FloatDateConverterTest.java
@@ -1,15 +1,21 @@
 package com.novoda.floatschedule.convert;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import java.util.Calendar;
 import java.util.Date;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class FloatDateConverterTest {
 
     private FloatDateConverter dateConverter = new FloatDateConverter();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void givenADate_whenConvertingToFloatFormat_thenItConvertsAppropriately() throws Exception {
@@ -21,7 +27,7 @@ public class FloatDateConverterTest {
     }
 
     @Test
-    public void givenAnotherDate_whenConvertingToFloatFormat_thenItConvertsAppropriately() throws Exception {
+    public void givenNonExistingDateInFebruary_whenConvertingToFloatFormat_thenItShiftsDaysToMarchAndConvertsAppropriately() throws Exception {
         Date date = givenADate(1994, Calendar.FEBRUARY, 30);
 
         String actual = dateConverter.toFloatDateFormat(date);
@@ -29,8 +35,37 @@ public class FloatDateConverterTest {
         assertEquals("1994-03-02", actual);
     }
 
+    @Test
+    public void givenValidDateAsString_whenConvertingFromFloatFormat_thenItConvertsAppropriately() throws InvalidFloatDateException {
+        String date = "2016-07-20";
+
+        Date actual = dateConverter.fromFloatDateFormat(date);
+
+        Date expected = givenADate(2016, 6, 20);
+
+        assertEquals(0, expected.compareTo(actual));
+    }
+
+    @Test
+    public void givenInvalidDateAsString_whenConvertingFromFloat_thenItThrowsInvalidFloatDateException() throws InvalidFloatDateException {
+        String date = "clearly-invalid-date-lol";
+
+        expectedException.expect(InvalidFloatDateException.class);
+        dateConverter.fromFloatDateFormat(date);
+    }
+
+    @Test
+    public void givenInvalidDateAsString_whenConvertingFromFloatOrNoDate_thenItReturnsNull() throws InvalidFloatDateException {
+        String date = "clearly-invalid-date-lol";
+
+        Date actual = dateConverter.fromFloatDateFormatOrNoDate(date);
+
+        assertNull(actual);
+    }
+
     private Date givenADate(int year, int month, int day) {
         Calendar calendar = Calendar.getInstance();
+        calendar.clear();
         calendar.set(year, month, day);
         return calendar.getTime();
     }
