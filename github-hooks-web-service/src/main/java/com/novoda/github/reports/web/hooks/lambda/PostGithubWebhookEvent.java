cc857e084a4b8d6d7f83d2ca821c4d13fc999bf2
takecare <rui@vazteixeira.org>
2016-07-20 15:20:40 +0100

    print out json input

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/lambda/PostGithubWebhookEvent.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/lambda/PostGithubWebhookEvent.java
@@ -1,12 +1,34 @@
 package com.novoda.github.reports.web.hooks.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class PostGithubWebhookEvent implements RequestHandler<String, String> {
+import java.util.Map;
+
+public class PostGithubWebhookEvent implements RequestHandler<Map<String, Object>, String> {
 
     @Override
-    public String handleRequest(String input, Context context) {
-        return "JUST GOT: " + input;
+    public String handleRequest(Map<String, Object> input, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("received : " + input);
+        return jsonHandler(input, context);
+    }
+
+    public String jsonHandler(Map<String, Object> data, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("\n>>> function name: " + context.getFunctionName());
+        logger.log("\n>>> data: " + data);
+        return mapToString(data);
+    }
+
+    private String mapToString(Map<String, Object> data) {
+        final String[] result = {""};
+        data.forEach((s, o) -> result[0] += valueOrNull(s) + ", " + valueOrNull(o) + "\n");
+        return result[0];
+    }
+
+    private String valueOrNull(Object o) {
+        return o == null ? "[null]" : o.toString();
     }
 }
