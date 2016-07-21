8dbd306c44032e8ffeb7c88737f4525f9e740f16
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-21 16:54:33 +0100

    Retrieve aggregated stats from DB

--- core/src/main/java/com/novoda/github/reports/data/model/AggregatedStats.java
+++ core/src/main/java/com/novoda/github/reports/data/model/AggregatedStats.java
@@ -16,11 +16,11 @@ public abstract class AggregatedStats implements Stats {
         return new AutoValue_AggregatedStats.Builder();
     }
 
-    abstract Map<String, AggregatedUserStats> userStats();
+    abstract Map<String, AggregatedUserStats> usersStats();
 
     @Override
     public String describeStats() {
-        return userStats().entrySet()
+        return usersStats().entrySet()
                 .stream()
                 .map(userEntryToString())
                 .collect(Collectors.joining(NEW_LINE));
@@ -28,7 +28,7 @@ public abstract class AggregatedStats implements Stats {
 
     private Function<Map.Entry<String, AggregatedUserStats>, String> userEntryToString() {
         return userEntry -> String.format(
-                "- %s%s%s",
+                "- %s%s%s\n",
                 userEntry.getKey(),
                 NEW_LINE,
                 describeAggregatedUserStats(userEntry.getValue())
@@ -45,7 +45,7 @@ public abstract class AggregatedStats implements Stats {
     @AutoValue.Builder
     public abstract static class Builder {
 
-        public abstract Builder userStats(Map<String, AggregatedUserStats> userStats);
+        public abstract Builder usersStats(Map<String, AggregatedUserStats> usersStats);
 
         public abstract AggregatedStats build();
 
