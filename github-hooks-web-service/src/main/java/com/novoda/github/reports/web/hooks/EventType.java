180196a4aa1c18fb3bfe0e738434e1fd221ee50a
takecare <rui@vazteixeira.org>
2016-07-21 15:09:46 +0100

    add event type enum to categorize types of incoming webhook events

---
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/EventType.java
@@ -0,0 +1,11 @@
+package com.novoda.github.reports.web.hooks;
+
+public enum EventType {
+
+    COMMIT_COMMENT,     // payload has 'comment' object and no 'issue' or 'pull_request' objects
+    ISSUE,              // payload has 'issue' object
+    PULL_REQUEST,       // payload has 'pull_request' object
+    ISSUE_COMMENT,      // payload has both 'issue' and 'comment' objects
+    REVIEW_COMMENT      // payload has both 'pull_request' and 'comment' objects
+
+}
