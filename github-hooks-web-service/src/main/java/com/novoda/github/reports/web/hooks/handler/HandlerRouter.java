9033be3328855db82836423d7f2016e18334c1f5
takecare <rui@vazteixeira.org>
2016-07-22 17:11:34 +0100

    simplify handler router it now tries, 1-by-1, to check which handle should handle an event

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/HandlerRouter.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/HandlerRouter.java
@@ -1,53 +1,33 @@
 package com.novoda.github.reports.web.hooks.handler;
 
-import com.novoda.github.reports.web.hooks.EventType;
-import com.novoda.github.reports.web.hooks.extract.WebhookPayloadExtractor;
+import com.novoda.github.reports.service.persistence.ConnectionManagerContainer;
 import com.novoda.github.reports.web.hooks.lambda.GithubWebhookEvent;
 import com.novoda.github.reports.web.hooks.parse.EventHandler;
-import com.novoda.github.reports.web.hooks.parse.WebhookEventClassifier;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class HandlerRouter {
 
-    private static final Map<EventType, EventHandler> HANDLERS = new HashMap<>(5); // TODO unmodifiable?
-    static {
-        HANDLERS.put(EventType.COMMIT_COMMENT, new PullRequestHandler());   // TODO proper handler
-        HANDLERS.put(EventType.ISSUE, new PullRequestHandler());            // TODO proper handler
-        HANDLERS.put(EventType.PULL_REQUEST, new PullRequestHandler());
-        HANDLERS.put(EventType.ISSUE_COMMENT, new PullRequestHandler());    // TODO proper handler
-        HANDLERS.put(EventType.REVIEW_COMMENT, new PullRequestHandler());   // TODO proper handler
-    }
+    private static final List<EventHandler> handlers = Collections.unmodifiableList(Arrays.asList(
+            // TODO more handlers
+            PullRequestHandler.newInstance(ConnectionManagerContainer.getConnectionManager())
+    ));
 
-    private WebhookEventClassifier eventClassifier;
-    private WebhookPayloadExtractor eventExtractor;
-
-    public static HandlerRouter newInstance() {
-        WebhookEventClassifier eventClassifier = new WebhookEventClassifier();
-        WebhookPayloadExtractor payloadExtractor = WebhookPayloadExtractor.newInstance();
-        return new HandlerRouter(eventClassifier, payloadExtractor);
-    }
-
-    public HandlerRouter(WebhookEventClassifier eventClassifier, WebhookPayloadExtractor eventExtractor) {
-        this.eventClassifier = eventClassifier;
-        this.eventExtractor = eventExtractor;
-    }
-
-    public void route(GithubWebhookEvent event) {
-
-        // ?? @RUI who extracts the payload, Extractors here or inside each handler?
-        // probably inside each handler 'cause this guy should only route
-
-        EventType eventType = eventClassifier.classify(event);
-        EventHandler eventHandler = HANDLERS.get(eventType);
-
-        if (eventHandler == null) {
-            throw new IllegalStateException("Could not find correct handler for the event.");
+    public void route(GithubWebhookEvent event) throws UnhandledEventException {
+        boolean eventWasNotHandled = handlers
+                .stream()
+                .noneMatch(eventHandler -> eventHandler.handle(event));
+        if (eventWasNotHandled) {
+            throw new UnhandledEventException(event);
         }
+    }
 
-        //eventHandler.handle(event);
-        eventHandler.handle(event); // FIXME eventHandler.handle(eventExtractor.extract(event)) does not produce compiler error and "should"!
+    public static class UnhandledEventException extends Exception {
+        public UnhandledEventException(GithubWebhookEvent event) {
+            super("Unhandled event: " + event.toString());
+        }
     }
 
 }
