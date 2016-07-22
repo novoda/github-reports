3118048dd7c27ebe6053dc9785efeae2b8fca379
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-22 11:40:14 +0100

    Add constructors for CLI options

--- reports-stats/src/main/java/com/novoda/github/reports/stats/command/OverallOptions.java
+++ reports-stats/src/main/java/com/novoda/github/reports/stats/command/OverallOptions.java
@@ -2,9 +2,19 @@ package com.novoda.github.reports.stats.command;
 
 import com.beust.jcommander.Parameters;
 
+import java.util.Date;
+import java.util.List;
+
 @Parameters(commandDescription = "Retrieve overall statistics about users contributions")
 public class OverallOptions extends FloatTaskBasedOptions {
 
     // all parameters are in super class (from, to, users)
 
+    public OverallOptions(List<String> users, Date from, Date to) {
+        super(users, from, to);
+    }
+
+    public OverallOptions() {
+        // no-op
+    }
 }
