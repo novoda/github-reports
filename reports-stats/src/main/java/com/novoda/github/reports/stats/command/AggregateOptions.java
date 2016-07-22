eb2aa5ecfbf4ff8c5b3402a4fff8abb5dce928b4
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-22 11:40:14 +0100

    Add constructors for CLI options

--- reports-stats/src/main/java/com/novoda/github/reports/stats/command/AggregateOptions.java
+++ reports-stats/src/main/java/com/novoda/github/reports/stats/command/AggregateOptions.java
@@ -2,9 +2,19 @@ package com.novoda.github.reports.stats.command;
 
 import com.beust.jcommander.Parameters;
 
+import java.util.Date;
+import java.util.List;
+
 @Parameters(commandDescription = "Retrieve aggregated assigned/external statistics about users contributions")
 public class AggregateOptions extends FloatTaskBasedOptions {
 
     // all parameters are in super class (from, to, users)
 
+    public AggregateOptions(List<String> users, Date from, Date to) {
+        super(users, from, to);
+    }
+
+    public AggregateOptions() {
+        // no-op
+    }
 }
--- reports-stats/src/main/java/com/novoda/github/reports/stats/command/PullRequestOptions.java
+++ reports-stats/src/main/java/com/novoda/github/reports/stats/command/PullRequestOptions.java
@@ -3,20 +3,20 @@ package com.novoda.github.reports.stats.command;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
+import java.util.Date;
 import java.util.List;
 
 @Parameters(commandDescription = "Retrieve statistics about Pull Requests filtered on date, users, projects and repositories")
 public class PullRequestOptions extends RangeOptions {
 
-    public PullRequestOptions() {
-        // no-op
-    }
+    public PullRequestOptions(List<String> repositories,
+                              List<String> organisationUsers,
+                              PullRequestOptionsGroupBy groupBy,
+                              boolean withAverage,
+                              Date from,
+                              Date to) {
 
-    PullRequestOptions(List<String> repositories,
-                       List<String> organisationUsers,
-                       PullRequestOptionsGroupBy groupBy,
-                       boolean withAverage) {
-
+        super(from, to);
         this.repositories = repositories;
         this.organisationUsers = organisationUsers;
         this.groupBy = groupBy;
@@ -40,6 +40,10 @@ public class PullRequestOptions extends RangeOptions {
     @Parameter(names = {"--average", "-a"},
             description = "Whether to add user average for every group")
     private boolean withAverage;
+
+    public PullRequestOptions() {
+        // no-op
+    }
 
     public List<String> getRepositories() {
         return repositories;
