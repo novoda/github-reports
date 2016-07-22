b9be585fb0e5473ec2299e8ffa1752074068a589
takecare <rui@vazteixeira.org>
2016-07-22 17:10:30 +0100

    pr handler now comforms to the new interface

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/PullRequestHandler.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/PullRequestHandler.java
@@ -1,15 +1,47 @@
 package com.novoda.github.reports.web.hooks.handler;
 
+import com.novoda.github.reports.data.db.ConnectionManager;
 import com.novoda.github.reports.data.db.DbEventDataLayer;
+import com.novoda.github.reports.service.issue.GithubIssue;
+import com.novoda.github.reports.web.hooks.EventType;
+import com.novoda.github.reports.web.hooks.extract.PullRequestExtractor;
 import com.novoda.github.reports.web.hooks.lambda.GithubWebhookEvent;
 import com.novoda.github.reports.web.hooks.parse.EventHandler;
+import com.novoda.github.reports.web.hooks.parse.WebhookEventClassifier;
 
 class PullRequestHandler implements EventHandler {
 
+    private WebhookEventClassifier eventClassifier;
+    private PullRequestExtractor extractor;
     private DbEventDataLayer eventDataLayer;
 
+    public static PullRequestHandler newInstance(ConnectionManager connectionManager) {
+        WebhookEventClassifier eventClassifier = new WebhookEventClassifier();
+        PullRequestExtractor pullRequestExtractor = new PullRequestExtractor();
+        DbEventDataLayer eventDataLayer = DbEventDataLayer.newInstance(connectionManager);
+        return new PullRequestHandler(eventClassifier, pullRequestExtractor, eventDataLayer);
+    }
+
+    PullRequestHandler(WebhookEventClassifier eventClassifier, PullRequestExtractor extractor, DbEventDataLayer eventDataLayer) {
+        this.eventClassifier = eventClassifier;
+        this.extractor = extractor;
+        this.eventDataLayer = eventDataLayer;
+    }
+
     @Override
-    public void handle(GithubWebhookEvent event) {
-        // TODO persist and... ?
+    public boolean handle(GithubWebhookEvent event) {
+        if (cannotHandleEvent(event)) {
+            return false;
+        }
+
+        GithubWebhookEvent.Action action = event.action();
+        GithubIssue pullRequest = extractor.extractFrom(event);
+        // TODO convert and persist and... ?
+
+        return true;
+    }
+
+    private boolean cannotHandleEvent(GithubWebhookEvent event) {
+        return eventClassifier.classify(event) != EventType.PULL_REQUEST;
     }
 }
