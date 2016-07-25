572da35708793e8f06428a53f36c0caff530fe7b
takecare <rui@vazteixeira.org>
2016-07-25 17:34:35 +0100

    add test around pr event type handling

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/handler/PullRequestHandlerTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/handler/PullRequestHandlerTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -72,5 +73,10 @@ public class PullRequestHandlerTest {
         } catch (ExtractException e) {
             // nothing to do
         }
+    }
+
+    @Test
+    public void handledEventTypeShouldBePullRequest() {
+        assertEquals(EventType.PULL_REQUEST, pullRequestHandler.handledEventType());
     }
 }
