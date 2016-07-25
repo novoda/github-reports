66854dc869d3293a9344bc06a1d6dd317efdebe0
takecare <rui@vazteixeira.org>
2016-07-25 17:05:12 +0100

    add not null annotation to handled event type method

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/EventHandler.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/EventHandler.java
@@ -3,10 +3,13 @@ package com.novoda.github.reports.web.hooks.handler;
 import com.novoda.github.reports.web.hooks.classification.EventType;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
 
+import org.jetbrains.annotations.NotNull;
+
 interface EventHandler {
 
     void handle(GithubWebhookEvent event) throws UnhandledEventException;
 
+    @NotNull
     EventType handledEventType();
 
 }
