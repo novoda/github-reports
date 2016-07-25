7f929a7ce0880107cd7a91db236183ee5c4cdf05
takecare <rui@vazteixeira.org>
2016-07-25 10:17:39 +0100

    add tests around pr extractor

---
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/PullRequestExtractorTest.java
@@ -0,0 +1,44 @@
+package com.novoda.github.reports.web.hooks.extract;
+
+import com.novoda.github.reports.service.issue.GithubIssue;
+import com.novoda.github.reports.web.hooks.lambda.GithubWebhookEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class PullRequestExtractorTest {
+
+    @Mock
+    private GithubWebhookEvent mockEvent;
+
+    private PullRequestExtractor pullRequestExtractor;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        pullRequestExtractor = new PullRequestExtractor();
+    }
+
+    @Test
+    public void givenAPullRequestEvent_whenExtractingThePayload_thenItIsExtracted() throws Exception {
+        GithubIssue mockIssue = mock(GithubIssue.class);
+        when(mockEvent.pullRequest()).thenReturn(mockIssue);
+
+        GithubIssue actual = pullRequestExtractor.extractFrom(mockEvent);
+
+        assertEquals(mockIssue, actual);
+    }
+
+    @Test(expected = ExtractException.class)
+    public void givenANonPullRequestEvent_whenExtractingThePayload_thenAnExceptionIsThrown() throws Exception {
+
+        pullRequestExtractor.extractFrom(mockEvent);
+    }
+
+}
