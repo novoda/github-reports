9d6d4b143b975127a5c7dd0ad236f8b9cdf0855a
takecare <rui@vazteixeira.org>
2016-07-25 17:32:19 +0100

    add commit comment handler and tests around it

---
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/handler/CommitCommentHandlerTest.java
@@ -0,0 +1,56 @@
+package com.novoda.github.reports.web.hooks.handler;
+
+import com.novoda.github.reports.web.hooks.classification.EventType;
+import com.novoda.github.reports.web.hooks.extract.CommitCommentExtractor;
+import com.novoda.github.reports.web.hooks.extract.ExtractException;
+import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class CommitCommentHandlerTest {
+
+    @Mock
+    private CommitCommentExtractor mockExtractor;
+
+    @InjectMocks
+    private CommitCommentHandler commitCommentHandler;
+
+    @Mock
+    private GithubWebhookEvent mockEvent;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+    }
+
+    @Test
+    public void givenAnEventThatIsACommitComment_whenHandlingIt_thenThePayloadIsExtracted() throws Exception {
+
+        commitCommentHandler.handle(mockEvent);
+
+        verify(mockExtractor).extractFrom(mockEvent);
+    }
+
+    @Test(expected = UnhandledEventException.class)
+    public void givenAnEventThatIsNotACommitComment_whenHandlingIt_thenThrowsException() throws Exception {
+        given(mockExtractor.extractFrom(mockEvent)).willThrow(ExtractException.class);
+
+        commitCommentHandler.handle(mockEvent);
+
+        verify(mockExtractor).extractFrom(mockEvent);
+    }
+
+    @Test
+    public void handledEventTypeShouldBeCommitComment() {
+        assertEquals(EventType.COMMIT_COMMENT, commitCommentHandler.handledEventType());
+    }
+
+}
