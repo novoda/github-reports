38b2ce93f657f91286415ae4546bfb83a6058b5a
takecare <rui@vazteixeira.org>
2016-07-26 22:03:41 +0100

    add 'was merged' field to github pr model so we can, at the webhook service level, differentiate between prs that were merged and ones that were closed (with unmerged commits)

--- github/src/main/java/com/novoda/github/reports/service/pullrequest/GithubPullRequest.java
+++ github/src/main/java/com/novoda/github/reports/service/pullrequest/GithubPullRequest.java
@@ -2,4 +2,10 @@ package com.novoda.github.reports.service.pullrequest;
 
 public class GithubPullRequest {
     // used only to understand if an issue is a PR
+
+    private boolean wasMerged;
+
+    public boolean isMerged() {
+        return wasMerged;
+    }
 }
