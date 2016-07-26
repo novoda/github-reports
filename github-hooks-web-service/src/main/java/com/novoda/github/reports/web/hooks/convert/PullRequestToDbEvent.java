fceba305f5454d993f75c2319b6b22ff7fbf0d89
takecare <rui@vazteixeira.org>
2016-07-26 22:07:14 +0100

    convert pr action into event type (db) differentiating between a pr merge and a pr close

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/convert/PullRequestToDbEvent.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/convert/PullRequestToDbEvent.java
@@ -16,33 +16,48 @@ public class PullRequestToDbEvent implements EventConverter<PullRequest, Event> 
         GithubIssue issue = pullRequest.getIssue();
         GithubRepository repository = pullRequest.getRepository();
 
+        EventType eventType = convertPullRequestAction(pullRequest);
+
         return Event.create(
                 issue.getId(),
                 repository.getId(),
                 issue.getUserId(),
                 issue.getUserId(),
-                com.novoda.github.reports.data.model.EventType.BRANCH_DELETE, // TODO ActionToEventType
+                eventType,
                 issue.getUpdatedAt()
         );
+    }
+
+    private EventType convertPullRequestAction(PullRequest pullRequest) {
+        EventType eventType = convertAction(pullRequest.getAction());
+        if (eventType == EventType.PULL_REQUEST_CLOSE) {
+            // TODO extract
+            return pullRequest.getIssue().getPullRequest().isMerged() ?
+                    EventType.PULL_REQUEST_MERGE
+                    : EventType.PULL_REQUEST_CLOSE;
+        }
+        return eventType;
     }
 
     @Override
     public EventType convertAction(GithubAction action) {
         switch (action) {
-            case ADDED:
-                //return EventType.PULL_REQUEST_LABEL_ADD ?
-                break;
-            case CLOSED:
-
-                break;
-            case CREATED:
-
-                break;
+            case LABELED:
+                return EventType.PULL_REQUEST_LABEL_ADD;
+            case UNLABELED:
+                return EventType.PULL_REQUEST_LABEL_REMOVE;
             case OPENED:
-
+                return EventType.PULL_REQUEST_OPEN;
+            case CLOSED:
+                return EventType.PULL_REQUEST_CLOSE;
+            case EDITED:
+                // no db support
                 break;
-            case PUBLISHED:
-
+            case REOPENED:
+                // no db support
+                break;
+            case SYNCHRONIZE:
+                // no db support
                 break;
         }
 
