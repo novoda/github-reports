bfcdf7baf303dacdd6ac9ab29b92a9bd223a39e0
takecare <rui@vazteixeira.org>
2016-07-26 19:49:40 +0100

    make pr model extend the event model

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/model/PullRequest.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/model/PullRequest.java
@@ -3,7 +3,7 @@ package com.novoda.github.reports.web.hooks.model;
 import com.novoda.github.reports.service.issue.GithubIssue;
 import com.novoda.github.reports.service.repository.GithubRepository;
 
-public class PullRequest {
+public class PullRequest extends Event {
 
     private GithubIssue issue;
     private GithubRepository repository;
