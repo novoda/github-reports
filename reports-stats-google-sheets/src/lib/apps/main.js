38bbd25b7f0c9ab144c285f6def6780cab28a46a
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-27 16:17:15 +0100

    Reorder methods in top-down fashion, rename for clarity

--- reports-stats-google-sheets/src/lib/apps/main.js
+++ reports-stats-google-sheets/src/lib/apps/main.js
@@ -6,6 +6,38 @@ function Main(reports, spreadsheet) {
   this.spreadsheet = spreadsheet;
 }
 
+Main.prototype.showPrStats = function(from, to, repos, groupBy, withAverage, requestISODate) {
+  var self = this;
+  return this.reports.getPrStats(from, to, repos, groupBy, withAverage)
+    .then(function(stats) {
+      return {
+        stats: stats,
+        lines: stats.groups.map(prStatsGroupToLines)
+      };
+    })
+    .then(function(stats) {
+      return {
+        stats: stats.stats,
+        lines: [].concat.apply([], stats.lines)
+      };
+    })
+    .then(function(stats) {
+      var statsSheet = self.spreadsheet.createNewSheet(buildNewSheetName('PR Stats', requestISODate));
+      statsSheet.clear();
+      setPrHeader(statsSheet, groupBy);
+      freezePrRowAndColumn(statsSheet);
+      setPrStats(statsSheet, stats.lines);
+      setPrBorderToEndOfGroups(statsSheet, stats.stats);
+      setPrBackgroundToGroupAverages(statsSheet, stats.stats);
+      return stats.lines;
+    })['catch'](
+    // awful hax because Google Apps Script uses the stupidest AST parser or whatevs
+    // see https://github.com/stefanpenner/es6-promise#usage-in-ie9
+    function(error) {
+      self.spreadsheet.showAlert('Error', 'There was an error while executing the request:\n' + error);
+    });
+};
+
 var SHEET_STATS_HEADER_ROW = 1;
 var SHEET_STATS_FIRST_ROW = 2;
 var SHEET_STATS_FIRST_COLUMN = 1;
@@ -14,8 +46,8 @@ var PR_STATS_USER_ATTRIBUTE_QTY = 11;
 
 var AVERAGE_ROW_BACKGROUND_COLOR = '#8dc5db';
 
-function groupToLines(group) {
-  var userToLine = userToLineFn(group);
+function prStatsGroupToLines(group) {
+  var userToLine = prUserToLineFn(group);
   var lines = group.users.map(userToLine);
 
   if (group.organisationAverage) {
@@ -31,7 +63,7 @@ function groupToLines(group) {
   return lines;
 }
 
-function userToLineFn(group) {
+function prUserToLineFn(group) {
   return function(user) {
     return [
       group.name,
@@ -49,23 +81,7 @@ function userToLineFn(group) {
   };
 }
 
-function buildNewSheetName(baseName, requestISODate) {
-  var requestDate = new Date(requestISODate);
-  return baseName + ' ' +
-    requestDate.getFullYear() + '-' + padTens(requestDate.getMonth()) + '-' + padTens(requestDate.getDay()) + ' ' +
-    padTens(requestDate.getHours()) + ':' + padTens(requestDate.getMinutes()) + ':' + padTens(requestDate.getSeconds());
-}
-
-function padTens(value) {
-  return leftPad('00', value);
-}
-
-function leftPad(mask, value) {
-  var str = value.toString();
-  return mask.substring(0, mask.length - str.length) + str;
-}
-
-function setHeader(statsSheet, groupBy) {
+function setPrHeader(statsSheet, groupBy) {
   var header = [buildStatsHeader(groupBy)];
   statsSheet.setValues(
     SHEET_STATS_HEADER_ROW,
@@ -118,12 +134,12 @@ function buildStatsHeader(groupBy) {
   ];
 }
 
-function freezeRowAndColumn(statsSheet) {
+function freezePrRowAndColumn(statsSheet) {
   statsSheet.setFrozenRows(1);
   statsSheet.setFrozenColumns(2);
 }
 
-function setStats(statsSheet, lines) {
+function setPrStats(statsSheet, lines) {
   statsSheet.setValues(
     SHEET_STATS_FIRST_ROW,
     SHEET_STATS_FIRST_COLUMN,
@@ -139,7 +155,7 @@ function setStats(statsSheet, lines) {
   );
 }
 
-function setBorderToEndOfGroups(statsSheet, stats) {
+function setPrBorderToEndOfGroups(statsSheet, stats) {
   var rowAccumulator = SHEET_STATS_FIRST_ROW - 1;
   stats.groups.forEach(function(group) {
     rowAccumulator += group.users.length + countAverages(group);
@@ -152,7 +168,7 @@ function setBorderToEndOfGroups(statsSheet, stats) {
   });
 }
 
-function setBackgroundToGroupAverages(statsSheet, stats) {
+function setPrBackgroundToGroupAverages(statsSheet, stats) {
   var rowAccumulator = SHEET_STATS_FIRST_ROW - 1;
   stats.groups.forEach(function(group) {
     var averages = countAverages(group);
@@ -185,37 +201,6 @@ function countAverages(group) {
   return count;
 }
 
-Main.prototype.showPrStats = function(from, to, repos, groupBy, withAverage, requestISODate) {
-  var self = this;
-  return this.reports.getPrStats(from, to, repos, groupBy, withAverage)
-    .then(function(stats) {
-      return {
-        stats: stats,
-        lines: stats.groups.map(groupToLines)
-      };
-    })
-    .then(function(stats) {
-      return {
-        stats: stats.stats,
-        lines: [].concat.apply([], stats.lines)
-      };
-    })
-    .then(function(stats) {
-      var statsSheet = self.spreadsheet.createNewSheet(buildNewSheetName('PR Stats', requestISODate));
-      statsSheet.clear();
-      setHeader(statsSheet, groupBy);
-      freezeRowAndColumn(statsSheet);
-      setStats(statsSheet, stats.lines);
-      setBorderToEndOfGroups(statsSheet, stats.stats);
-      setBackgroundToGroupAverages(statsSheet, stats.stats);
-      return stats.lines;
-    })['catch'](
-    // awful hax because Google Apps Script uses the stupidest AST parser or whatevs
-    // see https://github.com/stefanpenner/es6-promise#usage-in-ie9
-    function(error) {
-      self.spreadsheet.showAlert('Error', 'There was an error while executing the request:\n' + error);
-    });
-};
 
 Main.prototype.showAggregatedStats = function(from, to, users, requestISODate) {
   var self = this;
@@ -260,6 +245,33 @@ function describeProjectsOrRepositoriesStats(stats) {
     .join('\n');
 }
 
+function setStatsIntoNewSheet(spreadsheet, requestISODate) {
+  return function(stats) {
+    var statsSheet = spreadsheet.createNewSheet(buildNewSheetName('Aggregated Stats', requestISODate));
+    statsSheet.clear();
+    setAggregatedHeader(statsSheet);
+    freezeAggregatedRowAndColumn(statsSheet);
+    setAggregatedStats(statsSheet, stats);
+    return stats;
+  };
+}
+
+function buildNewSheetName(baseName, requestISODate) {
+  var requestDate = new Date(requestISODate);
+  return baseName + ' ' +
+    requestDate.getFullYear() + '-' + padTens(requestDate.getMonth()) + '-' + padTens(requestDate.getDay()) + ' ' +
+    padTens(requestDate.getHours()) + ':' + padTens(requestDate.getMinutes()) + ':' + padTens(requestDate.getSeconds());
+}
+
+function padTens(value) {
+  return leftPad('00', value);
+}
+
+function leftPad(mask, value) {
+  var str = value.toString();
+  return mask.substring(0, mask.length - str.length) + str;
+}
+
 function setAggregatedHeader(statsSheet) {
   var header = [[
     'Username',
@@ -300,17 +312,6 @@ function setAggregatedHeader(statsSheet) {
     AGGREGATED_STATS_NUMBER_OF_COLUMNS,
     true
   );
-}
-
-function setStatsIntoNewSheet(spreadsheet, requestISODate) {
-  return function(stats) {
-    var statsSheet = spreadsheet.createNewSheet(buildNewSheetName('Aggregated Stats', requestISODate));
-    statsSheet.clear();
-    setAggregatedHeader(statsSheet);
-    freezeAggregatedRowAndColumn(statsSheet);
-    setAggregatedStats(statsSheet, stats);
-    return stats;
-  };
 }
 
 function freezeAggregatedRowAndColumn(statsSheet) {
