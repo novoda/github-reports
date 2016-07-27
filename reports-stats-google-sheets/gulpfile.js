c789868c40ab67b6aa25bce7e8e51cb940372e08
Francesco Pontillo <francescopontillo@gmail.com>
2016-07-27 14:42:18 +0100

    Add better build tasks and live reload mechanism

--- reports-stats-google-sheets/gulpfile.js
+++ reports-stats-google-sheets/gulpfile.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var eslint = require('gulp-eslint');
-var Karma = require('karma').Server;
-var inject = require('gulp-inject');
-var clean = require('gulp-clean');
-var bowerFiles = require('main-bower-files');
-var rename = require('gulp-rename');
-var flatmap = require('gulp-flatmap');
+let gulp = require('gulp');
+let shell = require('gulp-shell');
+let eslint = require('gulp-eslint');
+let Karma = require('karma').Server;
+let inject = require('gulp-inject');
+let clean = require('gulp-clean');
+let bowerFiles = require('main-bower-files');
+let rename = require('gulp-rename');
+let flatmap = require('gulp-flatmap');
+let connect = require('gulp-connect');
+var openURL = require('open');
 
 gulp.task('lint', function() {
   return gulp.src(['src/**/*.js'])
@@ -26,34 +28,42 @@ gulp.task('karma', function(done) {
 
 gulp.task('test', ['lint', 'karma']);
 
-gulp.task('clean-sidebar', function() {
+gulp.task('clean:sidebar', function() {
   return gulp.src('build/sidebar', {read: false})
     .pipe(clean());
 });
 
-gulp.task('clean-lib', function() {
+gulp.task('clean:sidebar:dev', function() {
+  return gulp.src('.tmp/sidebar', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('clean:lib', function() {
   return gulp.src(['build/lib', 'build/*.js'], {read: false})
     .pipe(clean());
 });
 
-gulp.task('clean-bower', function() {
+gulp.task('clean:bower', function() {
   return gulp.src('build/bower_components', {read: false})
     .pipe(clean());
 });
 
-var wrapTagJs = function(content) {
+const wrapTagJs = function(content) {
   return `<script type="text/javascript">
 ${content}
 </script>`;
 };
 
-var wrapTagCss = function(content) {
+const wrapTagCss = function(content) {
   return `<style type="text/css">
 ${content}
 </style>`;
 };
 
-var transformContents = function(partialName) {
+const getContentsOrReferences = function(includeContents) {
+  if (!includeContents) {
+    return undefined;
+  }
   return function(filePath, file) {
     let fileContents = file.contents.toString('utf8');
     if (filePath.slice(-3) === '.js') {
@@ -62,47 +72,67 @@ var transformContents = function(partialName) {
     if (filePath.slice(-4) === '.css') {
       return wrapTagCss(fileContents);
     }
+  };
+};
+
+const getHtmlContents = function(partialName) {
+  return function(filePath, file) {
+    let fileContents = file.contents.toString('utf8');
     if (filePath.slice(-5) === '.html' && partialName === file.relative) {
       return fileContents;
     }
   };
 };
 
-gulp.task('build-sidebar', ['test', 'clean-sidebar'], function() {
-  var allSourceFiles = [
+const getDestinationFolder = function(forBuild) {
+  let destFolder = forBuild ? 'build' : '.tmp';
+  return `${destFolder}/sidebar`;
+};
+
+const buildSidebarTask = function(forBuild) {
+  let allSourceFiles = [
     'src/sidebar/**/*.css',
     'src/lib/common/**/*.js',
     'src/lib/web/**/*.js',
-    '!src/lib/web/**/*.controller.js',
-    'src/sidebar/**/*.partial.html'
+    '!src/lib/web/**/*.controller.js'
   ];
-  var allBowerFiles = bowerFiles();
+  let allBowerFiles = bowerFiles();
+  let destFolder = getDestinationFolder(forBuild);
 
-  return gulp.src('src/sidebar/**/*.partial.html')
-    .pipe(flatmap(function(partialHtmlStream, partialHtmlFile) {
-      var partialHtmlFileName = partialHtmlFile.relative;
-      const regExp = /^(.*)(?:\.partial\.html)$/g;
-      var partialPageName = regExp.exec(partialHtmlFileName)[1];
+  return function() {
+    return gulp.src('src/sidebar/**/*.partial.html')
+      .pipe(flatmap(function(partialHtmlStream, partialHtmlFile) {
+        let partialHtmlFileName = partialHtmlFile.relative;
+        const regExp = /^(.*)(?:\.partial\.html)$/g;
+        let partialPageName = regExp.exec(partialHtmlFileName)[1];
 
-      return gulp
-        .src('src/sidebar/sidebar.template.html', {base: './src/sidebar'})
-        .pipe(inject(gulp.src(allBowerFiles), {
-          starttag: '<!-- inject:bower:{{ext}} -->',
-          transform: transformContents()
-        }))
-        .pipe(inject(gulp.src(allSourceFiles), {
-          transform: transformContents(partialHtmlFileName)
-        }))
-        .pipe(inject(gulp.src(`src/lib/web/**/${partialPageName}.controller.js`), {
-          starttag: '<!-- inject:pageController:js -->',
-          transform: transformContents()
-        }))
-        .pipe(rename(`sidebar-${partialPageName}.html`));
-    }))
-    .pipe(gulp.dest('build/sidebar'));
-});
+        return gulp
+          .src('src/sidebar/sidebar.template.html', {base: 'src/sidebar'})
+          .pipe(inject(gulp.src(allBowerFiles), {
+            starttag: '<!-- inject:bower:{{ext}} -->',
+            transform: getContentsOrReferences(forBuild)
+          }))
+          .pipe(inject(gulp.src(allSourceFiles), {
+            transform: getContentsOrReferences(forBuild)
+          }))
+          .pipe(inject(gulp.src('src/sidebar/**/*.partial.html'), {
+            transform: getHtmlContents(partialHtmlFileName)
+          }))
+          .pipe(inject(gulp.src(`src/lib/web/**/${partialPageName}.controller.js`), {
+            starttag: '<!-- inject:pageController:js -->',
+            transform: getContentsOrReferences(forBuild)
+          }))
+          .pipe(rename(`sidebar-${partialPageName}.html`));
+      }))
+      .pipe(gulp.dest(destFolder));
+  };
+};
 
-gulp.task('copy-lib', ['test', 'clean-lib'], function() {
+gulp.task('build:sidebar:dev', ['test', 'clean:sidebar:dev'], buildSidebarTask(false));
+
+gulp.task('build:sidebar', ['test', 'clean:sidebar'], buildSidebarTask(true));
+
+gulp.task('copy:lib', ['test', 'clean:lib'], function() {
   return gulp
     .src([
       'src/*.js',
@@ -114,7 +144,7 @@ gulp.task('copy-lib', ['test', 'clean-lib'], function() {
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('bower', ['clean-bower'], function() {
+gulp.task('bower', ['clean:bower'], function() {
   return gulp
     .src(bowerFiles({
       overrides: {
@@ -128,6 +158,30 @@ gulp.task('bower', ['clean-bower'], function() {
     .pipe(gulp.dest('build/bower_components'))
 });
 
-gulp.task('build', ['build-sidebar', 'copy-lib', 'bower']);
+gulp.task('build', ['build:sidebar', 'copy:lib', 'bower']);
 
 gulp.task('upload', ['build'], shell.task(['gapps upload']));
+
+gulp.task('build:dev', ['build:sidebar:dev'], function() {
+  gulp.src('src/**/*')
+    .pipe(connect.reload());
+});
+
+gulp.task('start:server', ['build:dev'], function() {
+  connect.server({
+    root: ['.tmp/sidebar', '.'],
+    livereload: true,
+    host: '0.0.0.0',
+    port: 9000
+  });
+});
+
+gulp.task('start:client', ['start:server'], function() {
+  openURL('http://localhost:9000');
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./src/**/*'], ['build:dev']);
+});
+
+gulp.task('serve', ['start:client', 'watch'])
