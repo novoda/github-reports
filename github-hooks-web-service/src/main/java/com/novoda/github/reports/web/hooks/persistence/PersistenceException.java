bc4bbc48a7bb51008b3d7e466d5859269dcffb9b
takecare <rui@vazteixeira.org>
2016-07-27 13:49:41 +0100

    add a persistence exception

---
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/persistence/PersistenceException.java
@@ -0,0 +1,14 @@
+package com.novoda.github.reports.web.hooks.persistence;
+
+import com.novoda.github.reports.web.hooks.model.Event;
+
+public class PersistenceException extends Exception {
+
+    PersistenceException(String message) {
+        super(message);
+    }
+
+    PersistenceException(Event event) {
+        super("Unable to persist event: " + event.toString());
+    }
+}
