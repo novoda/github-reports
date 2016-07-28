b1189419bac28e636ad34129e1282f24b65edd5e
takecare <rui@vazteixeira.org>
2016-07-28 17:48:48 +0100

    implement issue converter

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/converter/UnsupportedActionException.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/converter/UnsupportedActionException.java
@@ -2,7 +2,7 @@ package com.novoda.github.reports.web.hooks.converter;
 
 import com.novoda.github.reports.web.hooks.model.GithubAction;
 
-public class UnsupportedActionException extends Throwable {
+class UnsupportedActionException extends Throwable {
     UnsupportedActionException(GithubAction action) {
         super("Unable to convert action: " + action.toString());
     }
