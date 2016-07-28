2d04a8319439e9c5c4443d0be00ba26c96114f7e
takecare <rui@vazteixeira.org>
2016-07-28 11:51:40 +0100

    rename test classes

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/convert/PullRequestToDbEventConverterTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/convert/PullRequestToDbEventConverterTest.java
@@ -1,5 +1,7 @@
 package com.novoda.github.reports.web.hooks.convert;
 
+import com.novoda.github.reports.data.model.Event;
+import com.novoda.github.reports.data.model.EventType;
 import com.novoda.github.reports.service.issue.GithubIssue;
 import com.novoda.github.reports.service.persistence.converter.ConverterException;
 import com.novoda.github.reports.service.pullrequest.GithubPullRequest;
@@ -7,17 +9,50 @@ import com.novoda.github.reports.service.repository.GithubRepository;
 import com.novoda.github.reports.web.hooks.model.GithubAction;
 import com.novoda.github.reports.web.hooks.model.PullRequest;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static com.novoda.github.reports.web.hooks.model.GithubAction.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+@RunWith(Parameterized.class)
 public class PullRequestToDbEventConverterTest {
 
-    private static final GithubAction ANY_UNSUPPORTED_ACTION = GithubAction.SYNCHRONIZE;
+    @Parameterized.Parameters(name = "{index}: action={0}, isPrMerged={1} expectedEvent={2}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {OPENED, false, EventType.PULL_REQUEST_OPEN},
+                {CLOSED, false, EventType.PULL_REQUEST_CLOSE},
+                {CLOSED, true, EventType.PULL_REQUEST_MERGE},
+                {LABELED, false, EventType.PULL_REQUEST_LABEL_ADD},
+                {UNLABELED, false, EventType.PULL_REQUEST_LABEL_REMOVE}
+        });
+    }
+
+    private static final long ANY_ISSUE_ID = 23L;
+    private static final long ANY_REPOSITORY_ID = 42L;
+    private static final long ANY_USER_ID = 88L;
+    private static final Date ANY_DATE = new Date();
+
+    @Parameter(0)
+    public GithubAction action;
+
+    @Parameter(1)
+    public boolean isPullRequestMerged;
+
+    @Parameter(2)
+    public EventType expectedEventType;
 
     @Mock
     private GithubIssue mockGithubIssue;
@@ -39,10 +74,28 @@ public class PullRequestToDbEventConverterTest {
         initMocks(this);
     }
 
-    @Test(expected = ConverterException.class)
-    public void givenAPullRequestWithAnUnsupportedAction_whenConverting_thenThrowsException() throws ConverterException {
-        given(mockPullRequest.getAction()).willReturn(ANY_UNSUPPORTED_ACTION);
+    @Test
+    public void givenAPullRequest_whenConverting_thenConvertsSuccessfully() throws ConverterException {
+        givenAPullRequest();
 
-        converter.convertFrom(mockPullRequest);
+        Event actual = converter.convertFrom(mockPullRequest);
+
+        assertThat(actual).isEqualToComparingFieldByField(buildExpectedEvent(expectedEventType));
+    }
+
+    private void givenAPullRequest() {
+        given(mockGithubPullRequest.isMerged()).willReturn(isPullRequestMerged);
+        given(mockGithubRepository.getId()).willReturn(ANY_REPOSITORY_ID);
+        given(mockGithubIssue.getId()).willReturn(ANY_ISSUE_ID);
+        given(mockGithubIssue.getUserId()).willReturn(ANY_USER_ID);
+        given(mockGithubIssue.getUpdatedAt()).willReturn(ANY_DATE);
+        given(mockGithubIssue.getPullRequest()).willReturn(mockGithubPullRequest);
+        given(mockPullRequest.getAction()).willReturn(action);
+        given(mockPullRequest.getIssue()).willReturn(mockGithubIssue);
+        given(mockPullRequest.getRepository()).willReturn(mockGithubRepository);
+    }
+
+    private Event buildExpectedEvent(EventType eventType) {
+        return Event.create(ANY_ISSUE_ID, ANY_REPOSITORY_ID, ANY_USER_ID, ANY_USER_ID, eventType, ANY_DATE);
     }
 }
