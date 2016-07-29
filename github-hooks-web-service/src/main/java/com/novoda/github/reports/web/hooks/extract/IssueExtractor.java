6d12b61bed51de9652aa7f431974f8e7706b6a79
takecare <rui@vazteixeira.org>
2016-07-29 01:19:53 +0100

    issue extractor returns issue instead of github issue

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/extract/IssueExtractor.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/extract/IssueExtractor.java
@@ -1,15 +1,18 @@
 package com.novoda.github.reports.web.hooks.extract;
 
 import com.novoda.github.reports.service.issue.GithubIssue;
+import com.novoda.github.reports.service.repository.GithubRepository;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+import com.novoda.github.reports.web.hooks.model.Issue;
 
-public class IssueExtractor implements PayloadExtractor<GithubIssue> {
+public class IssueExtractor implements PayloadExtractor<Issue> {
     @Override
-    public GithubIssue extractFrom(GithubWebhookEvent event) throws ExtractException {
+    public Issue extractFrom(GithubWebhookEvent event) throws ExtractException {
         GithubIssue issue = event.issue();
+        GithubRepository repository = event.repository();
         if (issue == null) {
             throw new ExtractException(event);
         }
-        return issue;
+        return new Issue(issue, repository, event.action());
     }
 }
