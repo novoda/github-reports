cd6fc5afc9d2471c9dd7263ec20e92e171528e4e
takecare <rui@vazteixeira.org>
2016-08-02 11:01:57 +0100

    implement commit comment extractor and add tests around it

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/CommitCommentExtractorTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/CommitCommentExtractorTest.java
@@ -1,18 +1,30 @@
 package com.novoda.github.reports.web.hooks.extract;
 
+import com.novoda.github.reports.service.GithubUser;
 import com.novoda.github.reports.service.issue.GithubComment;
+import com.novoda.github.reports.service.repository.GithubRepository;
+import com.novoda.github.reports.web.hooks.model.CommitComment;
+import com.novoda.github.reports.web.hooks.model.GithubAction;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class CommitCommentExtractorTest {
+
+    private static final long ANY_OWNER_ID = 88;
+    private static final long ANY_REPOSITORY_ID = 42L;
+    private static final GithubAction ANY_ACTION = GithubAction.OPENED;
+    private static final long ANY_COMMENT_ID = 23L;
+    private static final Date ANY_DATE = new Date();
 
     @Mock
     private GithubWebhookEvent mockEvent;
@@ -27,17 +39,26 @@ public class CommitCommentExtractorTest {
 
     @Test
     public void givenACommitCommentEvent_whenExtractingThePayload_thenItIsExtracted() throws Exception {
-        GithubComment comment = new GithubComment();
-        given(mockEvent.comment()).willReturn(comment);
+        CommitComment expected = givenACommitComment();
 
-        GithubComment actual = extractor.extractFrom(mockEvent);
+        CommitComment actual = extractor.extractFrom(mockEvent);
 
-        assertEquals(comment, actual);
+        assertThat(actual).isEqualToComparingFieldByField(expected);
+    }
+
+    private CommitComment givenACommitComment() {
+        GithubUser githubUser = new GithubUser(ANY_OWNER_ID);
+        GithubComment githubComment = new GithubComment(ANY_COMMENT_ID, githubUser, ANY_DATE);
+        GithubRepository githubRepository = new GithubRepository(ANY_REPOSITORY_ID);
+        given(mockEvent.comment()).willReturn(githubComment);
+        given(mockEvent.repository()).willReturn(githubRepository);
+        given(mockEvent.action()).willReturn(ANY_ACTION);
+        return new CommitComment(githubComment, githubRepository, ANY_ACTION);
     }
 
     @Test(expected = ExtractException.class)
     public void givenACommitCommentEvent_whenExtractingThePayload_thenAnExceptionIsThrown() throws Exception {
-        given(mockEvent.comment()).willReturn(null);
+        given(mockEvent.issue()).willReturn(null);
 
         extractor.extractFrom(mockEvent);
     }
