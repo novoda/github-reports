0a31e5bd31553a4b194126fa9d4d8de88bf65bc0
takecare <rui@vazteixeira.org>
2016-08-03 00:43:33 +0100

    update comments on event types

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/classification/EventType.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/classification/EventType.java
@@ -3,8 +3,8 @@ package com.novoda.github.reports.web.hooks.classification;
 public enum EventType {
 
     REVIEW_COMMENT(new ReviewCommentRule()),    // payload has: 'comment', 'pull_request' and 'repository' objects
-    ISSUE(new IssueRule()),                     // payload has: 'issue' object
-    PULL_REQUEST(new PullRequestRule()),        // payload has: 'pull_request' object
+    ISSUE(new IssueRule()),                     // payload has: 'issue' and 'repository' objects
+    PULL_REQUEST(new PullRequestRule()),        // payload has: 'pull_request', 'repository' objects
     ISSUE_COMMENT(new IssueCommentRule()),      // payload has: 'issue', 'repository' and 'comment' objects
     COMMIT_COMMENT(new CommitCommentRule());    // payload has: 'repository' and 'comment' objects
 
