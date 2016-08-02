7465a8ba46fc7565bb58925d5cfbec6b8515deee
takecare <rui@vazteixeira.org>
2016-08-03 00:52:44 +0100

    reimplement commit comment extractor and its tests

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/extract/CommitCommentExtractor.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/extract/CommitCommentExtractor.java
@@ -1,19 +1,18 @@
 package com.novoda.github.reports.web.hooks.extract;
 
 import com.novoda.github.reports.service.issue.GithubComment;
-import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
-import com.novoda.github.reports.web.hooks.model.GithubWebhookPullRequest;
+import com.novoda.github.reports.service.repository.GithubRepository;
 import com.novoda.github.reports.web.hooks.model.CommitComment;
+import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
 
 public class CommitCommentExtractor implements PayloadExtractor<CommitComment> {
     @Override
     public CommitComment extractFrom(GithubWebhookEvent event) throws ExtractException {
-        GithubComment comment = event.comment();
-        GithubWebhookPullRequest webhookPullRequest = event.pullRequest();
-        if (comment == null || webhookPullRequest == null) {
+        GithubComment githubComment = event.comment();
+        GithubRepository githubRepository = event.repository();
+        if (githubComment == null || githubRepository == null) {
             throw new ExtractException(event);
         }
-        webhookPullRequest.setIsPullRequest(true);
-        return new CommitComment(webhookPullRequest, comment);
+        return new CommitComment(githubComment, githubRepository, event.action());
     }
 }
