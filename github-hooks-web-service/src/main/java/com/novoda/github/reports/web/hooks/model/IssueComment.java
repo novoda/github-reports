e77aadcaf52c7748a16f93be96fe08050a24839f
takecare <rui@vazteixeira.org>
2016-08-02 13:45:14 +0100

    make issue comment extend event model to contain an action field

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/model/IssueComment.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/model/IssueComment.java
@@ -3,14 +3,15 @@ package com.novoda.github.reports.web.hooks.model;
 import com.novoda.github.reports.service.issue.GithubComment;
 import com.novoda.github.reports.service.issue.GithubIssue;
 
-public class IssueComment {
+public class IssueComment extends Event {
 
     private GithubIssue githubIssue;
     private GithubComment githubComment;
 
-    public IssueComment(GithubIssue githubIssue, GithubComment githubComment) {
+    public IssueComment(GithubIssue githubIssue, GithubComment githubComment, GithubAction action) {
         this.githubIssue = githubIssue;
         this.githubComment = githubComment;
+        this.action = action;
     }
 
     public GithubIssue getIssue() {
