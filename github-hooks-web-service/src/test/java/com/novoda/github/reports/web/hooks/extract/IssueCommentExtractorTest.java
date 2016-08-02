7290bef042422fa6e5194b83eb9732a3b7624449
takecare <rui@vazteixeira.org>
2016-08-02 14:20:09 +0100

    update issue comment extractor tests

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/IssueCommentExtractorTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/IssueCommentExtractorTest.java
@@ -1,25 +1,32 @@
 package com.novoda.github.reports.web.hooks.extract;
 
+import com.novoda.github.reports.service.GithubUser;
 import com.novoda.github.reports.service.issue.GithubComment;
 import com.novoda.github.reports.service.issue.GithubIssue;
+import com.novoda.github.reports.service.repository.GithubRepository;
+import com.novoda.github.reports.web.hooks.model.GithubAction;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
 import com.novoda.github.reports.web.hooks.model.IssueComment;
+
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class IssueCommentExtractorTest {
 
     private static final int ANY_ISSUE_NUMBER = 23;
-    private static final long ANY_OWNER_ID = 88;
     private static final boolean ANY_IS_PULL_REQUEST = false;
+    private static final long ANY_USER_ID = 88;
+    private static final long ANY_REPOSITORY_ID = 42L;
+    private static final long ANY_COMMENT_ID = 23L;
+    private static final Date ANY_DATE = new Date();
 
     @Mock
     private GithubWebhookEvent mockEvent;
@@ -34,15 +41,23 @@ public class IssueCommentExtractorTest {
 
     @Test
     public void givenAnIssueCommentEvent_whenExtractingThePayload_thenItIsExtracted() throws Exception {
-        GithubComment comment = mock(GithubComment.class);
-        GithubIssue issue = new GithubIssue(ANY_ISSUE_NUMBER, ANY_OWNER_ID, ANY_IS_PULL_REQUEST);
-        given(mockEvent.issue()).willReturn(issue);
-        given(mockEvent.comment()).willReturn(comment);
+        IssueComment expected = givenAnIssueComment();
 
         IssueComment actual = extractor.extractFrom(mockEvent);
 
-        assertEquals(comment, actual.getComment());
-        assertEquals(issue, actual.getIssue());
+        assertThat(actual).isEqualToComparingFieldByField(expected);
+    }
+
+    private IssueComment givenAnIssueComment() {
+        GithubRepository githubRepository = new GithubRepository(ANY_REPOSITORY_ID);
+        GithubIssue githubIssue = new GithubIssue(ANY_ISSUE_NUMBER, ANY_USER_ID, ANY_IS_PULL_REQUEST);
+        GithubUser githubUser = new GithubUser(ANY_USER_ID);
+        GithubComment githubComment = new GithubComment(ANY_COMMENT_ID, githubUser, ANY_DATE);
+        given(mockEvent.comment()).willReturn(githubComment);
+        given(mockEvent.repository()).willReturn(githubRepository);
+        given(mockEvent.issue()).willReturn(githubIssue);
+        given(mockEvent.action()).willReturn(GithubAction.OPENED);
+        return new IssueComment(githubComment, githubRepository, githubIssue, GithubAction.OPENED);
     }
 
     @Test(expected = ExtractException.class)
