a66149b9f40e017fd7a2114c13231d18c0e30c25
takecare <rui@vazteixeira.org>
2016-08-02 19:45:21 +0100

    reduce visibility

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/classification/IssueCommentRule.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/classification/IssueCommentRule.java
@@ -2,7 +2,7 @@ package com.novoda.github.reports.web.hooks.classification;
 
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
 
-public class IssueCommentRule implements ClassificationRule {
+class IssueCommentRule implements ClassificationRule {
     @Override
     public boolean check(GithubWebhookEvent event) {
         return event.issue() != null && event.comment() != null;
