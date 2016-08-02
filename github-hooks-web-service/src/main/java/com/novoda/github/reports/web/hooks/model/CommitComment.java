4d9d6a6c3798b95999771a4e7a5ab146f7fb45c7
takecare <rui@vazteixeira.org>
2016-08-03 00:46:26 +0100

    update commit comment model object

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/model/CommitComment.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/model/CommitComment.java
@@ -1,22 +1,24 @@
 package com.novoda.github.reports.web.hooks.model;
 
 import com.novoda.github.reports.service.issue.GithubComment;
+import com.novoda.github.reports.service.repository.GithubRepository;
 
-public class CommitComment {
+public class CommitComment extends Event {
 
-    private GithubWebhookPullRequest webhookPullRequest;
     private GithubComment comment;
+    private GithubRepository repository;
 
-    public CommitComment(GithubWebhookPullRequest webhookPullRequest, GithubComment comment) {
-        this.webhookPullRequest = webhookPullRequest;
+    public CommitComment(GithubComment comment, GithubRepository repository, GithubAction action) {
         this.comment = comment;
-    }
-
-    public GithubWebhookPullRequest getWebhookPullRequest() {
-        return webhookPullRequest;
+        this.repository = repository;
+        this.action = action;
     }
 
     public GithubComment getComment() {
         return comment;
+    }
+
+    public GithubRepository getRepository() {
+        return repository;
     }
 }
