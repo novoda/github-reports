49b9dfae8121ec8173f204e51f4432bf13409a2a
takecare <rui@vazteixeira.org>
2016-08-03 09:34:29 +0100

    add commit comment converter

---
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/converter/CommitCommentConverter.java
@@ -0,0 +1,49 @@
+package com.novoda.github.reports.web.hooks.converter;
+
+import com.novoda.github.reports.data.model.Event;
+import com.novoda.github.reports.data.model.EventType;
+import com.novoda.github.reports.service.issue.GithubComment;
+import com.novoda.github.reports.service.persistence.converter.ConverterException;
+import com.novoda.github.reports.service.repository.GithubRepository;
+import com.novoda.github.reports.web.hooks.model.CommitComment;
+import com.novoda.github.reports.web.hooks.model.GithubAction;
+
+public class CommitCommentConverter implements EventConverter<CommitComment> {
+
+    @Override
+    public Event convertFrom(CommitComment commitComment) throws ConverterException {
+        GithubComment githubComment = commitComment.getComment();
+        GithubRepository githubRepository = commitComment.getRepository();
+        EventType eventType = getEventType(commitComment);
+        return Event.create(
+                githubComment.getId(),
+                githubRepository.getId(),
+                githubComment.getUserId(),
+                githubRepository.getOwner().getId(),
+                eventType,
+                githubComment.getUpdatedAt()
+        );
+    }
+
+    private EventType getEventType(CommitComment commitComment) throws ConverterException {
+        try {
+            return convertAction(commitComment.getAction());
+        } catch (UnsupportedActionException e) {
+            throw new ConverterException(e);
+        }
+    }
+
+    @Override
+    public EventType convertAction(GithubAction action) throws UnsupportedActionException {
+        switch (action) {
+            case CREATED:
+                // we currently don't have a specific event type for a commit comment
+                return EventType.PULL_REQUEST_COMMENT;
+            case EDITED:
+            case DELETED:
+                // no db support
+        }
+
+        throw new UnsupportedActionException(action);
+    }
+}
