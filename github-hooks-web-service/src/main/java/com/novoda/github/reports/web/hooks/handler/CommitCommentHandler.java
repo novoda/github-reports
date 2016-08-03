dadc0032f68277e174e6819ec2886f2c04dabc66
takecare <rui@vazteixeira.org>
2016-08-03 10:35:23 +0100

    implement commit comment handler

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/CommitCommentHandler.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/CommitCommentHandler.java
@@ -1,35 +1,52 @@
 package com.novoda.github.reports.web.hooks.handler;
 
+import com.novoda.github.reports.data.db.ConnectionManager;
 import com.novoda.github.reports.web.hooks.classification.EventType;
-import com.novoda.github.reports.web.hooks.extract.ExtractException;
 import com.novoda.github.reports.web.hooks.extract.CommitCommentExtractor;
-import com.novoda.github.reports.web.hooks.model.GithubAction;
-import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+import com.novoda.github.reports.web.hooks.extract.ExtractException;
+import com.novoda.github.reports.web.hooks.extract.PayloadExtractor;
 import com.novoda.github.reports.web.hooks.model.CommitComment;
+import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+import com.novoda.github.reports.web.hooks.persistence.CommitCommentPersister;
+import com.novoda.github.reports.web.hooks.persistence.PersistenceException;
+import com.novoda.github.reports.web.hooks.persistence.Persister;
 
-public class CommitCommentHandler implements EventHandler {
+class CommitCommentHandler implements EventHandler {
 
-    private final CommitCommentExtractor extractor;
+    private final PayloadExtractor<CommitComment> extractor;
+    private final Persister<CommitComment> persister;
 
-    public static CommitCommentHandler newInstance() {
-        CommitCommentExtractor extractor = new CommitCommentExtractor();
-        return new CommitCommentHandler(extractor);
+    static CommitCommentHandler newInstance(ConnectionManager connectionManager) {
+        PayloadExtractor<CommitComment> extractor = new CommitCommentExtractor();
+        Persister<CommitComment> persister = CommitCommentPersister.newInstance(connectionManager);
+        return new CommitCommentHandler(extractor, persister);
     }
 
-    CommitCommentHandler(CommitCommentExtractor extractor) {
+    private CommitCommentHandler(PayloadExtractor<CommitComment> extractor, Persister<CommitComment> persister) {
         this.extractor = extractor;
+        this.persister = persister;
     }
 
     @Override
     public void handle(GithubWebhookEvent event) throws UnhandledEventException {
+        CommitComment reviewComment = extractCommitComment(event);
+        persist(reviewComment);
+    }
 
-        GithubAction action = event.action();
+    private CommitComment extractCommitComment(GithubWebhookEvent event) throws UnhandledEventException {
         try {
-            CommitComment commitComment = extractor.extractFrom(event);
+            return extractor.extractFrom(event);
         } catch (ExtractException e) {
             throw new UnhandledEventException(e.getMessage());
         }
-        // TODO convert and persist, taking into account the value of 'action'
+    }
+
+    private void persist(CommitComment reviewComment) throws UnhandledEventException {
+        try {
+            persister.persist(reviewComment);
+        } catch (PersistenceException e) {
+            throw new UnhandledEventException(e.getMessage());
+        }
     }
 
     @Override
