dcd4c243f655ef756f5e2dbf47147dcafc221ff3
takecare <rui@vazteixeira.org>
2016-08-03 09:06:05 +0100

    update test use value classes instead of mocks; rename tests and one more test

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/PullRequestExtractorTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/PullRequestExtractorTest.java
@@ -1,9 +1,8 @@
 package com.novoda.github.reports.web.hooks.extract;
 
 import com.novoda.github.reports.service.GithubUser;
-import com.novoda.github.reports.service.issue.GithubComment;
-import com.novoda.github.reports.service.issue.GithubIssue;
 import com.novoda.github.reports.service.repository.GithubRepository;
+import com.novoda.github.reports.web.hooks.model.GithubAction;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookPullRequest;
 import com.novoda.github.reports.web.hooks.model.PullRequest;
@@ -15,19 +14,21 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class PullRequestExtractorTest {
 
-    private static final int ANY_OWNER_ID = 2;
-    private static final long ANY_REPO_ID = 1L;
-    private static final GithubComment NO_COMMENT = null;
     private static final long ANY_ISSUE_ID = 23L;
     private static final Date ANY_DATE = new Date();
+    private static final long ANY_REPOSITORY_ID = 88L;
     private static final boolean ANY_WAS_MERGED = false;
+    private static final long ANY_USER_ID = 86L;
+    private static final String ANY_USERNAME = "pirata";
+    private static final String ANY_REPOSITORY_NAME = "presunto";
+    private static final boolean ANY_IS_PRIVATE_REPOSITORY = false;
+    private static final GithubAction ANY_ACTION = GithubAction.OPENED;
 
     @Mock
     private GithubWebhookEvent mockEvent;
@@ -41,35 +42,34 @@ public class PullRequestExtractorTest {
     }
 
     @Test
-    public void givenAPullRequestEvent_whenExtractingThePayload_thenItIsExtracted() throws Exception {
-        GithubRepository repository = new GithubRepository(ANY_REPO_ID);
-        GithubUser user = new GithubUser(ANY_OWNER_ID);
-        GithubWebhookPullRequest webhookPullRequest = new GithubWebhookPullRequest(ANY_ISSUE_ID, ANY_DATE, user, ANY_WAS_MERGED);
-        given(mockEvent.pullRequest()).willReturn(webhookPullRequest);
-        given(mockEvent.repository()).willReturn(repository);
+    public void givenAPullRequestEvent_whenExtractingTheIssue_thenItIsMarkedAsAPullRequest() throws Exception {
+        PullRequest expected = givenAPullRequest();
 
         PullRequest actual = extractor.extractFrom(mockEvent);
 
-        assertEquals(repository, actual.getRepository());
-        assertEquals(webhookPullRequest, actual.getWebhookPullRequest());
+        assertThat(actual).isEqualToComparingFieldByField(expected);
     }
 
-    @Test
-    public void givenAPullRequesEvent_whenExtractingTheIssue_thenItIsMarkedAsAPullRequest() throws Exception {
-        GithubRepository repository = new GithubRepository(ANY_REPO_ID);
-        GithubUser user = new GithubUser(ANY_OWNER_ID);
-        GithubWebhookPullRequest webhookPullRequest = new GithubWebhookPullRequest(ANY_ISSUE_ID, ANY_DATE, user, ANY_WAS_MERGED);
+    private PullRequest givenAPullRequest() {
+        GithubUser githubUser = new GithubUser(ANY_USER_ID, ANY_USERNAME);
+        GithubWebhookPullRequest webhookPullRequest = new GithubWebhookPullRequest(ANY_ISSUE_ID, ANY_DATE, githubUser, ANY_WAS_MERGED);
+        GithubRepository githubRepository = new GithubRepository(ANY_REPOSITORY_ID, ANY_REPOSITORY_NAME, ANY_IS_PRIVATE_REPOSITORY);
         given(mockEvent.pullRequest()).willReturn(webhookPullRequest);
-        given(mockEvent.repository()).willReturn(repository);
-
-        GithubIssue actualIssue = extractor.extractFrom(mockEvent).getWebhookPullRequest();
-
-        assertTrue(actualIssue.isPullRequest());
+        given(mockEvent.repository()).willReturn(githubRepository);
+        given(mockEvent.action()).willReturn(ANY_ACTION);
+        return new PullRequest(webhookPullRequest, githubRepository, ANY_ACTION);
     }
 
     @Test(expected = ExtractException.class)
-    public void givenAReviewCommentEvent_whenExtractingThePayload_thenAnExceptionIsThrown() throws Exception {
-        given(mockEvent.comment()).willReturn(NO_COMMENT);
+    public void givenAPullRequestEventWithNoComment_whenExtractingThePayload_thenAnExceptionIsThrown() throws Exception {
+        given(mockEvent.comment()).willReturn(null);
+
+        extractor.extractFrom(mockEvent);
+    }
+
+    @Test(expected = ExtractException.class)
+    public void givenAPullRequestEventWithNoPullRequest_whenExtractingThePayload_thenAnExceptionIsThrown() throws Exception {
+        given(mockEvent.pullRequest()).willReturn(null);
 
         extractor.extractFrom(mockEvent);
     }
