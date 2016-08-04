babfc53b488c2922e2aa709577d9041d3b3f80ed
takecare <rui@vazteixeira.org>
2016-08-04 10:14:51 +0100

    reduce ctor visibility

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/UnhandledEventException.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/handler/UnhandledEventException.java
@@ -1,12 +1,11 @@
 package com.novoda.github.reports.web.hooks.handler;
 
 public class UnhandledEventException extends Exception {
-
     UnhandledEventException(String message) {
         super(message);
     }
 
-    public UnhandledEventException(Throwable cause) {
+    UnhandledEventException(Throwable cause) {
         super(cause);
     }
 }
