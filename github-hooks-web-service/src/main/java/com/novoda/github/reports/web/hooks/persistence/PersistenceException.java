ffda16dcc337db6df209452d322ba9798ccc6ce1
takecare <rui@vazteixeira.org>
2016-08-04 10:09:39 +0100

    remove unused ctors

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/persistence/PersistenceException.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/persistence/PersistenceException.java
@@ -1,17 +1,6 @@
 package com.novoda.github.reports.web.hooks.persistence;
 
-import com.novoda.github.reports.web.hooks.model.Event;
-
 public class PersistenceException extends Exception {
-
-    PersistenceException(String message) {
-        super(message);
-    }
-
-    PersistenceException(Event event) {
-        super("Unable to persist event: " + event.toString());
-    }
-
     PersistenceException(Throwable cause) {
         super(cause);
     }
