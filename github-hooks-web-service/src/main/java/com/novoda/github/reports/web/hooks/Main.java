7f8b8e0d4587ae65ecb0c9126faad15915536de2
takecare <rui@vazteixeira.org>
2016-08-04 11:10:07 +0100

    extract param to local to improve readability

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/Main.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
 
-        String json = readFile("issue_edited.sample.json");
+        String json = readFile("commit_comment_created.sample.json");
 
         PostGithubWebhookEventHandler handler = new PostGithubWebhookEventHandler();
         handler.handleRequest(new StringInputStream(json), new ByteArrayOutputStream(), null);
