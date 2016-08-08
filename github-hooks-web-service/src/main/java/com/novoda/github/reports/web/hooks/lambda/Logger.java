73ae195adc6c2b63161ec7614666ddd2e3b2e860
takecare <rui@vazteixeira.org>
2016-08-08 11:25:51 +0100

    extract logging and writing to output to their own classes

---
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/lambda/Logger.java
@@ -0,0 +1,22 @@
+package com.novoda.github.reports.web.hooks.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class Logger {
+
+    private final LambdaLogger logger;
+
+    public static Logger newInstance(Context context) {
+        LambdaLogger logger = context == null ? System.out::println : context.getLogger();
+        return new Logger(logger);
+    }
+
+    private Logger(LambdaLogger logger) {
+        this.logger = logger;
+    }
+
+    public void log(String message) {
+        logger.log("> " + message);
+    }
+}
---
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/lambda/OutputWriter.java
@@ -0,0 +1,62 @@
+package com.novoda.github.reports.web.hooks.lambda;
+
+import com.google.gson.Gson;
+import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+class OutputWriter implements Closeable {
+
+    private final OutputStream outputStream;
+    private final Gson gson;
+
+    public static OutputWriter newInstance(OutputStream outputStream, Gson gson) {
+        return new OutputWriter(outputStream, gson);
+    }
+
+    private OutputWriter(OutputStream outputStream, Gson gson) {
+        this.outputStream = outputStream;
+        this.gson = gson;
+    }
+
+    public void outputInfo(String message) {
+        try {
+            writeToOutput(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void outputException(Exception exception) {
+        outputInfo(wrapInError(exception.getMessage()));
+    }
+
+    public void outputEvent(GithubWebhookEvent event) {
+        try {
+            outputInfo(gson.toJson(event, GithubWebhookEvent.class));
+        } catch (Exception e) {
+            outputException(e);
+        }
+    }
+
+    private String wrapInError(String message) {
+        return "{\"error\": \"" + message + "\"}";
+    }
+
+    private void writeToOutput(String message) throws IOException {
+        try (OutputStreamWriter writer = new OutputStreamWriter(outputStream)) {
+            writer.write(message);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        outputStream.close();
+    }
+}
