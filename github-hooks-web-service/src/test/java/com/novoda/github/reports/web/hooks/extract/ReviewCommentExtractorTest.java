8ddaeee23d220007ca5075d1db7addb267e1ac34
takecare <rui@vazteixeira.org>
2016-08-08 14:51:46 +0100

    extract pull request data into review comment object

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/ReviewCommentExtractorTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/extract/ReviewCommentExtractorTest.java
@@ -3,9 +3,10 @@ package com.novoda.github.reports.web.hooks.extract;
 import com.novoda.github.reports.service.GithubUser;
 import com.novoda.github.reports.service.issue.GithubComment;
 import com.novoda.github.reports.service.repository.GithubRepository;
-import com.novoda.github.reports.web.hooks.model.ReviewComment;
 import com.novoda.github.reports.web.hooks.model.GithubAction;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
+import com.novoda.github.reports.web.hooks.model.GithubWebhookPullRequest;
+import com.novoda.github.reports.web.hooks.model.ReviewComment;
 
 import java.util.Date;
 
@@ -25,6 +26,8 @@ public class ReviewCommentExtractorTest {
     private static final GithubAction ANY_ACTION = GithubAction.OPENED;
     private static final long ANY_COMMENT_ID = 23L;
     private static final Date ANY_DATE = new Date();
+    private static final long ANY_PULL_REQUEST_ID = 94L;
+    private static final boolean ANY_WAS_MERGED = false;
 
     @Mock
     private GithubWebhookEvent mockEvent;
@@ -50,10 +53,13 @@ public class ReviewCommentExtractorTest {
         GithubUser githubUser = new GithubUser(ANY_OWNER_ID);
         GithubComment githubComment = new GithubComment(ANY_COMMENT_ID, githubUser, ANY_DATE);
         GithubRepository githubRepository = new GithubRepository(ANY_REPOSITORY_ID);
+        GithubWebhookPullRequest githubWebhookPullRequest =
+                new GithubWebhookPullRequest(ANY_PULL_REQUEST_ID, ANY_DATE, githubUser, ANY_WAS_MERGED);
         given(mockEvent.comment()).willReturn(githubComment);
         given(mockEvent.repository()).willReturn(githubRepository);
+        given(mockEvent.pullRequest()).willReturn(githubWebhookPullRequest);
         given(mockEvent.action()).willReturn(ANY_ACTION);
-        return new ReviewComment(githubComment, githubRepository, ANY_ACTION);
+        return new ReviewComment(githubComment, githubRepository, githubWebhookPullRequest, ANY_ACTION);
     }
 
     @Test(expected = ExtractException.class)
