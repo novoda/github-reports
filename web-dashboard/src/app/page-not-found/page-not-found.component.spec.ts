929c41bde0a44087d7c89d22ea1db268e30b22e2
Francesco Pontillo <francescopontillo@gmail.com>
2016-08-08 15:32:57 +0100

    Add work dashboard and 404 page

---
+++ web-dashboard/src/app/page-not-found/page-not-found.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+describe('Component: PageNotFound', () => {
+  it('should create an instance', () => {
+    let component = new PageNotFoundComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web-dashboard/src/app/work-dashboard/work-dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-work-dashboard',
+  templateUrl: 'work-dashboard.component.html',
+  styleUrls: ['work-dashboard.component.css']
+})
+export class WorkDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
