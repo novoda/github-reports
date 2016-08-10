72204d7883d6b59a1e61193f57f474c385e137d3
Francesco Pontillo <francescopontillo@gmail.com>
2016-08-10 16:58:51 +0100

    Reformat code

--- web-dashboard/src/app/page-not-found/page-not-found.component.ts
+++ web-dashboard/src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   moduleId: module.id,
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- web-dashboard/src/app/work-user/work-user.component.ts
+++ web-dashboard/src/app/work-user/work-user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input} from "@angular/core";
 
 @Component({
   moduleId: module.id,
@@ -8,7 +8,8 @@ import {Component, Input} from '@angular/core';
 })
 export class WorkUserComponent {
 
-  constructor() { }
+  constructor() {
+  }
 
   @Input() user: any;
 
