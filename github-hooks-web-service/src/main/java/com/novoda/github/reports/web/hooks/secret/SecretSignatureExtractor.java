591c1e56f973797d93b7fb004bc43f2df601785b
takecare <rui@vazteixeira.org>
2016-08-10 14:51:55 +0100

    reduce visibility

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/secret/SecretSignatureExtractor.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/secret/SecretSignatureExtractor.java
@@ -4,9 +4,9 @@ import com.novoda.github.reports.web.hooks.model.WebhookRequest;
 
 import java.util.Map;
 
-public class SecretSignatureExtractor {
+class SecretSignatureExtractor {
 
-    public String extractSignatureFrom(WebhookRequest request) throws SecurityException {
+    String extractSignatureFrom(WebhookRequest request) throws SecurityException {
 
         Map<String, String> headers = request.headers();
         String signature = headers.get("X-Hub-Signature");
