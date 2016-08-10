b85e7b025d62c598dfd158c8ba1ac06ccb58f282
takecare <rui@vazteixeira.org>
2016-08-10 15:23:48 +0100

    throw secret exception instead of security exception

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/secret/SecretException.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/secret/SecretException.java
@@ -1,6 +1,10 @@
 package com.novoda.github.reports.web.hooks.secret;
 
 public class SecretException extends Exception {
+    SecretException(String message) {
+        super(message);
+    }
+
     SecretException(Throwable cause) {
         super(cause);
     }
