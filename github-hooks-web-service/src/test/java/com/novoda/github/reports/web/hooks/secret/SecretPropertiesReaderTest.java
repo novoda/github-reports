a9b721bdcc27d5364fb2f3103145239fd71d8544
takecare <rui@vazteixeira.org>
2016-08-10 14:25:54 +0100

    add a secret properties reader

---
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/secret/SecretPropertiesReaderTest.java
@@ -0,0 +1,36 @@
+package com.novoda.github.reports.web.hooks.secret;
+
+import com.novoda.github.reports.properties.PropertiesReader;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class SecretPropertiesReaderTest {
+
+    private static final java.lang.String ANY_KEY = "sempartirtudo";
+    @Mock
+    private PropertiesReader mockPropertiesReader;
+
+    @InjectMocks
+    private SecretPropertiesReader secretPropertiesReader;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        when(mockPropertiesReader.readProperty("SECRET")).thenReturn(ANY_KEY);
+    }
+
+    @Test
+    public void givenAPropertiesReader_whenReadingTheSecret_thenItIsReturned() {
+
+        String actual = secretPropertiesReader.getSecret();
+
+        assertEquals(ANY_KEY, actual);
+    }
+}
