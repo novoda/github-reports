c85a33b6224184445859603a564d6223d116d406
Francesco Pontillo <francescopontillo@gmail.com>
2016-08-11 15:08:40 +0100

    Make non external contributors cards glow in orange

--- web-dashboard/src/app/work-user/work-user.component.ts
+++ web-dashboard/src/app/work-user/work-user.component.ts
@@ -1,20 +1,22 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges, HostBinding} from '@angular/core';
 
 @Component({
   selector: 'work-user',
   templateUrl: 'work-user.component.html',
   styleUrls: ['work-user.component.scss']
 })
-export class WorkUserComponent {
+export class WorkUserComponent implements OnChanges {
 
   @Input() user: any;
+
+  @HostBinding('class.bad') isBad: boolean = false;
 
   constructor() {
   }
 
-  getLevel(user: any): string {
-    const total = user.assignedCount + user.externalCount;
-    return total > 0 ? 'good' : 'bad';
+  ngOnChanges(changes: SimpleChanges): void {
+    const total = this.user.assignedCount + this.user.externalCount;
+    this.isBad = total <= 0;
   }
 
 }
