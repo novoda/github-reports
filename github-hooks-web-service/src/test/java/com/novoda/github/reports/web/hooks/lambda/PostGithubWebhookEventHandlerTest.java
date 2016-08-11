a98a48acaef6c41f5a92dd863e1ed6fccd6c0345
takecare <rui@vazteixeira.org>
2016-08-11 17:37:16 +0100

    move json test content to json files

--- github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/lambda/PostGithubWebhookEventHandlerTest.java
+++ github-hooks-web-service/src/test/java/com/novoda/github/reports/web/hooks/lambda/PostGithubWebhookEventHandlerTest.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.util.StringInputStream;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.novoda.github.reports.web.hooks.Main;
 import com.novoda.github.reports.web.hooks.handler.EventForwarder;
 import com.novoda.github.reports.web.hooks.handler.UnhandledEventException;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
@@ -12,7 +13,15 @@ import com.novoda.github.reports.web.hooks.secret.PayloadVerifier;
 import com.novoda.github.reports.web.hooks.secret.SecretException;
 import com.ryanharter.auto.value.gson.AutoValueGsonTypeAdapterFactory;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,22 +37,6 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class PostGithubWebhookEventHandlerTest {
 
     private static final Context ANY_CONTEXT = null;
-
-    private static final String VALID_KEY = "0245b9d1f8012444a121aeadc0acf6669a9d0c53";
-    private static final String INVALID_KEY = "0245b9d1f22444a121aeaoic0acf6669a9d0c53";
-    private static final String JSON_REQUEST = "{\"method\":\"POST\",\"body\":{\"action\":\"created\",\"issue\":{\"id\":170115015," +
-            "\"number\":5,\"title\":\"NullPointerException\",\"user\":{\"login\":\"takecare\",\"id\":212528,\"type\":\"User\"}," +
-            "\"state\":\"open\",\"comments\":2,\"created_at\":\"2016-08-09T08:58:23Z\",\"updated_at\":\"2016-08-10T14:45:27Z\"," +
-            "\"pull_request\":{\"url\":\"https://api.github.com/repos/takecare/dump/pulls/5\"},\"body\":\"wut wut wut\"},\"comment\":{" +
-            "\"id\":238889574,\"user\":{\"login\":\"takecare\",\"id\":212528,\"type\":\"User\"},\"created_at\":\"2016-08-10T14:45:27Z\"," +
-            "\"updated_at\":\"2016-08-10T14:45:27Z\",\"body\":\"another one\"},\"repository\":{\"id\":63702872,\"name\":\"dump\"," +
-            "\"full_name\":\"takecare/dump\",\"owner\":{\"login\":\"takecare\",\"id\":212528,\"type\":\"User\"},\"private\":false,\"" +
-            "description\":\"Github testbed \",\"fork\":false,\"created_at\":\"2016-07-19T14:51:48Z\",\"updated_at\":" +
-            "\"2016-07-19T14:51:48Z\",\"has_issues\":true},\"sender\":{\"login\":\"takecare\",\"id\":212528,\"type\":\"User\"}},\"headers\":{" +
-            "\"content-type\":\"application/json\",\"Host\":\"***REMOVED***\"," +
-            "\"User-Agent\":\"GitHub-Hookshot/c80b888\",\"X-Hub-Signature\":\"sha1=%s\"}}";
-    private static final String VALID_JSON_REQUEST = String.format(JSON_REQUEST, VALID_KEY);
-    private static final String INVALID_JSON_REQUEST = String.format(JSON_REQUEST, INVALID_KEY);
 
     @Mock
     private PayloadVerifier mockPayloadVerifier;
@@ -72,20 +65,24 @@ public class PostGithubWebhookEventHandlerTest {
 
     @Test
     public void givenAValidRequest_whenHandlingIt_thenWeCheckIfItsPayloadIsValid() throws Exception {
-        WebhookRequest request = gson.fromJson(VALID_JSON_REQUEST, WebhookRequest.class);
+        WebhookRequest request = gson.fromJson(givenAValidJsonRequest(), WebhookRequest.class);
 
-        handler.handleRequest(new StringInputStream(VALID_JSON_REQUEST), mock(OutputStream.class), ANY_CONTEXT);
+        handler.handleRequest(new StringInputStream(givenAValidJsonRequest()), mock(OutputStream.class), ANY_CONTEXT);
 
         verify(mockPayloadVerifier).checkIfPayloadIsValid(request);
     }
 
     @Test
     public void givenAValidRequest_whenHandlingIt_thenTheEventIsForwarded() throws Exception {
-        WebhookRequest request = gson.fromJson(VALID_JSON_REQUEST, WebhookRequest.class);
+        WebhookRequest request = gson.fromJson(givenAValidJsonRequest(), WebhookRequest.class);
 
-        handler.handleRequest(new StringInputStream(VALID_JSON_REQUEST), mock(OutputStream.class), ANY_CONTEXT);
+        handler.handleRequest(new StringInputStream(givenAValidJsonRequest()), mock(OutputStream.class), ANY_CONTEXT);
 
         assertThatTheEventIsForwarded(request);
+    }
+
+    private String givenAValidJsonRequest() throws IOException, URISyntaxException {
+        return readFile("valid_request.json");
     }
 
     private void assertThatTheEventIsForwarded(WebhookRequest request) throws UnhandledEventException {
@@ -102,8 +99,21 @@ public class PostGithubWebhookEventHandlerTest {
     public void givenAnInvalidRequest_whenHandlingIt_thenAnExceptionIsThrown() throws Exception {
         doThrow(SecretException.class).when(mockPayloadVerifier).checkIfPayloadIsValid(any(WebhookRequest.class));
 
-        handler.handleRequest(new StringInputStream(INVALID_JSON_REQUEST), mock(OutputStream.class), ANY_CONTEXT);
+        handler.handleRequest(new StringInputStream(givenAnInvalidJsonRequest()), mock(OutputStream.class), ANY_CONTEXT);
 
+    }
+
+    private String givenAnInvalidJsonRequest() throws IOException, URISyntaxException {
+        return readFile("invalid_request.json");
+    }
+
+    private static String readFile(String fileName) throws URISyntaxException, IOException {
+        URL url = Main.class.getClassLoader().getResource(fileName);
+        if (url == null) {
+            throw new FileNotFoundException(fileName + " was not found in the resources directory.");
+        }
+        Path path = Paths.get(url.toURI());
+        return Files.lines(path).collect(Collectors.joining("\n"));
     }
 
 }
