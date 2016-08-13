3133d865f5e7684c3cb7f0beb1cfa66d1edd45af
takecare <rui@vazteixeira.org>
2016-08-13 15:56:13 +0100

    replace dependency on repos service with api service, on repos service client

--- batch-aws-lambda/src/main/java/com/novoda/github/reports/lambda/repository/RepositoriesServiceClient.java
+++ batch-aws-lambda/src/main/java/com/novoda/github/reports/lambda/repository/RepositoriesServiceClient.java
@@ -10,12 +10,12 @@ import com.novoda.github.reports.data.db.properties.DatabaseCredentialsReader;
 import com.novoda.github.reports.data.db.tables.records.RepositoryRecord;
 import com.novoda.github.reports.data.model.Repository;
 import com.novoda.github.reports.lambda.persistence.PersistOperator;
+import com.novoda.github.reports.service.network.GithubApiService;
+import com.novoda.github.reports.service.network.GithubServiceContainer;
 import com.novoda.github.reports.service.persistence.ConnectionManagerContainer;
 import com.novoda.github.reports.service.persistence.converter.Converter;
 import com.novoda.github.reports.service.persistence.converter.RepositoryConverter;
 import com.novoda.github.reports.service.repository.GithubRepository;
-import com.novoda.github.reports.service.repository.GithubRepositoryService;
-import com.novoda.github.reports.service.repository.RepositoryService;
 
 import rx.Observable;
 
@@ -23,40 +23,39 @@ public class RepositoriesServiceClient {
 
     private static final int DEFAULT_PER_PAGE_COUNT = 100;
 
-    private final RepositoryService repositoryService;
+    private final GithubApiService apiService;
     private final DbDataLayer<Repository, RepositoryRecord> repoDataLayer;
     private final Converter<GithubRepository, Repository> converter;
 
     public static RepositoriesServiceClient newInstance() {
-        RepositoryService repositoriesService = GithubRepositoryService.newInstance();
+        GithubApiService apiService = GithubServiceContainer.getGithubService();
         ConnectionManager connectionManager = ConnectionManagerContainer.getConnectionManager();
         DbDataLayer<Repository, RepositoryRecord> repoDataLayer = DbRepoDataLayer.newInstance(connectionManager);
         Converter<GithubRepository, Repository> converter = RepositoryConverter.newInstance();
-        return new RepositoriesServiceClient(repositoriesService, repoDataLayer, converter);
+        return new RepositoriesServiceClient(apiService, repoDataLayer, converter);
     }
 
     public static RepositoriesServiceClient newInstance(DatabaseCredentialsReader databaseCredentialsReader) {
-
-        RepositoryService repositoriesService = GithubRepositoryService.newInstance();
+        GithubApiService apiService = GithubServiceContainer.getGithubService();
         ConnectionManager connectionManager = ConnectionManagerContainer.getConnectionManager(databaseCredentialsReader);
         DbDataLayer<Repository, RepositoryRecord> repoDataLayer = DbRepoDataLayer.newInstance(connectionManager);
         Converter<GithubRepository, Repository> converter = RepositoryConverter.newInstance();
 
-        return new RepositoriesServiceClient(repositoriesService, repoDataLayer, converter);
+        return new RepositoriesServiceClient(apiService, repoDataLayer, converter);
     }
 
-    private RepositoriesServiceClient(RepositoryService repositoryService,
+    private RepositoriesServiceClient(GithubApiService apiService,
                                       DbDataLayer<Repository, RepositoryRecord> repoDataLayer,
                                       Converter<GithubRepository, Repository> converter) {
 
-        this.repositoryService = repositoryService;
+        this.apiService = apiService;
         this.repoDataLayer = repoDataLayer;
         this.converter = converter;
     }
 
     public Observable<AmazonQueueMessage> retrieveRepositoriesFor(AmazonGetRepositoriesQueueMessage message) {
-        return repositoryService
-                .getRepositoriesFor(message.organisationName(), pageFrom(message), DEFAULT_PER_PAGE_COUNT)
+        return apiService
+                .getRepositoriesResponseForPage(message.organisationName(), pageFrom(message), DEFAULT_PER_PAGE_COUNT)
                 .lift(persistRepositories())
                 .compose(getNextQueueMessages(message));
     }
