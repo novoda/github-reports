e8aaf08231d8dc9dfd0dba30949d9f40ff32d290
takecare <rui@vazteixeira.org>
2016-08-13 13:58:56 +0100

    replace dependency on pull request service with api service, on pull request service client

--- reports-batch/src/main/java/com/novoda/github/reports/batch/local/pullrequest/PullRequestServiceClient.java
+++ reports-batch/src/main/java/com/novoda/github/reports/batch/local/pullrequest/PullRequestServiceClient.java
@@ -2,9 +2,10 @@ package com.novoda.github.reports.batch.local.pullrequest;
 
 import com.novoda.github.reports.service.issue.GithubComment;
 import com.novoda.github.reports.service.network.DateToISO8601Converter;
+import com.novoda.github.reports.service.network.GithubApiService;
+import com.novoda.github.reports.service.network.GithubCachingServiceContainer;
 import com.novoda.github.reports.service.network.PagedTransformer;
 import com.novoda.github.reports.service.network.RateLimitDelayTransformer;
-import com.novoda.github.reports.service.pullrequest.GithubPullRequestService;
 
 import java.util.Date;
 import java.util.List;
@@ -17,22 +18,22 @@ public class PullRequestServiceClient {
     private static final int DEFAULT_PER_PAGE_COUNT = 100;
     private static final int FIRST_PAGE = 1;
 
-    private final GithubPullRequestService githubPullRequestService;
+    private final GithubApiService apiService;
     private final DateToISO8601Converter dateConverter;
     private final RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer;
 
     public static PullRequestServiceClient newInstance() {
-        GithubPullRequestService githubPullRequestService = GithubPullRequestService.newCachingInstance();
+        GithubApiService apiService = GithubCachingServiceContainer.getGithubService();
         DateToISO8601Converter dateConverter = new DateToISO8601Converter();
         RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer = RateLimitDelayTransformer.newInstance();
-        return new PullRequestServiceClient(githubPullRequestService, dateConverter, commentRateLimitDelayTransformer);
+        return new PullRequestServiceClient(apiService, dateConverter, commentRateLimitDelayTransformer);
     }
 
-    private PullRequestServiceClient(GithubPullRequestService githubPullRequestService,
+    private PullRequestServiceClient(GithubApiService apiService,
                                      DateToISO8601Converter dateConverter,
                                      RateLimitDelayTransformer<GithubComment> commentRateLimitDelayTransformer) {
 
-        this.githubPullRequestService = githubPullRequestService;
+        this.apiService = apiService;
         this.dateConverter = dateConverter;
         this.commentRateLimitDelayTransformer = commentRateLimitDelayTransformer;
     }
@@ -50,7 +51,7 @@ public class PullRequestServiceClient {
                                                                                               int page,
                                                                                               int pageCount) {
 
-        return githubPullRequestService.getPullRequestReviewCommentsFor(organisation, repository, pullRequestNumber, since, page, pageCount)
+        return apiService.getReviewCommentsResponseForPullRequestAndPage(organisation, repository, pullRequestNumber, since, page, pageCount)
                 .compose(commentRateLimitDelayTransformer)
                 .compose(PagedTransformer.newInstance(nextPage -> getPagedReviewCommentsForPullRequestFor(
                         organisation,
