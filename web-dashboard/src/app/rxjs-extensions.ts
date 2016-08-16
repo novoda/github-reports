4d3df973d0c3bd0d357acec3b3034c5aa0d4785e
Francesco Pontillo <francescopontillo@gmail.com>
2016-08-16 14:26:12 +0100

    Remove useless rxjs extensions, included by default

--- web-dashboard/src/app/rxjs-extensions.ts
+++
@@ -1,9 +0,0 @@
-// Observable class extensions
-import 'rxjs/add/observable/from';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/filter';
-import 'rxjs/operator/groupBy';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toArray';
-// Observable operators
