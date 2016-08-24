7f64b31eb1ccb79fc892d503e9a73b9fb289a9fa
Francesco Pontillo <francescopontillo@gmail.com>
2016-08-24 17:05:34 +0100

    Extract constants in test

--- github/src/test/java/com/novoda/github/reports/service/issue/RepositoryIssueEventTypeTest.java
+++ github/src/test/java/com/novoda/github/reports/service/issue/RepositoryIssueEventTypeTest.java
@@ -8,12 +8,15 @@ import static org.junit.Assert.assertEquals;
 
 public class RepositoryIssueEventTypeTest {
 
+    private static final GithubEvent.Type ANY_NON_SUPPORTED_EVENT_TYPE = GithubEvent.Type.UNLOCKED;
+    private static final GithubEvent.Type ANY_SUPPORTED_EVENT_TYPE = GithubEvent.Type.COMMENTED;
+
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void givenSupportedEventType_whenCreateTypeFromEvent_thenReturnCorrectMatch() {
-        GithubEvent.Type type = GithubEvent.Type.COMMENTED;
+        GithubEvent.Type type = ANY_SUPPORTED_EVENT_TYPE;
 
         RepositoryIssueEvent.Type actual = RepositoryIssueEvent.Type.fromEvent(type.toString());
 
@@ -22,7 +25,7 @@ public class RepositoryIssueEventTypeTest {
 
     @Test
     public void givenNonSupportedEventType_whenCreateTypeFromEvent_thenThrowException() {
-        GithubEvent.Type type = GithubEvent.Type.UNLOCKED;
+        GithubEvent.Type type = ANY_NON_SUPPORTED_EVENT_TYPE;
 
         expectedException.expect(IllegalArgumentException.class);
 
