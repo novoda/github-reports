e7e095429c9343d86f755e36c5d466d1fc52f6b9
takecare <rui@vazteixeira.org>
2016-11-22 12:29:00 +0000

    add json deserializer to parse the new Entry

--- sheets/src/main/java/com/novoda/github/reports/sheets/network/SheetsServiceFactory.java
+++ sheets/src/main/java/com/novoda/github/reports/sheets/network/SheetsServiceFactory.java
@@ -1,8 +1,13 @@
 package com.novoda.github.reports.sheets.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonObject;
 import com.novoda.github.reports.network.HttpClientFactory;
 import com.novoda.github.reports.network.OkHttpClientFactory;
 import com.novoda.github.reports.network.ServiceFactory;
+import com.novoda.github.reports.sheets.sheet.Entry;
 
 import okhttp3.OkHttpClient;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -24,7 +29,20 @@ public class SheetsServiceFactory extends ServiceFactory<SheetsApiService> {
 
     private static SheetsServiceFactory newInstance(HttpClientFactory httpClientFactory) {
         OkHttpClient okHttpClient = httpClientFactory.createClient();
-        GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        JsonDeserializer<Entry> jsonDeserializer = (json, typeOfT, context) -> {
+            JsonObject jsonObject = json.getAsJsonObject();
+
+            JsonObject titleObject = jsonObject.get("title").getAsJsonObject();
+            JsonObject contentObject = jsonObject.get("content").getAsJsonObject();
+
+            return new Entry(titleObject.get("$t").getAsString(), contentObject.get("$t").getAsString());
+        };
+        gsonBuilder.registerTypeAdapter(Entry.class, jsonDeserializer);
+        Gson gson = gsonBuilder.create();
+
+        GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create(gson);
         RxJavaCallAdapterFactory rxJavaCallAdapterFactory = RxJavaCallAdapterFactory.create();
         return new SheetsServiceFactory(okHttpClient, gsonConverterFactory, rxJavaCallAdapterFactory);
     }
