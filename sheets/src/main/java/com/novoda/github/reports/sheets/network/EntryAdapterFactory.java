6105c67fcd0a373ea1b617044ee181bffcf641ba
takecare <rui@vazteixeira.org>
2016-11-23 09:15:11 +0000

    refactor entry adapter factory by moving the call adapter to its own class

--- sheets/src/main/java/com/novoda/github/reports/sheets/network/EntryAdapterFactory.java
+++ sheets/src/main/java/com/novoda/github/reports/sheets/network/EntryAdapterFactory.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-import retrofit2.Call;
 import retrofit2.CallAdapter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -22,23 +21,9 @@ class EntryAdapterFactory extends CallAdapter.Factory {
             return null; // move on to next adapter
         }
 
-//        CallAdapter delegate = retrofit.nextCallAdapter(this, returnType, annotations);
         Type type = new TypeToken<Observable<Response<Sheet>>>() {}.getType();
-        CallAdapter delegate = retrofit.nextCallAdapter(this, type, annotations);
-        return new CallAdapter<Observable<Entry>>() {
-            @Override
-            public Type responseType() {
-                return delegate.responseType();
-            }
-
-            @Override
-            public <R> Observable<Entry> adapt(Call<R> call) {
-                Observable<Response<Sheet>> observable = (Observable<Response<Sheet>>) delegate.adapt(call);
-                return observable
-                        .flatMap(sheetResponse -> Observable.from(sheetResponse.body().getFeed().getEntries()));
-            }
-
-        };
+        CallAdapter<Observable<Response<Sheet>>> delegate = getDelegateCallAdapter(annotations, retrofit, type);
+        return new EntryCallAdapter(delegate);
     }
 
     private boolean isAcceptedType(Type type) {
@@ -59,6 +44,11 @@ class EntryAdapterFactory extends CallAdapter.Factory {
         }
 
         return true;
+    }
+
+    @SuppressWarnings("unchecked") // we're forced to cast due to having to implement <R> T adapt(Call<R> call)
+    private CallAdapter<Observable<Response<Sheet>>> getDelegateCallAdapter(Annotation[] annotations, Retrofit retrofit, Type type) {
+        return (CallAdapter<Observable<Response<Sheet>>>) retrofit.nextCallAdapter(this, type, annotations);
     }
 
 }
