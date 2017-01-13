6a6c7372dee3721209e34a4de3708ebc43c31f58
takecare <rui@vazteixeira.org>
2017-01-13 18:18:07 +0000

    use classes with extracted responsibilities

--- github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/lambda/PostGithubWebhookEventHandler.java
+++ github-hooks-web-service/src/main/java/com/novoda/github/reports/web/hooks/lambda/PostGithubWebhookEventHandler.java
@@ -8,63 +8,80 @@ import com.novoda.github.reports.web.hooks.handler.EventForwarder;
 import com.novoda.github.reports.web.hooks.model.GithubWebhookEvent;
 import com.novoda.github.reports.web.hooks.model.WebhookRequest;
 import com.novoda.github.reports.web.hooks.secret.PayloadVerifier;
-import com.novoda.github.reports.web.hooks.secret.InvalidSecretException;
 import com.ryanharter.auto.value.gson.AutoValueGsonTypeAdapterFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
 
-import org.jetbrains.annotations.Nullable;
 import org.jooq.tools.JooqLogger;
 
 public class PostGithubWebhookEventHandler implements RequestStreamHandler {
 
-    private final PayloadVerifier payloadVerifier;
     private final EventForwarder eventForwarder;
     private final OutputWriter outputWriter;
-    private Logger logger;
-    private final Gson gson;
+    private final Logger logger;
+
+    private final PayloadVerificationRunner payloadVerificationRunner;
+    private final WebhookRequestExtractor webhookRequestExtractor;
+    private final EventExtractor eventExtractor;
 
     public PostGithubWebhookEventHandler() {
+
         Gson gson = new GsonBuilder()
                 .registerTypeAdapterFactory(new AutoValueGsonTypeAdapterFactory())
                 .create();
+
+        PayloadVerifier payloadVerifier = PayloadVerifier.newInstance();
+        InputStreamReaderFactory inputStreamReaderFactory = new InputStreamReaderFactory();
+        OutputWriter outputWriter = OutputWriter.newInstance(gson);
+
+        this.payloadVerificationRunner = new PayloadVerificationRunner(payloadVerifier, outputWriter);
+        this.webhookRequestExtractor = new WebhookRequestExtractor(inputStreamReaderFactory, outputWriter, gson);
+        this.eventExtractor = new EventExtractor(outputWriter, gson);
+
         this.eventForwarder = EventForwarder.newInstance();
-        this.payloadVerifier = PayloadVerifier.newInstance();
         this.outputWriter = OutputWriter.newInstance(gson);
-        this.gson = gson;
+        this.logger = Logger.newInstance();
     }
 
     PostGithubWebhookEventHandler(
-            PayloadVerifier payloadVerifier,
+            PayloadVerificationRunner payloadVerificationRunner,
+            WebhookRequestExtractor webhookRequestExtractor,
+            EventExtractor eventExtractor,
             EventForwarder eventForwarder,
             OutputWriter outputWriter,
-            Logger logger,
-            Gson gson) {
+            Logger logger) {
 
-        this.payloadVerifier = payloadVerifier;
+        this.payloadVerificationRunner = payloadVerificationRunner;
+        this.webhookRequestExtractor = webhookRequestExtractor;
+        this.eventExtractor = eventExtractor;
         this.eventForwarder = eventForwarder;
         this.outputWriter = outputWriter;
         this.logger = logger;
-        this.gson = gson;
     }
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) {
-        logger = Logger.newInstance(context);
         disableJooqLogAd();
-
-        attachOutputStream(output);
+        logger.setLoggerFrom(context);
+        outputWriter.setOutputStream(output);
 
         logger.log("λ STARTING...");
 
-        WebhookRequest request = getRequestFrom(input);
-        breakIfPayloadNotValid(request);
+        try {
+            WebhookRequest request = webhookRequestExtractor.extractFrom(input);
+            handle(request);
+        } finally {
+            closeOutputWriter();
+        }
+    }
 
-        GithubWebhookEvent event = getEventFrom(request);
+    private void handle(WebhookRequest request) {
+
+        payloadVerificationRunner.verify(request);
+
+        GithubWebhookEvent event = eventExtractor.extractFrom(request);
 
         try {
             logger.log("FORWARDING EVENT...");
@@ -74,60 +91,11 @@ public class PostGithubWebhookEventHandler implements RequestStreamHandler {
         } catch (Exception e) {
             logger.log("ERROR: Failed to forward an event (" + event.toString() + "). " + e.getMessage());
             outputWriter.outputException(e);
-        } finally {
-            closeOutputWriter();
         }
     }
 
     private void disableJooqLogAd() {
         JooqLogger.globalThreshold(JooqLogger.Level.WARN);
-    }
-
-    private void attachOutputStream(OutputStream output) {
-        try {
-            outputWriter.attach(output);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Nullable
-    private WebhookRequest getRequestFrom(InputStream input) {
-        Reader reader = new InputStreamReader(input);
-        WebhookRequest request = null;
-
-        try {
-            request = gson.fromJson(reader, WebhookRequest.class);
-        } catch (Exception e) {
-            outputWriter.outputException(e);
-        }
-
-        return request;
-    }
-
-    private void breakIfPayloadNotValid(WebhookRequest request) {
-        try {
-            payloadVerifier.checkIfPayloadIsValid(request);
-        } catch (InvalidSecretException e) {
-            e.printStackTrace();
-            outputWriter.outputException(e);
-        }
-    }
-
-    @Nullable
-    private GithubWebhookEvent getEventFrom(WebhookRequest request) {
-        if (request.body() == null) {
-            outputWriter.outputException(new NullPointerException("No event data found. Check Github's webhook delivery report."));
-        }
-
-        GithubWebhookEvent event = null;
-        try {
-            event = gson.fromJson(request.body(), GithubWebhookEvent.class);
-        } catch (Exception e) {
-            outputWriter.outputException(e);
-        }
-
-        return event;
     }
 
     private void closeOutputWriter() {
